71a937fa71f2bd1b4f275123609e0551f838211b
youngmtool <youngmtool@gmail.com>
2017-10-30 22:17:24 +0900

    Add code for pagination into ProductController

--- CSharpWeb/SportsStore/SportsStore/Controllers/ProductController.cs
+++ CSharpWeb/SportsStore/SportsStore/Controllers/ProductController.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using System.Linq;
+
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int productPage = 1)
+        {
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
+        }
+             
     }
 }
