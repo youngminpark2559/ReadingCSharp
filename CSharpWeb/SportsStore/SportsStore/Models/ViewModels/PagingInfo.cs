72126dc985486a5f53f5ce537dc31fba1a9ee634
youngmtool <youngmtool@gmail.com>
2017-10-30 22:33:01 +0900

    Add PagingInfo view-model which delivers pagination data from controller to view

---
+++ CSharpWeb/SportsStore/SportsStore/Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//c Add PagingInfo view-model which delivers pagination data from controller to view.
+
+namespace SportsStore.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+    }
+}
