57ab5f216b10510abff0c12ccd6f50e3c7a04c0f
youngmtool <youngmtool@gmail.com>
2017-10-30 20:03:10 +0900

    Add code setting the default route

--- CSharpWeb/SportsStore/SportsStore/Startup.cs
+++ CSharpWeb/SportsStore/SportsStore/Startup.cs
@@ -27,6 +27,9 @@ namespace SportsStore
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Product}/{action=List}/{id?}");
             });
         }
     }
