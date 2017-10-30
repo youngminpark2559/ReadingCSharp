f15aee8b2e54612c88e8f18ec5ec49bd7c370187
youngmtool <youngmtool@gmail.com>
2017-10-30 18:41:24 +0900

    Add IProductRepository.cs which consists of Repository layer which plays a role of logic for storing and retrieving date from persistence data store, in conjunction with classes which implement IProductRepository.cs

---
+++ CSharpWeb/SportsStore/SportsStore/Models/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//c Add IProductRepository.cs which consists of Repository layer which plays a role of logic for storing and retrieving date from persistence data store, in conjunction with classes which implement IProductRepository.cs.
+
+namespace SportsStore.Models
+{
+    interface IProductRepository
+    {
+    }
+}
