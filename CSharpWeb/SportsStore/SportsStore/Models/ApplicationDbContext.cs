39762d59629a7c6d318d15f7d1fb923e9f85914f
youngmtool <youngmtool@gmail.com>
2017-10-30 20:25:46 +0900

    Add EFProductRepository which implements IProductRepository

--- CSharpWeb/SportsStore/SportsStore/Models/ApplicationDbContext.cs
+++ CSharpWeb/SportsStore/SportsStore/Models/ApplicationDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.DependencyInjection;
+
+//c Add ApplicationDbContext which is derived from DbContext base class which is one of the components of EF Core, which plays a role of kind of bridge between my app and DB
+
 namespace SportsStore.Models
 {
     public class ApplicationDbContext : DbContext
---
+++ CSharpWeb/SportsStore/SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
