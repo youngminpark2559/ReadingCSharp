0337518f3198310b14ae8bc45ac43813b077ab11
youngmtool <youngmtool@gmail.com>
2017-11-01 14:08:13 +0900

    Add unit test Can_Remove_Line() to test if I can remove certain product(in this case, p2) which is contained in CartLine from the Cart

--- CSharpWeb/SportsStore/SportsStore.Tests/CartTests.cs
+++ CSharpWeb/SportsStore/SportsStore.Tests/CartTests.cs
@@ -6,6 +6,8 @@ using Xunit;
 
 //c Add unit test Can_Add_Quantity_For_Existing_Lines() to test if the functionality of adding orders on existing containing products CartLine by incrementing the number of the products works well.
 
+//c Add unit test Can_Remove_Line() to test if I can remove certain product(in this case, p2) which is contained in CartLine from the Cart.
+
 namespace SportsStore.Tests
 {
     public class CartTests
@@ -52,6 +54,31 @@ namespace SportsStore.Tests
             Assert.Equal(2, results.Length);
             Assert.Equal(11, results[0].Quantity);
             Assert.Equal(1, results[1].Quantity);
+        }
+
+
+        
+        [Fact]
+        public void Can_Remove_Line()
+        {
+            // Arrange - create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+            // Arrange - create a new cart
+            Cart target = new Cart();
+            // Arrange - add some products to the cart
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+
+            // Act
+            target.RemoveLine(p2);
+
+            // Assert
+            Assert.Equal(0, target.Lines.Where(c => c.Product == p2).Count());
+            Assert.Equal(2, target.Lines.Count());
         }
     }
 }
