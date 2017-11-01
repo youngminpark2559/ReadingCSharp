5f7a261b89e85285dbc53ff38d2320fab7752448
youngmtool <youngmtool@gmail.com>
2017-11-01 13:15:53 +0900

    Add unit test Generate_Category_Specific_Product_Count() to test if the implementation for correction of page number works well

--- CSharpWeb/SportsStore/SportsStore.Tests/ProductControllerTests.cs
+++ CSharpWeb/SportsStore/SportsStore.Tests/ProductControllerTests.cs
@@ -5,6 +5,10 @@ using SportsStore.Controllers;
 using SportsStore.Models;
 using Xunit;
 using SportsStore.Models.ViewModels;
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+//c Add unit test Generate_Category_Specific_Product_Count() to test if the implementation for correction of page number works well.
 
 namespace SportsStore.Tests
 {
@@ -95,5 +99,33 @@ namespace SportsStore.Tests
             Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
         }
 
+        [Fact]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+        new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+        new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+        new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+        new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+        new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+    }).AsQueryable<Product>());
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+            Func<ViewResult, ProductsListViewModel> GetModel = result =>
+                result?.ViewData?.Model as ProductsListViewModel;
+
+            // Action
+            int? res1 = GetModel(target.List("Cat1"))?.PagingInfo.TotalItems;
+            int? res2 = GetModel(target.List("Cat2"))?.PagingInfo.TotalItems;
+            int? res3 = GetModel(target.List("Cat3"))?.PagingInfo.TotalItems;
+            int? resAll = GetModel(target.List(null))?.PagingInfo.TotalItems;
+            // Assert
+            Assert.Equal(2, res1);
+            Assert.Equal(2, res2);
+            Assert.Equal(1, res3);
+            Assert.Equal(5, resAll);
+        }
     }
 }
