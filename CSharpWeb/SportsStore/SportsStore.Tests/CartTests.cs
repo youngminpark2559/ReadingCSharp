cca99f9726ce1b7fd73f0fa076d540a5f7ddc139
youngmtool <youngmtool@gmail.com>
2017-11-01 13:36:20 +0900

    Add unit test Can_Add_New_Lines() to test if functionality of adding orders into new CartLine and Cart, works well

---
+++ CSharpWeb/SportsStore/SportsStore.Tests/CartTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using SportsStore.Models;
+using Xunit;
+
+//c Add unit test Can_Add_New_Lines() to test if functionality of adding orders into new CartLine and Cart, works well.
+
+namespace SportsStore.Tests
+{
+    public class CartTests
+    {
+        [Fact]
+        public void Can_Add_New_Lines()
+        {
+            // Arrange - create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            // Arrange - create a new cart
+            Cart target = new Cart();
+
+            // Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            CartLine[] results = target.Lines.ToArray();
+
+            // Assert
+            Assert.Equal(2, results.Length);
+            Assert.Equal(p1, results[0].Product);
+            Assert.Equal(p2, results[1].Product);
+        }
+    }
+}
