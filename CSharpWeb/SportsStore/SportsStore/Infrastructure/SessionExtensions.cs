792a92d10180ef6a1f9233042d29bbfcda5d6327
youngmtool <youngmtool@gmail.com>
2017-11-01 16:58:41 +0900

    Add CartIndexViewModel.cs which is used to store Cart object and ReturnUrl string and to display Cart state on the web page

--- CSharpWeb/SportsStore/SportsStore/Infrastructure/SessionExtensions.cs
+++ CSharpWeb/SportsStore/SportsStore/Infrastructure/SessionExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 
-//c Add SessionExtensions.cs and add SetJson(), GetJson() extension methods which extend funtionalities of ISession interface by adding these extension methods into interface. 1)GetJson() is used to extract JSON string which is keyed by "Cart" from HttpContext.Session(On server memory) and deserialize this "Cart" keyed JSON string to Cart type object. 2)SetJson() is used to serialize Cart object to JSON type and sets key of this JSON string to "Cart" string.
+//c Add SessionExtensions.cs and add SetJson(), GetJson() extension methods which extend funtionalities of ISession interface by adding these extension methods into interface. 1)GetJson() is used to extract JSON string which is keyed by "Cart" from HttpContext.Session(On server memory) and deserialize this "Cart" keyed JSON string to Cart type object. 2)SetJson() is used to serialize Cart object to JSON format and sets key of this JSON string to "Cart" string.
 
 namespace SportsStore.Infrastructure
 {
---
+++ CSharpWeb/SportsStore/SportsStore/Models/ViewModels/CartIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SportsStore.Models;
+
+//c Add CartIndexViewModel.cs which is used to store Cart object and ReturnUrl string and to display Cart state on the web page.
+
+namespace SportsStore.Models.ViewModels
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
