b3be1d1ed15b545f34f47d669e2dc805820c5ce5
youngmtool <youngmtool@gmail.com>
2017-11-01 13:01:47 +0900

    Update ProductController.cs to correct page number. If the category is not selected(category==null), entire number of Product is assigned to TotalItems. If certain category is selected, the number of that category is assigned to TotalItems

--- CSharpWeb/SportsStore/SportsStore/Controllers/ProductController.cs
+++ CSharpWeb/SportsStore/SportsStore/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using SportsStore.Models;
 using SportsStore.Models.ViewModels;
 using System.Linq;
 
+//c Update ProductController.cs to correct page number. If the category is not selected(category==null), entire number of Product is assigned to TotalItems. If certain category is selected, the number of that category is assigned to TotalItems.
+
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
@@ -27,7 +29,9 @@ namespace SportsStore.Controllers
                {
                    CurrentPage = productPage,
                    ItemsPerPage = PageSize,
-                   TotalItems = repository.Products.Count()
+                   TotalItems =  category == null ?
+                        repository.Products.Count() :
+                        repository.Products.Where(e => e.Category == category).Count()
                },
                CurrentCategory = category
            });
