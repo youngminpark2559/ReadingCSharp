5c6ba94ca4899c40e60cb8fe9d434ca73d92404e
youngmtool <youngmtool@gmail.com>
2017-11-01 13:57:48 +0900

    Add unit test Can_Add_Quantity_For_Existing_Lines() to test if the functionality of adding orders on existing containing products CartLine by incrementing the number of the products works well

--- CSharpWeb/SportsStore/SportsStore.Tests/CartTests.cs
+++ CSharpWeb/SportsStore/SportsStore.Tests/CartTests.cs
@@ -4,6 +4,8 @@ using Xunit;
 
 //c Add unit test Can_Add_New_Lines() to test if functionality of adding orders into new CartLine and Cart, works well.
 
+//c Add unit test Can_Add_Quantity_For_Existing_Lines() to test if the functionality of adding orders on existing containing products CartLine by incrementing the number of the products works well.
+
 namespace SportsStore.Tests
 {
     public class CartTests
@@ -26,6 +28,30 @@ namespace SportsStore.Tests
             Assert.Equal(2, results.Length);
             Assert.Equal(p1, results[0].Product);
             Assert.Equal(p2, results[1].Product);
+        }
+
+
+
+        [Fact]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            // Arrange - create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            // Arrange - create a new cart
+            Cart target = new Cart();
+
+            // Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            CartLine[] results = target.Lines
+                .OrderBy(c => c.Product.ProductID).ToArray();
+
+            // Assert
+            Assert.Equal(2, results.Length);
+            Assert.Equal(11, results[0].Quantity);
+            Assert.Equal(1, results[1].Quantity);
         }
     }
 }
