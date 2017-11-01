5f0d0a5eacde2b4c8668c3b83f923729a16233f6
youngmtool <youngmtool@gmail.com>
2017-11-01 14:18:53 +0900

    Add UrlExtensions.cs and define PathAndQuery() extension method which extracts querystring from HttpRequest object and combines path and querystring and returns that string to caller

---
+++ CSharpWeb/SportsStore/SportsStore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+//c Add UrlExtensions.cs and define PathAndQuery() extension method which extracts querystring from HttpRequest object and combines path and querystring and returns that string to caller.
+
+namespace SportsStore.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        public static string PathAndQuery(this HttpRequest request) =>
+            request.QueryString.HasValue
+                ? $"{request.Path}{request.QueryString}"
+                : request.Path.ToString();
+    }
+}
