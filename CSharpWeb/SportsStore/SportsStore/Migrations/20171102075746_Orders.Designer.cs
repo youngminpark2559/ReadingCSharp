bf4f602aa2ab9da24a5d765104c1f02623b5a769
youngmtool <youngmtool@gmail.com>
2017-11-02 17:02:05 +0900

    Add IOrderRepository

--- CSharpWeb/SportsStore/SportsStore/Migrations/20171102075746_Orders.Designer.cs
+++ CSharpWeb/SportsStore/SportsStore/Migrations/20171102075746_Orders.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SportsStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171102075746_Orders")]
+    partial class Orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
