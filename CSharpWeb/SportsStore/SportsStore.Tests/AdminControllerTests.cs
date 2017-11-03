b5e03332cbe559aead966486789346d7d9709250
youngmtool <youngmtool@gmail.com>
2017-11-03 15:30:28 +0900

    Update AdminController with adding 2 tests(Can_Edit_Product(), Cannot_Edit_Nonexistent_Product())

--- CSharpWeb/SportsStore/SportsStore.Tests/AdminControllerTests.cs
+++ CSharpWeb/SportsStore/SportsStore.Tests/AdminControllerTests.cs
@@ -36,5 +36,46 @@ namespace SportsStore.Tests
         {
             return (result as ViewResult)?.ViewData.Model as T;
         }
+
+
+        [Fact]
+        public void Can_Edit_Product()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+        new Product {ProductID = 1, Name = "P1"},
+        new Product {ProductID = 2, Name = "P2"},
+        new Product {ProductID = 3, Name = "P3"},
+            }.AsQueryable<Product>());
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+            // Act
+            Product p1 = GetViewModel<Product>(target.Edit(1));
+            Product p2 = GetViewModel<Product>(target.Edit(2));
+            Product p3 = GetViewModel<Product>(target.Edit(3));
+            // Assert
+            Assert.Equal(1, p1.ProductID);
+            Assert.Equal(2, p2.ProductID);
+            Assert.Equal(3, p3.ProductID);
+        }
+
+        [Fact]
+        public void Cannot_Edit_Nonexistent_Product()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+        new Product {ProductID = 1, Name = "P1"},
+        new Product {ProductID = 2, Name = "P2"},
+        new Product {ProductID = 3, Name = "P3"},
+             }.AsQueryable<Product>());
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+            // Act
+            Product result = GetViewModel<Product>(target.Edit(4));
+            // Assert
+            Assert.Null(result);
+        }
     }
 }
