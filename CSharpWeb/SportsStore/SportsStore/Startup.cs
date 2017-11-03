537b38ff36582d84154f0faa5af0f667b0a9f419
youngmtool <youngmtool@gmail.com>
2017-11-03 17:14:17 +0900

    Update Startup.cs by registering services related to Identity

--- CSharpWeb/SportsStore/SportsStore/Startup.cs
+++ CSharpWeb/SportsStore/SportsStore/Startup.cs
@@ -9,10 +9,13 @@ using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 //c Add code for registering Repository service. Each component in MVC is recommended to be loosely coupled each other. In this case, controller and Repository layer should be loosely coupled. For this, FakeProductRepository object which is needed by controller is supplied by DI container, by stating code like this - services.AddTransient<IProductRepository, FakeProductRepository>();
 
 //c Update ConfigureServices() by invoking AddMemoryCache(), AddSession(), UseSession(), for implementing Session system which stores user's cart detail with using session state, in this example on server memory(There are different ways to store session state). 1)Adding services : AddMemoryCache() -  Sets up the in-memory data store.AddSession() - Registers the service which is used to access session data. 2)Adding middleware : UseSession() - Allows the session system to automatically associate requests with sessions when they arrive from the client.
+
+//c Update Startup.cs by registering services related to Identity.
 
 namespace SportsStore
 {
@@ -31,6 +34,14 @@ namespace SportsStore
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
             //services.AddTransient<IProductRepository, FakeProductRepository>();
+
+
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseSqlServer(Configuration["Data:SportStoreIdentity:ConnectionString"]));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddTransient<IOrderRepository, EFOrderRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
@@ -84,7 +95,7 @@ namespace SportsStore
                 productPage = 1
             });
 
-            routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
 
 
             });
