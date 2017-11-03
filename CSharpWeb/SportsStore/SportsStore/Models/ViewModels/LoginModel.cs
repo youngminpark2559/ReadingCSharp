f71048647b77b7f3c02780386e2bbb1eb80f24cd
youngmtool <youngmtool@gmail.com>
2017-11-03 18:13:22 +0900

    Add LoginModel.cs

---
+++ CSharpWeb/SportsStore/SportsStore/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+//c Add LoginModel.cs 
+
+namespace SportsStore.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
