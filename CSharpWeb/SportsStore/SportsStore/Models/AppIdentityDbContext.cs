df9540e00e4505000d8581c64484db0307fd63e0
youngmtool <youngmtool@gmail.com>
2017-11-03 16:56:13 +0900

    Add AppIdentityDbContext.cs

---
+++ CSharpWeb/SportsStore/SportsStore/Models/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+//c Add AppIdentityDbContext.cs
+
+namespace SportsStore.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+            : base(options) { }
+    }
+}
