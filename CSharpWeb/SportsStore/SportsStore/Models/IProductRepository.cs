d71414182ad06ee72a4c86e46b038f10585ac9ef
youngmtool <youngmtool@gmail.com>
2017-11-03 15:36:16 +0900

    Add SaveProduct() in IProductRepository

--- CSharpWeb/SportsStore/SportsStore/Models/IProductRepository.cs
+++ CSharpWeb/SportsStore/SportsStore/Models/IProductRepository.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 
 //c Add IProductRepository.cs which consists of Repository layer which plays a role of logic for storing and retrieving date from persistence data store, in conjunction with classes which implement IProductRepository.cs.
 
+//c Add SaveProduct() in IProductRepository.
+
 namespace SportsStore.Models
 {
     public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
+        void SaveProduct(Product product);
     }
 }
