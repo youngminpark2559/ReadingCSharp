eb4468c728cce01fa67e1fce86352d16dd68dd96
youngmtool <youngmtool@gmail.com>
2017-11-03 15:41:22 +0900

    Update EFProductRepository with adding SaveProduct(). This method gets Product object as a parameter. This method does either it adds a Product object into DbSet and DB or it updates values of the object by new values

--- CSharpWeb/SportsStore/SportsStore/Models/EFProductRepository.cs
+++ CSharpWeb/SportsStore/SportsStore/Models/EFProductRepository.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+
+//c Update EFProductRepository with adding SaveProduct(). This method gets Product object as a parameter. This method does either it adds a Product object into DbSet and DB or it updates values of the object by new values.
+
 namespace SportsStore.Models
 {
     public class EFProductRepository : IProductRepository
@@ -12,5 +15,28 @@ namespace SportsStore.Models
         }
 
         public IQueryable<Product> Products => context.Products;
+
+
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = context.Products
+                    .FirstOrDefault(p => p.ProductID == product.ProductID);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+            context.SaveChanges();
+        }
     }
 }
