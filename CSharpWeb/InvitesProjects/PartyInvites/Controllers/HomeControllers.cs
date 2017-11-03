88f65b48616dbaa59c825d8bb5eb5b2962f93d3b
youngmtool <youngmtool@gmail.com>
2017-11-03 23:22:05 +0900

    Add HomeController.cs

---
+++ CSharpWeb/InvitesProjects/PartyInvites/Controllers/HomeControllers.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
+
+//c Add HomeController.cs
+
+namespace PartyInvites.Controllers {
+    public class HomeController : Controller {
+        private IRepository repository;
+        public HomeController(IRepository repo) =>
+            this.repository = repo;
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm() => View();
+        
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            if (ModelState.IsValid) {
+                repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else {
+                // there is a validation error
+                return View();
+            }
+        }
+        
+        public ViewResult ListResponses() =>
+            View(repository.Responses.Where(r => r.WillAttend == true));
+    }
+}
