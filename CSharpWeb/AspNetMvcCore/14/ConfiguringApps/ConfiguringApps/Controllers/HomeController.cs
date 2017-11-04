d5bb615f8b07adf371719500063e930e716fa7b3
youngmtool <youngmtool@gmail.com>
2017-11-04 16:57:32 +0900

    Update HomeController by configuring HomeController to accept UptimeService object from Startup service provider and assign that service object to the field

--- CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using ConfiguringApps.Infrastructure;
 
 //c Add HomeController
+//c Update HomeController by configuring HomeController to accept UptimeService object from Startup service provider and assign that service object to the field.
+
 
 namespace ConfiguringApps.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() => View(new Dictionary<string, string>
-        {
-            ["Message"] = "This is the Index action"
-        });
+        private UptimeService uptime;
+
+        public HomeController(UptimeService up) => uptime = up;
+
+        public ViewResult Index()
+            => View(new Dictionary<string, string>
+            {
+                ["Message"] = "This is the Index action",
+                ["Uptime"] = $"{uptime.Uptime}ms"
+            });
     }
 }
