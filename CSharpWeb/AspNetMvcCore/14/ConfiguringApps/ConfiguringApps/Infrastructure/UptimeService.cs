639d73a45d8656c0571781929030a8a1faf8e3f9
youngmtool <youngmtool@gmail.com>
2017-11-04 16:47:31 +0900

    Add UptimeService.cs in Infrastructure folder

---
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Infrastructure/UptimeService.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+//c Add UptimeService.cs in Infrastructure folder.
+
+namespace ConfiguringApps.Infrastructure
+{
+    public class UptimeService
+    {
+        private Stopwatch timer;
+        public UptimeService()
+        {
+            timer = Stopwatch.StartNew();
+        }
+        public long Uptime => timer.ElapsedMilliseconds;
+    }
+}
