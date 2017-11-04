f209aa5b46fbcbdfef72c077052c23db65d78275
youngmtool <youngmtool@gmail.com>
2017-11-04 17:34:16 +0900

    Add ShortCircuitMiddleware.cs which will play a role of one middleware component which intercepts the request before the ContentMiddleware gets the request

---
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Infrastructure/ShortCircuitMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+//c Add ShortCircuitMiddleware.cs which will play a role of one middleware component which intercepts the request before the ContentMiddleware gets the request.
+
+namespace ConfiguringApps.Infrastructure
+{
+    public class ShortCircuitMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public ShortCircuitMiddleware(RequestDelegate next) => nextDelegate = next;
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Headers["User-Agent"]
+                    .Any(h => h.ToLower().Contains("edge")))
+            {
+                httpContext.Response.StatusCode = 403;
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+}
