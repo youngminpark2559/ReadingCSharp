7c28371b764b730e6eaed131d9c4e645aa03bb0a
youngmtool <youngmtool@gmail.com>
2017-11-04 16:51:37 +0900

    Update Startup.cs by adding service UptimeService which starts Stopwatch when UptimeService class is instantiated and ends Stopwatch when the application is ended

--- CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Startup.cs
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Startup.cs
@@ -6,12 +6,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using ConfiguringApps.Infrastructure;
+
+//c Update Startup.cs by adding service UptimeService which starts Stopwatch when UptimeService class is instantiated and ends Stopwatch when the application is ended.
+
 namespace ConfiguringApps
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<UptimeService>();
             services.AddMvc();
         }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
