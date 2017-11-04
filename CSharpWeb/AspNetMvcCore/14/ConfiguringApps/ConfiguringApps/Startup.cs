37faff92812294890740b3c75b6ff4f966642d68
youngmtool <youngmtool@gmail.com>
2017-11-04 18:32:28 +0900

    Update Startup.cs by adding code to register app.UseMiddleware<BrowserTypeMiddleware>()

--- CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Startup.cs
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Startup.cs
@@ -11,7 +11,7 @@ using ConfiguringApps.Infrastructure;
 //c Update Startup.cs by adding service UptimeService which starts Stopwatch when UptimeService class is instantiated and ends Stopwatch when the application is ended.
 //c Update Startup.cs by adding one middleware component app.UseMiddleware<ContentMiddleware>() to examine how middleware works.
 //c Update Startup.cs by adding one middleware component app.UseMiddleware<ShortCircuitMiddleware>() which intercepts Http request from the client, and inspects HttpContext object if it contains "edge" in their Http request header and this either hands it to next middleware component or send 404 error contained in HttpContext.Response.StatusCode.
-
+//c Update Startup.cs by adding code to register app.UseMiddleware<BrowserTypeMiddleware>().
 
 namespace ConfiguringApps
 {
@@ -25,6 +25,7 @@ namespace ConfiguringApps
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             //app.UseMvcWithDefaultRoute();
+            app.UseMiddleware<BrowserTypeMiddleware>();
             app.UseMiddleware<ShortCircuitMiddleware>();
             app.UseMiddleware<ContentMiddleware>();
         }
