93f2a796ca05b2bfbb953fd8ee186e48514f8841
youngmtool <youngmtool@gmail.com>
2017-11-04 17:15:20 +0900

    Add ContentMiddleware.cs which is used to configure one middleware component generating content

---
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Infrastructure/ContentMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+//c Add ContentMiddleware.cs which is used to configure one middleware component generating content.
+
+namespace ConfiguringApps.Infrastructure
+{
+    public class ContentMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public ContentMiddleware(RequestDelegate next) => nextDelegate = next;
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.ToString().ToLower() == "/middleware")
+            {
+                await httpContext.Response
+                    .WriteAsync("This is from the content middleware", Encoding.UTF8);
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+}
