9a2ed2c3ba07534dcaf64c866a25d83a89bd0e03
youngmtool <youngmtool@gmail.com>
2017-11-04 13:48:07 +0900

    Add HomeController

---
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+//c Add HomeController
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View(new Dictionary<string, string>
+        {
+            ["Message"] = "This is the Index action"
+        });
+    }
+}
