35bcfa4336ac9553833c8dd6b131fd5a1374d44b
youngmtool <youngmtool@gmail.com>
2017-11-04 20:57:50 +0900

    Update HomeController by editing Index(), adding Error(). UseExceptionHandler middleware brings the flow to HomeController\Error(), and this action method brings the flow to the Index() with adding descriptive message into Dictionary object

--- CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using ConfiguringApps.Infrastructure;
 
 //c Add HomeController
 //c Update HomeController by configuring HomeController to accept UptimeService object from Startup service provider and assign that service object to the field.
-
+//c Update HomeController by editing Index(), adding Error(). UseExceptionHandler middleware brings the flow to HomeController\Error(), and this action method brings the flow to the Index() with adding descriptive message into Dictionary object.
 
 namespace ConfiguringApps.Controllers
 {
@@ -14,11 +14,22 @@ namespace ConfiguringApps.Controllers
 
         public HomeController(UptimeService up) => uptime = up;
 
-        public ViewResult Index()
-            => View(new Dictionary<string, string>
+        public ViewResult Index(bool throwException = false)
+        {
+            if (throwException)
+            {
+                throw new System.NullReferenceException();
+            }
+            return View(new Dictionary<string, string>
             {
                 ["Message"] = "This is the Index action",
                 ["Uptime"] = $"{uptime.Uptime}ms"
+            });
+        }
+        public ViewResult Error() => View(nameof(Index),
+            new Dictionary<string, string>
+            {
+                ["Message"] = "This is the Error action"
             });
     }
 }
