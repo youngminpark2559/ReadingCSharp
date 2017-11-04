60f2e15be9580207e856f1b9b4d3095db1b3943e
youngmtool <youngmtool@gmail.com>
2017-11-04 18:20:13 +0900

    Add BrowserTypeMiddleware.cs which inspects HttpContext object. If this object contains "edge" on Headers property, set Items property true value with EdgeBrowser key, and then hands this manipulated object to next middleware component

---
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Infrastructure/BrowserTypeMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+//c Add BrowserTypeMiddleware.cs which inspects HttpContext object. If this object contains "edge" on Headers property, set Items property true value with EdgeBrowser key, and then hands this manipulated object to next middleware component.
+
+
+namespace ConfiguringApps.Infrastructure
+{
+    public class BrowserTypeMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public BrowserTypeMiddleware(RequestDelegate next) => nextDelegate = next;
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Items["EdgeBrowser"] =
+            httpContext.Request.Headers["User-Agent"]
+                    .Any(v => v.ToLower().Contains("edge"));
+            await nextDelegate.Invoke(httpContext);
+        }
+    }
+}
