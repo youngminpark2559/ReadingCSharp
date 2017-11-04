9a3782d39cc38942fe5e53543726265fd62ccc35
youngmtool <youngmtool@gmail.com>
2017-11-04 17:31:19 +0900

    Update ContentMiddleware.cs adding UptimeService object as a argument of ContentMiddleware constructor and the object will be passed into here by service provider of Startup.cs. This means that services can be provided not only for controller but also any part of the application

--- CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Infrastructure/ContentMiddleware.cs
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Infrastructure/ContentMiddleware.cs
@@ -3,21 +3,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
 //c Add ContentMiddleware.cs which is used to configure one middleware component generating content.
+//c Update ContentMiddleware.cs adding UptimeService object as a argument of ContentMiddleware constructor and the object will be passed into here by service provider of Startup.cs. This means that services can be provided not only for controller but also any part of the application.
 
 namespace ConfiguringApps.Infrastructure
 {
     public class ContentMiddleware
     {
         private RequestDelegate nextDelegate;
+        private UptimeService uptime;
 
-        public ContentMiddleware(RequestDelegate next) => nextDelegate = next;
+        public ContentMiddleware(RequestDelegate next, UptimeService up)
+        {
+            nextDelegate = next;
+            uptime = up;
+        }
 
         public async Task Invoke(HttpContext httpContext)
         {
             if (httpContext.Request.Path.ToString().ToLower() == "/middleware")
             {
                 await httpContext.Response
-                    .WriteAsync("This is from the content middleware", Encoding.UTF8);
+                    .WriteAsync("This is from the content middleware " +
+                    $"(uptime: {uptime.Uptime}ms)", Encoding.UTF8);
             }
             else
             {
