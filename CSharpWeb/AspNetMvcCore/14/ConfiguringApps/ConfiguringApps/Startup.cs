cfb00957f79270d38ea9cac8839121739fe457bb
youngmtool <youngmtool@gmail.com>
2017-11-06 16:21:28 +0900

    I add ConfigureDevelopmentServices(), ConfigureDevelopment(), and update Configure(). These methods are suffixed by Development which means that these methods are for development environment. These are needed because even if the different configuration files are useful, they can't contain C# codes such as method.

--- CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Startup.cs
+++ CSharpWeb/AspNetMvcCore/14/ConfiguringApps/ConfiguringApps/Startup.cs
@@ -17,7 +17,7 @@ using Microsoft.Extensions.Configuration;
 //c Update Startup.cs by adding code to register app.UseMvc() which sets the middleware components for MVC system, including routing system. To complete MVC system work well, not only are MVC middleware componenets needed, but also services for MVC system are needed. It can be resolved by adding services.AddMvc() in ConfigureService(IServiceCollection services).
 //c Update Startup.cs by removing middleware components which were used to examine middleware and adding exception-handling middlewares.
 //c I invoke AddMvcOptions() additionally after AddMvc(). This followed method can customize options in MVC by manipulating the default behavior of MVC which is acted by rules defined by AddMvc().
-
+//c I add ConfigureDevelopmentServices(), ConfigureDevelopment(), and update Configure(). These methods are suffixed by Development which means that these methods are for development environment. These are needed because even if the different configuration files are useful, they can't contain C# codes such as method.
 
 namespace ConfiguringApps
 {
@@ -38,31 +38,31 @@ namespace ConfiguringApps
                 options.RespectBrowserAcceptHeader = true;
             });
         }
+        
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddSingleton<UptimeService>();
+            services.AddMvc();
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if ((Configuration.GetSection("ShortCircuitMiddleware")?.
-                GetValue<bool>("EnableBrowserShortCircuit")).Value)
-            {
-                app.UseMiddleware<BrowserTypeMiddleware>();
-                app.UseMiddleware<ShortCircuitMiddleware>();
-            }
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseStatusCodePages();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            app.UseExceptionHandler("/Home/Error");
             app.UseStaticFiles();
             app.UseMvc(routes => {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+        public void ConfigureDevelopment(IApplicationBuilder app,
+                IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseBrowserLink();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
