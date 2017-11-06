4b763e4b9d355e7a3731e6e311717018455e7943
youngmtool <youngmtool@gmail.com>
2017-11-06 17:08:55 +0900

    Add Result.cs model class.

---
+++ CSharpWeb/AspNetMvcCore/15/UrlsAndRoutes/Models/Result.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+//c Add Result.cs model class.
+
+namespace UrlsAndRoutes.Models
+{
+    public class Result
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public IDictionary<string, object> Data { get; }
+            = new Dictionary<string, object>();
+    }
+}
