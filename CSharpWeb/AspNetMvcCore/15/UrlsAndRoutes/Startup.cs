f3e6c97aeb6305a0abbdf839c83a35a4c3745690
youngmtool <youngmtool@gmail.com>
2017-11-06 18:48:45 +0900

    There are 2 ways to define default value for MapRoute(). One is using 3rd argument named default. The other one is inserting into 2nd argument named template.

--- CSharpWeb/AspNetMvcCore/15/UrlsAndRoutes/Startup.cs
+++ CSharpWeb/AspNetMvcCore/15/UrlsAndRoutes/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 //c I additionally pass 3rd parameter which is named defaults. With this configuration, when there is no specific action in URL, Index is used as default action method.
+//c There are 2 ways to define default value for MapRoute(). One is using 3rd argument named default. The other one is inserting into 2nd argument named template.
 
 namespace UrlsAndRoutes
 {
@@ -25,10 +26,10 @@ namespace UrlsAndRoutes
             app.UseMvc(routes => {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller}/{action}",
-                    defaults: new { action = "Index" });
+                    template: "{controller}/{action=Index}");
+                //defaults: new { action = "Index" });
             });
-            
+                       
         }
     }
 }
