64f229d0efaa1b73ca89eaad98a4c9be7c1ccdee
youngmtool <youngmtool@gmail.com>
2017-11-07 18:01:35 +0900

    Add other way of setting attribute route. With [Route("[controller]/MyAction")], I can access to this action by Customer/MyAction

--- CSharpWeb/AspNetMvcCore/15/UrlsAndRoutes/Controllers/CustomerController.cs
+++ CSharpWeb/AspNetMvcCore/15/UrlsAndRoutes/Controllers/CustomerController.cs
@@ -3,12 +3,14 @@ using UrlsAndRoutes.Models;
 
 //c Update List()
 //c Add attribute route [Route("myroute")] on Index(). With this setting, I can access like this, /Customer/List for List(), /myroute for Index(). 
+//c Add other way of setting attribute route. With [Route("[controller]/MyAction")], I can access to this action by Customer/MyAction
 
 namespace UrlsAndRoutes.Controllers
 {
     public class CustomerController : Controller
     {
-        [Route("myroute")]
+        //[Route("myroute")]
+        [Route("[controller]/MyAction")]
         public ViewResult Index() => View("Result",
             new Result
             {
