f865b98dbf709e2367f15d70ae864bcebad45221
youngmtool <youngmtool@gmail.com>
2017-11-07 13:17:21 +0900

    I use null coalescing operator for the case that id is null passed from URL. When the id is null, I assign a specific words(<no value>) into Result object.

--- CSharpWeb/AspNetMvcCore/15/UrlsAndRoutes/Controllers/HomeController.cs
+++ CSharpWeb/AspNetMvcCore/15/UrlsAndRoutes/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using UrlsAndRoutes.Models;
 
 //c I use id parameter in CustomVariable(). I already set URL mapping template to {controller=Home}/{action=Index}/{id=DefaultId}. So, whatever client input in id segment, this is passed into id parameter of CustomVariable().
+//c I use null coalescing operator for the case that id is null passed from URL. When the id is null, I assign a specific words(<no value>) into Result object.
 
 namespace UrlsAndRoutes.Controllers
 {
@@ -21,7 +22,9 @@ namespace UrlsAndRoutes.Controllers
                 Controller = nameof(HomeController),
                 Action = nameof(CustomVariable),
             };
-            r.Data["Id"] = id;
+
+            //r.Data["Id"] = id;
+            r.Data["Id"] = id ?? "<no value>";
             return View("Result", r);
         }
     }
