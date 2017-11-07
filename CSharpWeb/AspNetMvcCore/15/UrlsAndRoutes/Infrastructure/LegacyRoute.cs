dc1fcf2629728e5a188fe7263019087dea140396
youngmtool <youngmtool@gmail.com>
2017-11-07 19:51:13 +0900

    Add LegacyRoute.cs which implements IRouter, to create a custom route class.

---
+++ CSharpWeb/AspNetMvcCore/15/UrlsAndRoutes/Infrastructure/LegacyRoute.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add LegacyRoute.cs which implements IRouter, to create a custom route class.
+
+namespace UrlsAndRoutes.Infrastructure
+{
+    public class LegacyRoute : IRouter
+    {
+        private string[] urls;
+
+        public LegacyRoute(params string[] targetUrls)
+        {
+            this.urls = targetUrls;
+        }
+
+        public Task RouteAsync(RouteContext context)
+        {
+            string requestedUrl = context.HttpContext.Request.Path
+                .Value.TrimEnd('/');
+            if (urls.Contains(requestedUrl, StringComparer.OrdinalIgnoreCase))
+            {
+                context.Handler = async ctx => {
+                    HttpResponse response = ctx.Response;
+                    byte[] bytes = Encoding.ASCII.GetBytes($"URL: {requestedUrl}");
+                    await response.Body.WriteAsync(bytes, 0, bytes.Length);
+                };
+            }
+            return Task.CompletedTask;
+        }
+
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            return null;
+        }
+    }
+}
