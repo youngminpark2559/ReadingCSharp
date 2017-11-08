e0370cde2c14c91ac2bdc7e44f23e1cd19c32fd7
youngmtool <youngmtool@gmail.com>
2017-11-08 19:08:21 +0900

    The first way of retrieving data from the form, by using HttpRequest.Form property.

---
+++ CSharpWeb/AspNetMvcCore/17/ControllersAndActions/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+//c The first way of retrieving data from the form, by using HttpRequest.Form property.
+
+namespace ControllersAndActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("SimpleForm");
+        
+        public ViewResult ReceiveForm()
+        {
+            var name = Request.Form["name"];
+            var city = Request.Form["city"];
+            return View("Result", $"{name} lives in {city}");
+        }
+    }
+}
