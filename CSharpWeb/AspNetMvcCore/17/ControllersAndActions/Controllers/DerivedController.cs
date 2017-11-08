2408028674c0ade632be81e13315126f7635af59
youngmtool <youngmtool@gmail.com>
2017-11-08 17:52:51 +0900

    Add Headers(). I use Request property of Controller base class which returns HttpRequest object. This object contains informations of http request from client web browser. I convert informations of header to dictionary and send this object to DictionaryResult view. And I display this object in the web page by view file.

--- CSharpWeb/AspNetMvcCore/17/ControllersAndActions/Controllers/DerivedController.cs
+++ CSharpWeb/AspNetMvcCore/17/ControllersAndActions/Controllers/DerivedController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 //c Add DerivedController.cs which inherits Controller base class including many features of it.
+//c Add Headers(). I use Request property of Controller base class which returns HttpRequest object. This object contains informations of http request from client web browser. I convert informations of header to dictionary and send this object to DictionaryResult view. And I display this object in the web page by view file.
 
 namespace ControllersAndActions.Controllers
 {
@@ -8,5 +10,9 @@ namespace ControllersAndActions.Controllers
     {
         public ViewResult Index() =>
             View("Result", $"This is a derived controller");
+
+        public ViewResult Headers() => View("DictionaryResult",
+                Request.Headers.ToDictionary(kvp => kvp.Key,
+                    kvp => kvp.Value.First()));
     }
 }
