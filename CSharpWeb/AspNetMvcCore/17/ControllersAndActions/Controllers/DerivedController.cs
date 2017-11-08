5825ffd11aacf5ffcf644b90d8ff2b00da0aa385
youngmtool <youngmtool@gmail.com>
2017-11-08 17:19:18 +0900

    Add DerivedController.cs which inherits Controller base class including many features of it.

---
+++ CSharpWeb/AspNetMvcCore/17/ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+//c Add DerivedController.cs which inherits Controller base class including many features of it.
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public ViewResult Index() =>
+            View("Result", $"This is a derived controller");
+    }
+}
