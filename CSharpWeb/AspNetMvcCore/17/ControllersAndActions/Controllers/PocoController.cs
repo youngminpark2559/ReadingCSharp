e960c8ca741316a938b7a75c643969219ea13407
youngmtool <youngmtool@gmail.com>
2017-11-08 18:32:26 +0900

    Add property which is ControllerContext type and decorated by [ControllerContext]. This property provides the features which are provided by MVC API.

--- CSharpWeb/AspNetMvcCore/17/ControllersAndActions/Controllers/PocoController.cs
+++ CSharpWeb/AspNetMvcCore/17/ControllersAndActions/Controllers/PocoController.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Linq;
 
 //c Using MVC API in PocoController.cs.
+//c Add property which is ControllerContext type and decorated by [ControllerContext]. This property provides the features which are provided by MVC API.
 
 namespace ControllersAndActions.Controllers
 {
     public class PocoController
     {
+
+        [ControllerContext]
+        public ControllerContext ControllerContext { get; set; }
+
         public ViewResult Index() => new ViewResult()
         {
             ViewName = "Result",
@@ -18,5 +24,19 @@ namespace ControllersAndActions.Controllers
                 Model = $"This is a POCO controller"
             }
         };
+
+        public ViewResult Headers() =>
+            new ViewResult()
+            {
+                ViewName = "DictionaryResult",
+                ViewData = new ViewDataDictionary(
+                    new EmptyModelMetadataProvider(),
+                    new ModelStateDictionary())
+                {
+                    Model = ControllerContext.HttpContext.Request.Headers
+                        .ToDictionary(kvp => kvp.Key, kvp => kvp.Value.First())
+                }
+            };
+
     }
 }
