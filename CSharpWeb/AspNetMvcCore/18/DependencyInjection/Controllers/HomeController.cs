ec050ad7f6e10ad34927ea837d6ce99a78b67248
youngmtool <youngmtool@gmail.com>
2017-11-09 15:59:02 +0900

    Decouple between MemoryRepository class and HomeController class. I instantiate MemoryRepository object and assgin it into Repository property which is IRepository type. Visual architecture is like this. HomeController -- [IRepository -- MemoryRepository].

---
+++ CSharpWeb/AspNetMvcCore/18/DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        //c Decouple between MemoryRepository class and HomeController class. I instantiate MemoryRepository object and assgin it into Repository property which is IRepository type. Visual architecture is like this. HomeController -- [IRepository -- MemoryRepository].
+        public IRepository Repository { get; set; } = new MemoryRepository();
+        public ViewResult Index() => View(Repository.Products);
+    }
+}
---
+++ CSharpWeb/AspNetMvcCore/18/DependencyInjection/Models/MemoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace DependencyInjection.Models
+{
+    public class MemoryRepository : IRepository
+    {
+        private Dictionary<string, Product> products;
+        public MemoryRepository()
+        {
+            products = new Dictionary<string, Product>();
+            new List<Product> {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer ball", Price = 19.50M }
+            }.ForEach(p => AddProduct(p));
+        }
+        public IEnumerable<Product> Products => products.Values;
+        public Product this[string name] => products[name];
+        public void AddProduct(Product product) =>
+            products[product.Name] = product;
+        public void DeleteProduct(Product product) =>
+            products.Remove(product.Name);
+    }
+}
