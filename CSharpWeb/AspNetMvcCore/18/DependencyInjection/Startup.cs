1e8a6969411ba36f2f9679cc098fd06e3b30997c
youngmtool <youngmtool@gmail.com>
2017-11-09 16:31:47 +0900

    Set TypeBroker's type by invoking SetRepositoryType<AlternateRepository>().

--- CSharpWeb/AspNetMvcCore/18/DependencyInjection/Startup.cs
+++ CSharpWeb/AspNetMvcCore/18/DependencyInjection/Startup.cs
@@ -6,12 +6,19 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using DependencyInjection.Infrastructure;
+using DependencyInjection.Models;
+
+//c Set TypeBroker's type by invoking SetRepositoryType<AlternateRepository>().
+
 namespace DependencyInjection
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            //
+            TypeBroker.SetRepositoryType<AlternateRepository>();
             services.AddMvc();
         }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
