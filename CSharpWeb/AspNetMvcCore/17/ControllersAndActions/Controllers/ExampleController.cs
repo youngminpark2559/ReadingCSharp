da2e5ca97d080fb58e361443fb540f2ffcbf89a6
youngmtool <youngmtool@gmail.com>
2017-11-09 12:38:47 +0900

    Add ExampleController.cs

---
+++ CSharpWeb/AspNetMvcCore/17/ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index() => View(DateTime.Now);
+    }
+}
--- CSharpWeb/AspNetMvcCore/17/ControllersAndActions/Controllers/HomeController.cs
+++ CSharpWeb/AspNetMvcCore/17/ControllersAndActions/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControllersAndActions.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using System.Text;
 
 //c The first way of retrieving data from the form, by using HttpRequest.Form property.
@@ -18,13 +19,22 @@ namespace ControllersAndActions.Controllers
         //    return View("Result", $"{name} lives in {city}");
         //}
         
-        public void ReceiveForm(string name, string city)
-        {
-            Response.StatusCode = 200;
-            Response.ContentType = "text/html";
-            byte[] content = Encoding.ASCII
-                .GetBytes($"<html><body>{name} lives in {city}</body>");
-            Response.Body.WriteAsync(content, 0, content.Length);
-        }
+        //public void ReceiveForm(string name, string city)
+        //{
+        //    Response.StatusCode = 200;
+        //    Response.ContentType = "text/html";
+        //    byte[] content = Encoding.ASCII
+        //        .GetBytes($"<html><body>{name} lives in {city}</body>");
+        //    Response.Body.WriteAsync(content, 0, content.Length);
+        //}
+
+        //public IActionResult ReceiveForm(string name, string city)
+        //    => new CustomHtmlResult
+        //    {
+        //        Content = $"{name} lives in {city}"
+        //    };
+
+        public ViewResult ReceiveForm(string name, string city)
+            => View("Result", $"{name} lives in {city}");
     }
 }
