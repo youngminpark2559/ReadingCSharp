7d240e9a34b0b8476a1b4142eba2b6c321f1be4a
youngmtool <youngmtool@gmail.com>
2017-11-10 21:15:54 +0900

    Add RangeExceptionAttribute.cs which is used as exception filter. If the kind of exception which is verified from ExceptionContext object is ArgumentOutOfRangeException, then I create a ViewResult object which contains data.

--- CSharpWeb/AspNetMvcCore/19/Filters/Controllers/HomeController.cs
+++ CSharpWeb/AspNetMvcCore/19/Filters/Controllers/HomeController.cs
@@ -9,11 +9,12 @@ using Microsoft.AspNetCore.Mvc;
 //c Apply custom action filter to HomeController. I can apply the action filter for action method and controller.
 //c Apply a custom result filter to HomeController.
 //c By applying [ViewResultDetails] result filter, I can add data into response object aside from string message(This is..).
-//c Apply a custom hybrid action/result filter by using [Profile].
+//c Apply a custom hybrid action/result filter by using [Profile] and [ViewResultDetails].
 
 namespace Filters.Controllers
 {
     [Profile]
+    [ViewResultDetails]
     public class HomeController : Controller
     {
         public ViewResult Index() => View("Message", "This is the Index action on the Home controller");
---
+++ CSharpWeb/AspNetMvcCore/19/Filters/Infrastructure/RangeExceptionAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+//c Add RangeExceptionAttribute.cs which is used as exception filter. If the kind of exception which is verified from ExceptionContext object is ArgumentOutOfRangeException, then I create a ViewResult object which contains data.
+
+namespace Filters.Infrastructure
+{
+    public class RangeExceptionAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is ArgumentOutOfRangeException)
+            {
+                context.Result = new ViewResult()
+                {
+                    ViewName = "Message",
+                    ViewData = new ViewDataDictionary(
+                        new EmptyModelMetadataProvider(),
+                        new ModelStateDictionary())
+                    {
+                        Model = @"The data received by the
+                                application cannot be processed"
+                    }
+                };
+            }
+        }
+    }
+}
