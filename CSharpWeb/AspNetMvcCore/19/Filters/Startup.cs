9f82a81c38830326c7cb597bc69a3798dc754246
youngmtool <youngmtool@gmail.com>
2017-11-10 21:55:28 +0900

    Resolve filter dependancy.

--- CSharpWeb/AspNetMvcCore/19/Filters/Startup.cs
+++ CSharpWeb/AspNetMvcCore/19/Filters/Startup.cs
@@ -6,12 +6,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Filters.Infrastructure;
+
+//c Resolve filter dependancy.
+
 namespace Filters
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IFilterDiagnostics, DefaultFilterDiagnostics>();
             services.AddMvc();
         }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
