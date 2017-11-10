6c2a51d97bc47d3d6d6fa6f22fb30649af5cb4c6
youngmtool <youngmtool@gmail.com>
2017-11-10 19:05:05 +0900

    Add ProfileAttribute class to create [Profile] action filter.

---
+++ CSharpWeb/AspNetMvcCore/19/Filters/Infrastructure/ProfileAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+//c Add ProfileAttribute class to create [Profile] action filter.
+
+namespace Filters.Infrastructure
+{
+    public class ProfileAttribute : ActionFilterAttribute
+    {
+        private Stopwatch timer;
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            timer = Stopwatch.StartNew();
+        }
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            timer.Stop();
+            string result = "<div>Elapsed time: " + $"{timer.Elapsed.TotalMilliseconds} ms</div>";
+            byte[] bytes = Encoding.ASCII.GetBytes(result);
+            context.HttpContext.Response.Body.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
