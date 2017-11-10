98f671ba79069cc12f3b82f3cc45f871f5cd5c6d
youngmtool <youngmtool@gmail.com>
2017-11-10 16:33:33 +0900

    Apply [RequireHttps] filter to HomeController.

--- CSharpWeb/AspNetMvcCore/19/Filters/Controllers/HomeController.cs
+++ CSharpWeb/AspNetMvcCore/19/Filters/Controllers/HomeController.cs
@@ -3,15 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 
 //c I verify if the request is from https or not through Request.IsHttps.
 //c Apply [RequireHttps] filter to action method.
+//c Apply [RequireHttps] filter to HomeController.
 
 namespace Filters.Controllers
 {
+    [RequireHttps]
     public class HomeController : Controller
     {
-        [RequireHttps]
         public ViewResult Index() => View("Message", "This is the Index action on the Home controller");
 
-        [RequireHttps]
         public ViewResult SecondAction() => View("Message", "This is the SecondAction action on the Home controller");
     }
 }
