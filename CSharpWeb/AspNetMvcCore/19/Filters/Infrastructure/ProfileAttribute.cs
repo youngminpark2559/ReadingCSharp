b9057b98562e4ee95d5da1c884691cd7d5a46228
youngmtool <youngmtool@gmail.com>
2017-11-10 19:21:30 +0900

    Update ProfileAttribute.cs by using asynchronous way for action filter. ActionExecutionDelegate object represents action method to execute or the next filter to execute.

--- CSharpWeb/AspNetMvcCore/19/Filters/Infrastructure/ProfileAttribute.cs
+++ CSharpWeb/AspNetMvcCore/19/Filters/Infrastructure/ProfileAttribute.cs
@@ -1,25 +1,25 @@
 ﻿using System.Diagnostics;
 using System.Text;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
 
 //c Add ProfileAttribute class to create [Profile] action filter.
+//c Update ProfileAttribute.cs by using asynchronous way for action filter. ActionExecutionDelegate object represents action method to execute or the next filter to execute.
 
 namespace Filters.Infrastructure
 {
     public class ProfileAttribute : ActionFilterAttribute
     {
-        private Stopwatch timer;
-
-        public override void OnActionExecuting(ActionExecutingContext context)
+        public override async Task OnActionExecutionAsync(
+                ActionExecutingContext context,
+                ActionExecutionDelegate next)
         {
-            timer = Stopwatch.StartNew();
-        }
-        public override void OnActionExecuted(ActionExecutedContext context)
-        {
+            Stopwatch timer = Stopwatch.StartNew();
+            await next();
             timer.Stop();
             string result = "<div>Elapsed time: " + $"{timer.Elapsed.TotalMilliseconds} ms</div>";
             byte[] bytes = Encoding.ASCII.GetBytes(result);
-            context.HttpContext.Response.Body.Write(bytes, 0, bytes.Length);
+            await context.HttpContext.Response.Body.WriteAsync(bytes, 0, bytes.Length);
         }
     }
 }
