2902556f6694121e19c1c375e9fa19db2d2222db
youngmtool <youngmtool@gmail.com>
2017-11-10 17:44:36 +0900

    Add custom filter, HttpsOnlyAttribute, which can be used by [HttpsOnly].

---
+++ CSharpWeb/AspNetMvcCore/19/Filters/Infrastructure/HttpsOnlyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+//c Add custom filter, HttpsOnlyAttribute, which can be used by [HttpsOnly].
+
+namespace Filters.Infrastructure
+{
+    public class HttpsOnlyAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.Request.IsHttps)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+        }
+    }
+}
