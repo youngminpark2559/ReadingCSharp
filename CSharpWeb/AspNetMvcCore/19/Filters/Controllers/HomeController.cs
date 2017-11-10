f6baab3c2112d4e7e192e4cc6ca6ab913558af44
youngmtool <youngmtool@gmail.com>
2017-11-10 16:03:34 +0900

    I verify if the request is from https or not through Request.IsHttps.

---
+++ CSharpWeb/AspNetMvcCore/19/Filters/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+//c I verify if the request is from https or not through Request.IsHttps.
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            if (!Request.IsHttps)
+            {
+                return new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+            else
+            {
+                return View("Message", "This is the Index action on the Home controller");
+            }
+        }
+    }
+}
