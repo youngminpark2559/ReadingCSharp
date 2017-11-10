ae4e1a05335dda3948fb11eb8d88077905bf1fc9
youngmtool <youngmtool@gmail.com>
2017-11-10 21:48:28 +0900

    Add FilterDiagnostics.cs which cotains IFilterDiagnostics and its implementation, DefaultFilterDiagnostics class.

---
+++ CSharpWeb/AspNetMvcCore/19/Filters/Infrastructure/FilterDiagnostics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+//c Add FilterDiagnostics.cs which cotains IFilterDiagnostics and its implementation, DefaultFilterDiagnostics class.
+
+namespace Filters.Infrastructure
+{
+    public interface IFilterDiagnostics
+    {
+        IEnumerable<string> Messages { get; }
+        void AddMessage(string message);
+    }
+
+    public class DefaultFilterDiagnostics : IFilterDiagnostics
+    {
+        private List<string> messages = new List<string>();
+        public IEnumerable<string> Messages => messages;
+        public void AddMessage(string message) =>
+            messages.Add(message);
+    }
+}
