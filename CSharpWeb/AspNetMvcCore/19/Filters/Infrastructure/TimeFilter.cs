aa62842a1bcc04b8b3403b66da64e294dfdef663
youngmtool <youngmtool@gmail.com>
2017-11-11 19:50:56 +0900

    Add project to examine API controller, RESTful controller.

---
+++ CSharpWeb/AspNetMvcCore/19/Filters/Infrastructure/TimeFilter.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+namespace Filters.Infrastructure
+{
+    public class TimeFilter : IAsyncActionFilter, IAsyncResultFilter
+    {
+        private Stopwatch timer;
+        private IFilterDiagnostics diagnostics;
+        public TimeFilter(IFilterDiagnostics diags)
+        {
+            diagnostics = diags;
+        }
+        public async Task OnActionExecutionAsync(
+                ActionExecutingContext context,
+                ActionExecutionDelegate next)
+        {
+            timer = Stopwatch.StartNew();
+            await next();
+            diagnostics.AddMessage($@"Action time:
+                {timer.Elapsed.TotalMilliseconds}");
+        }
+        public async Task OnResultExecutionAsync(
+                ResultExecutingContext context,
+                ResultExecutionDelegate next)
+        {
+            await next();
+            timer.Stop();
+            diagnostics.AddMessage($@"Result time:
+                {timer.Elapsed.TotalMilliseconds}");
+        }
+    }
+}
