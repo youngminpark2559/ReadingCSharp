203fe12f5a70ae1f6dfd42dc501265696d86e598
youngmtool <youngmtool@gmail.com>
2017-11-19 21:49:37 +0900

    Update ListAllAssembliesInAppDomain() method to use LINQ API. Since you use LINQ API, this application loads additional assemblies for LINQ API. You can check out by this updated method.

--- CSharp/AnPhCs6Net46/17/DefaultAppDomainApp/Program.cs
+++ CSharp/AnPhCs6Net46/17/DefaultAppDomainApp/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 //c Get a default AppDomain object. Get informations of the default AppDomain.
 //c Run the application. CLR loads assemblies and get the process from the OS. CLR generates the default AppDomain in the process. Get the current default AppDomain. Get all loaded assemblies in the current default AppDomain. Print informations in each assembly.
+//c Update ListAllAssembliesInAppDomain() method to use LINQ API. Since you use LINQ API, this application loads additional assemblies for LINQ API. You can check out by this updated method.
 
 namespace DefaultAppDomainApp
 {
@@ -32,10 +33,12 @@ namespace DefaultAppDomainApp
             AppDomain defaultAD = AppDomain.CurrentDomain;
 
             // Now get all loaded assemblies in the default AppDomain.
-            Assembly[] loadedAssemblies = defaultAD.GetAssemblies();
+            var loadedAssemblies = from a in defaultAD.GetAssemblies()
+                                   orderby a.GetName().Name
+                                   select a;
             Console.WriteLine("***** Here are the assemblies loaded in {0} *****\n",
-              defaultAD.FriendlyName);
-            foreach (Assembly a in loadedAssemblies)
+      defaultAD.FriendlyName);
+            foreach (var a in loadedAssemblies)
             {
                 Console.WriteLine("-> Name: {0}", a.GetName().Name);
                 Console.WriteLine("-> Version: {0}\n", a.GetName().Version);
