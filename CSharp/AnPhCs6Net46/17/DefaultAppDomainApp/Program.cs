141a8e23bbd20ad30b1fe28a48ed7bfb869e54c8
youngmtool <youngmtool@gmail.com>
2017-11-19 21:45:11 +0900

    Run the application. CLR loads assemblies and get the process from the OS. CLR generates the default AppDomain in the process. Get the current default AppDomain. Get all loaded assemblies in the current default AppDomain. Print informations in each assembly.

--- CSharp/AnPhCs6Net46/17/DefaultAppDomainApp/Program.cs
+++ CSharp/AnPhCs6Net46/17/DefaultAppDomainApp/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 //c Get a default AppDomain object. Get informations of the default AppDomain.
+//c Run the application. CLR loads assemblies and get the process from the OS. CLR generates the default AppDomain in the process. Get the current default AppDomain. Get all loaded assemblies in the current default AppDomain. Print informations in each assembly.
 
 namespace DefaultAppDomainApp
 {
@@ -24,10 +26,27 @@ namespace DefaultAppDomainApp
             Console.WriteLine("Base directory of this domain: {0}", defaultAD.BaseDirectory);
         }
 
+        static void ListAllAssembliesInAppDomain()
+        {
+            // Get access to the AppDomain for the current thread.
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+
+            // Now get all loaded assemblies in the default AppDomain.
+            Assembly[] loadedAssemblies = defaultAD.GetAssemblies();
+            Console.WriteLine("***** Here are the assemblies loaded in {0} *****\n",
+              defaultAD.FriendlyName);
+            foreach (Assembly a in loadedAssemblies)
+            {
+                Console.WriteLine("-> Name: {0}", a.GetName().Name);
+                Console.WriteLine("-> Version: {0}\n", a.GetName().Version);
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with the default AppDomain *****\n");
-            DisplayDADStats();
+            //DisplayDADStats();
+            ListAllAssembliesInAppDomain();
             Console.ReadLine();
         }
     }
