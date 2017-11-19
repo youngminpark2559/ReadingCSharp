ceeb77df666157f02a80c2682357c9c914ad1ec8
youngmtool <youngmtool@gmail.com>
2017-11-19 23:12:33 +0900

    Use AssemblyLoad event which occurs when a new assembly has been loaded into a given AppDomain. I set event handler for the event by using lambda expression. This event handler using lambda expression can point any method taking a System.Object as the first parameter and an AssemblyLoadEventArgs as the second.

--- CSharp/AnPhCs6Net46/17/DefaultAppDomainApp/Program.cs
+++ CSharp/AnPhCs6Net46/17/DefaultAppDomainApp/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 //c Get a default AppDomain object. Get informations of the default AppDomain.
 //c Run the application. CLR loads assemblies and get the process from the OS. CLR generates the default AppDomain in the process. Get the current default AppDomain. Get all loaded assemblies in the current default AppDomain. Print informations in each assembly.
 //c Update ListAllAssembliesInAppDomain() method to use LINQ API. Since you use LINQ API, this application loads additional assemblies for LINQ API. You can check out by this updated method.
+//c Use AssemblyLoad event which occurs when a new assembly has been loaded into a given AppDomain. I set event handler for the event by using lambda expression. This event handler using lambda expression can point any method taking a System.Object as the first parameter and an AssemblyLoadEventArgs as the second.
 
 namespace DefaultAppDomainApp
 {
@@ -45,11 +46,24 @@ namespace DefaultAppDomainApp
             }
         }
 
+        private static void InitDAD()
+        {
+            // This logic will print out the name of any assembly
+            // loaded into the applicaion domain, after it has been
+            // created.
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+            defaultAD.AssemblyLoad += (o, s) =>
+            {
+                Console.WriteLine("{0} has been loaded!", s.LoadedAssembly.GetName().Name);
+            };
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with the default AppDomain *****\n");
             //DisplayDADStats();
             ListAllAssembliesInAppDomain();
+            InitDAD();
             Console.ReadLine();
         }
     }
