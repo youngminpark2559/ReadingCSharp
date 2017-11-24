b7cf91efc500ebcf71c4b811f59c8151f8d25195
youngmtool <youngmtool@gmail.com>
2017-11-24 17:12:15 +0900

    Add a project. I make BinaryOp delegate to execute method asynchronously by using BeginInvoke(), EndInvoke().

---
+++ CSharp/AnPhCs6Net46/19/AsyncDelegate/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+//c Add a project. I make BinaryOp delegate to execute method asynchronously by using BeginInvoke(), EndInvoke().
+
+namespace AsyncDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Async Delegate Invocation *****");
+
+            // Print out the ID of the executing thread.
+            Console.WriteLine("Main() invoked on thread {0}.",
+              Thread.CurrentThread.ManagedThreadId);
+
+            // Invoke Add() on a secondary thread.
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult iftAR = b.BeginInvoke(10, 10, null, null);
+
+            // Do other work on primary thread...
+            Console.WriteLine("Doing more work in Main()!");
+
+            // Obtain the result of the Add()
+            // method when ready.
+            int answer = b.EndInvoke(iftAR);
+            Console.WriteLine("10 + 10 is {0}.", answer);
+            Console.ReadLine();
+        }
+
+
+        static int Add(int x, int y)
+        {
+            // Print out the ID of the executing thread.
+            Console.WriteLine("Add() invoked on thread {0}.",
+              Thread.CurrentThread.ManagedThreadId);
+
+            // Pause to simulate a lengthy operation.
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
