862aa24700ef8813ade79a30cb2633bb9f5ae230
youngmtool <youngmtool@gmail.com>
2017-11-24 20:00:19 +0900

    Improve the features

--- CSharp/AnPhCs6Net46/19/AsyncDelegate/Program.cs
+++ CSharp/AnPhCs6Net46/19/AsyncDelegate/Program.cs
@@ -25,12 +25,18 @@ namespace AsyncDelegate
             BinaryOp b = new BinaryOp(Add);
             IAsyncResult iftAR = b.BeginInvoke(10, 10, null, null);
 
-            // Do other work on primary thread...
-            Console.WriteLine("Doing more work in Main()!");
 
-            // Obtain the result of the Add()
-            // method when ready.
+            // This message will keep printing until
+            // the Add() method is finished.
+            while (!iftAR.IsCompleted)
+            {
+                Console.WriteLine("Doing more work in Main()!");
+                Thread.Sleep(1000);
+            }
+            // Now we know the Add() method is complete.
             int answer = b.EndInvoke(iftAR);
+
+                      
             Console.WriteLine("10 + 10 is {0}.", answer);
             Console.ReadLine();
         }
