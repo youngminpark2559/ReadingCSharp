8a2b97f2d99ef684ceaa9758b96cdc0d9978fe23
youngmtool <youngmtool@gmail.com>
2017-11-25 20:43:53 +0900

    Do some foreach tasks on a single primary thread, so everything is hung because the primary thread is fully taken by the running task.

--- CSharp/AnPhCs6Net46/19/DataParallelismWithForEach/MainForm.Designer.cs
+++ CSharp/AnPhCs6Net46/19/DataParallelismWithForEach/MainForm.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnProcessImages = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(678, 216);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnProcessImages
+            // 
+            this.btnProcessImages.Location = new System.Drawing.Point(12, 539);
+            this.btnProcessImages.Name = "btnProcessImages";
+            this.btnProcessImages.Size = new System.Drawing.Size(75, 23);
+            this.btnProcessImages.TabIndex = 1;
+            this.btnProcessImages.Text = "button1";
+            this.btnProcessImages.UseVisualStyleBackColor = true;
+            this.btnProcessImages.Click += new System.EventHandler(this.btnProcessImages_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 574);
+            this.Controls.Add(this.btnProcessImages);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnProcessImages;
     }
 }
 
--- CSharp/AnPhCs6Net46/19/DataParallelismWithForEach/MainForm.cs
+++ CSharp/AnPhCs6Net46/19/DataParallelismWithForEach/MainForm.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+//c Do some foreach tasks on a single primary thread, so everything is hung because the primary thread is fully taken by the running task.
 
 namespace DataParallelismWithForEach
 {
@@ -15,6 +19,36 @@ namespace DataParallelismWithForEach
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnProcessImages_Click(object sender, EventArgs e)
+        {
+            ProcessFiles();
+        }
+
+        private void ProcessFiles()
+        {
+            // Load up all *.jpg files, and make a new folder for the modified data.
+            string[] files = Directory.GetFiles
+              (@"C:\TestPictures", "*.jpg", SearchOption.AllDirectories);
+            string newDir = @"C:\ModifiedPictures";
+            Directory.CreateDirectory(newDir);
+
+            // Process the image data in a blocking manner.
+            foreach (string currentFile in files)
+            {
+                string filename = Path.GetFileName(currentFile);
+
+                using (Bitmap bitmap = new Bitmap(currentFile))
+                {
+                    bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    bitmap.Save(Path.Combine(newDir, filename));
+
+                    // Print out the ID of the thread processing the current image.
+                    this.Text = string.Format("Processing {0} on thread {1}", filename,
+                      Thread.CurrentThread.ManagedThreadId);
+                }
+            }
         }
     }
 }
