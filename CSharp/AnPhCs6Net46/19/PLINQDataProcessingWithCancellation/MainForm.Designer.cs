8445a5ec3d063ad9a110db8bc0e1ae5482bbc5c6
youngmtool <youngmtool@gmail.com>
2017-11-26 08:44:51 +0900

    Add a project PLINQDataProcessingWithCancellation to examine how to use PLINQ. By the PLINQ, I can implement parallel programming by LINQ query by simply adding AsParallel() or WithCancellation() extension methods at the end of the source.

---
+++ CSharp/AnPhCs6Net46/19/PLINQDataProcessingWithCancellation/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PLINQDataProcessingWithCancellation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(36, 186);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 0;
+            this.btnExecute.Text = "button1";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(165, 185);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "button2";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnExecute);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ CSharp/AnPhCs6Net46/19/PLINQDataProcessingWithCancellation/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//c Add a project PLINQDataProcessingWithCancellation to examine how to use PLINQ. By the PLINQ, I can implement parallel programming by LINQ query by simply adding AsParallel() or WithCancellation() extension methods at the end of the source.
+
+namespace PLINQDataProcessingWithCancellation
+{
+    public partial class MainForm : Form
+    {
+        private CancellationTokenSource cancelToken = new CancellationTokenSource();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessIntData()
+        {
+            // Get a very large array of integers.
+            int[] source = Enumerable.Range(1, 10000000).ToArray();
+
+            // Find the numbers where num % 3 == 0 is true, returned
+            // in descending order.
+            int[] modThreeIsZero = null;
+            try
+            {
+                modThreeIsZero = (from num in source.AsParallel().WithCancellation(cancelToken.Token)
+                                  where num % 3 == 0
+                                  orderby num descending
+                                  select num).ToArray();
+                MessageBox.Show(string.Format("Found {0} numbers that match query!", modThreeIsZero.Count()));
+            }
+            catch (OperationCanceledException ex)
+            {
+                this.Invoke((Action)delegate
+                {
+                    this.Text = ex.Message;
+                });
+            }
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            // Start a new "task" to process the ints.
+            Task.Factory.StartNew(() =>
+            {
+                ProcessIntData();
+            });
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            cancelToken.Cancel();
+        }
+    }
+}
