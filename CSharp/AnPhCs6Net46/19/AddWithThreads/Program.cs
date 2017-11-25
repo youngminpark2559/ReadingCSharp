c3784f0f9db46dd993785061f15fe8c70c1cc289
youngmtool <youngmtool@gmail.com>
2017-11-25 09:23:42 +0900

    Add a AddParams class which contains data which will be passed into Add() method. Add Add(object data) static method which will be pass into the ParameterizedThreadStart delegate.

--- CSharp/AnPhCs6Net46/19/AddWithThreads/Program.cs
+++ CSharp/AnPhCs6Net46/19/AddWithThreads/Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 //c Add a project AddWithThreads to examine the feature of ParameterizedThreadStart delegate.
+//c Add a AddParams class which contains data which will be passed into Add() method. Add Add(object data) static method which will be pass into the ParameterizedThreadStart delegate. 
 
 namespace AddWithThreads
 {
@@ -19,10 +21,36 @@ namespace AddWithThreads
         }
     }
 
+
+
     class Program
     {
+        static void Add(object data)
+        {
+            if (data is AddParams)
+            {
+                Console.WriteLine("ID of thread in Add(): {0}",
+                  Thread.CurrentThread.ManagedThreadId);
+
+                AddParams ap = (AddParams)data;
+                Console.WriteLine("{0} + {1} is {2}", ap.a, ap.b, ap.a + ap.b);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Adding with Thread objects *****");
+            Console.WriteLine("ID of thread in Main(): {0}",
+              Thread.CurrentThread.ManagedThreadId);
+
+            // Make an AddParams object to pass to the secondary thread.
+            AddParams ap = new AddParams(10, 10);
+            Thread t = new Thread(new ParameterizedThreadStart(Add));
+            t.Start(ap);
+            // Force a wait to let other thread finish.
+            Thread.Sleep(3);
+
+            Console.ReadLine();
         }
     }
 }
