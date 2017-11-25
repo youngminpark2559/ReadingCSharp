1c56569d35e54f809f0a3b0c17cb0267e977805b
youngmtool <youngmtool@gmail.com>
2017-11-25 23:33:01 +0900

    Implement functionalities but in a single thread and synchronous way.

--- CSharp/AnPhCs6Net46/19/MyEBookReader/Form1.cs
+++ CSharp/AnPhCs6Net46/19/MyEBookReader/Form1.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 //c Implement btnDownload_Click() event handler method. I download text file asynchronously and when this task is completed, DownloadStringCompleted event is fired and event handler is invoked so downloaded text is assigned to theEBook variable and this variable is assigned ti text box.
+//c Implement functionalities but in a single thread and synchronous way.
 
 namespace MyEBookReader
 {
@@ -36,7 +37,42 @@ namespace MyEBookReader
 
         private void btnGetStats_Click(object sender, EventArgs e)
         {
+            // Get the words from the e-book.
+            string[] words = theEBook.Split(new char[]
+              { ' ', '\u000A', ',', '.', ';', ':', '-', '?', '/' },
+              StringSplitOptions.RemoveEmptyEntries);
 
+            // Now, find the ten most common words.
+            string[] tenMostCommon = FindTenMostCommon(words);
+            // Get the longest word.
+            string longestWord = FindLongestWord(words);
+
+            // Now that all tasks are complete, build a string to show all
+            // stats in a message box.
+            StringBuilder bookStats = new StringBuilder("Ten Most Common Words are:\n");
+            foreach (string s in tenMostCommon)
+            {
+                bookStats.AppendLine(s);
+            }
+
+            bookStats.AppendFormat("Longest word is: {0}", longestWord);
+            bookStats.AppendLine();
+            MessageBox.Show(bookStats.ToString(), "Book info");
+        }
+
+        private string[] FindTenMostCommon(string[] words)
+        {
+            var frequencyOrder = from word in words
+                                 where word.Length > 6
+                                 group word by word into g
+                                 orderby g.Count() descending
+                                 select g.Key;
+            string[] commonWords = (frequencyOrder.Take(10)).ToArray();
+            return commonWords;
+        }
+        private string FindLongestWord(string[] words)
+        {
+            return (from w in words orderby w.Length descending select w).FirstOrDefault();
         }
     }
 }
