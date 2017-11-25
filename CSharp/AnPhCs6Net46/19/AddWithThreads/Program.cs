488514c8998c98890cd5fd36c2578a34c8e9d432
youngmtool <youngmtool@gmail.com>
2017-11-25 09:16:41 +0900

    Add a project AddWithThreads to examine the feature of ParameterizedThreadStart delegate.

---
+++ CSharp/AnPhCs6Net46/19/AddWithThreads/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a project AddWithThreads to examine the feature of ParameterizedThreadStart delegate.
+
+namespace AddWithThreads
+{
+    class AddParams
+    {
+        public int a, b;
+
+        public AddParams(int numb1, int numb2)
+        {
+            a = numb1;
+            b = numb2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
