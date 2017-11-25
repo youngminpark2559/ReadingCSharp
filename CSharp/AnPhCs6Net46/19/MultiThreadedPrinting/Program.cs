50640d183fe591ab208dc0c06bb558628c8d079d
youngmtool <youngmtool@gmail.com>
2017-11-25 12:05:20 +0900

    Add a project MultiThreadedPrinting to examine the concurrency occurred when multiple threads attempt to manipulate a shared data.

---
+++ CSharp/AnPhCs6Net46/19/MultiThreadedPrinting/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+//c Add a project MultiThreadedPrinting to examine the concurrency occurred when multiple threads attempt to manipulate a shared data.
+
+namespace MultiThreadedPrinting
+{
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            // Display Thread info.
+            Console.WriteLine("-> {0} is executing PrintNumbers()",
+              Thread.CurrentThread.Name);
+
+            // Print out numbers.
+            for (int i = 0; i < 10; i++)
+            {
+                // Put thread to sleep for a random amount of time.
+                Random r = new Random();
+                Thread.Sleep(1000 * r.Next(5));
+                Console.Write("{0}, ", i);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Synchronizing Threads *****\n");
+
+            Printer p = new Printer();
+
+            // Make 10 threads that are all pointing to the same
+            // method on the same object.
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] =
+                  new Thread(new ThreadStart(p.PrintNumbers));
+                threads[i].Name = string.Format("Worker thread #{0}", i);
+            }
+            // Now start each one.
+            foreach (Thread t in threads)
+                t.Start();
+            Console.ReadLine();
+        }
+
+
+    }
+}
+}
