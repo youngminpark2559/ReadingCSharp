ce7127a4919efbc81e908a370363dc3fef95f62a
youngmtool <youngmtool@gmail.com>
2017-11-25 15:26:48 +0900

    Implement Main() method to use TimerCallback delegate.

--- CSharp/AnPhCs6Net46/19/TimerApp/Program.cs
+++ CSharp/AnPhCs6Net46/19/TimerApp/Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 //c Add a project TimerApp to examine the delegate TimerCallback.
+//c Implement Main() method to use TimerCallback delegate.
 
 namespace TimerApp
 {
@@ -18,6 +20,20 @@ namespace TimerApp
 
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Working with Timer type *****\n");
+
+            // Create the delegate for the Timer type.
+            TimerCallback timeCB = new TimerCallback(PrintTime);
+
+            // Establish timer settings.
+            Timer t = new Timer(
+              timeCB,     // The TimerCallback delegate object.
+              null,       // Any info to pass into the called method (null for no info).
+              0,          // Amount of time to wait before starting (in milliseconds).
+              1000);      // Interval of time between calls (in milliseconds).
+
+            Console.WriteLine("Hit key to terminate...");
+            Console.ReadLine();
         }
     }
 }
