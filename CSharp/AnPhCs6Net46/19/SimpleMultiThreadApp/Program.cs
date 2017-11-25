80a94eb2e0998bf2dc71c08e225a3ed840326935
youngmtool <youngmtool@gmail.com>
2017-11-25 09:08:50 +0900

    Update Main() method. I invoke the PrintNumbers() method either on the primary thread or on the secondary thread in conformity of the input of the user.

--- CSharp/AnPhCs6Net46/19/SimpleMultiThreadApp/Program.cs
+++ CSharp/AnPhCs6Net46/19/SimpleMultiThreadApp/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 //c Add a project SimpleMultiThreadApp to examine the multi-threading programming by using features of System.Thread namespace.
+//c Update Main() method. I invoke the PrintNumbers() method either on the primary thread or on the secondary thread in conformity of the input of the user.
 
 namespace SimpleMultiThreadApp
 {
@@ -32,6 +34,40 @@ namespace SimpleMultiThreadApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** The Amazing Thread App *****\n");
+            Console.Write("Do you want [1] or [2] threads? ");
+            string threadCount = Console.ReadLine();
+
+            // Name the current thread.
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "Primary";
+
+            // Display Thread info.
+            Console.WriteLine("-> {0} is executing Main()",
+            Thread.CurrentThread.Name);
+
+            // Make worker class.
+            Printer p = new Printer();
+
+            switch (threadCount)
+            {
+                case "2":
+                    // Now make the thread.
+                    Thread backgroundThread =
+                      new Thread(new ThreadStart(p.PrintNumbers));
+                    backgroundThread.Name = "Secondary";
+                    backgroundThread.Start();
+                    break;
+                case "1":
+                    p.PrintNumbers();
+                    break;
+                default:
+                    Console.WriteLine("I don't know what you want...you get 1 thread.");
+                    goto case "1";
+            }
+            // Do some additional work.
+            MessageBox.Show("I'm busy!", "Work on main thread...");
+            Console.ReadLine();
         }
     }
 }
