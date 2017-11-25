d4f70e190b082bae6388a47bd016e90fb99ff3df
youngmtool <youngmtool@gmail.com>
2017-11-25 09:01:08 +0900

    Add a project SimpleMultiThreadApp to examine the multi-threading programming by using features of System.Thread namespace.

---
+++ CSharp/AnPhCs6Net46/19/SimpleMultiThreadApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+//c Add a project SimpleMultiThreadApp to examine the multi-threading programming by using features of System.Thread namespace.
+
+namespace SimpleMultiThreadApp
+{
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            // Display Thread info.
+            Console.WriteLine("-> {0} is executing PrintNumbers()",
+              Thread.CurrentThread.Name);
+
+            // Print out numbers.
+            Console.Write("Your numbers: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0}, ", i);
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CSharp/AnPhCs6Net46/19/ThreadStats/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadStats
+{
+    class Program
+    {
+        // Be sure to import the System.Threading namespace.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Primary Thread stats *****\n");
+
+            // Obtain and name the current thread.
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "ThePrimaryThread";
+
+            // Show details of hosting AppDomain/Context.
+            Console.WriteLine("Name of current AppDomain: {0}",
+              Thread.GetDomain().FriendlyName);
+            Console.WriteLine("ID of current Context: {0}",
+              Thread.CurrentContext.ContextID);
+
+            // Print out some stats about this thread.
+            Console.WriteLine("Thread Name: {0}",
+              primaryThread.Name);
+            Console.WriteLine("Has thread started?: {0}",
+              primaryThread.IsAlive);
+            Console.WriteLine("Priority Level: {0}",
+              primaryThread.Priority);
+            Console.WriteLine("Thread State: {0}",
+              primaryThread.ThreadState);
+            Console.ReadLine();
+        }
+    }
+}
