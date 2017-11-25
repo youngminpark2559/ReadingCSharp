fc99bf42fff6b7d4e95c128e177cfbe49f0c702f
youngmtool <youngmtool@gmail.com>
2017-11-25 15:22:29 +0900

    Add a project TimerApp to examine the delegate TimerCallback.

---
+++ CSharp/AnPhCs6Net46/19/TimerApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a project TimerApp to examine the delegate TimerCallback.
+
+namespace TimerApp
+{
+    class Program
+    {
+        static void PrintTime(object state)
+        {
+            Console.WriteLine("Time is: {0}",
+              DateTime.Now.ToLongTimeString());
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
