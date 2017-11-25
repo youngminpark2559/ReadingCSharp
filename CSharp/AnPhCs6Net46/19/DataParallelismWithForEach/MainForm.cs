2906972037daf1fa4a7832f65cf61c7676c48ba9
youngmtool <youngmtool@gmail.com>
2017-11-25 22:55:52 +0900

    Update ProcessFiles() method to implement the functionality of suspending to task executing. For this, I use ParallelOptions object to store cancelToken I created via field. And I pass this configured ParallelOptions object to ForEach() method. And inside of this method, I implement ThrowIfCancellationRequested() to be invoked if I click cancel button on the UI.

--- CSharp/AnPhCs6Net46/19/DataParallelismWithForEach/MainForm.cs
+++ CSharp/AnPhCs6Net46/19/DataParallelismWithForEach/MainForm.cs
@@ -15,6 +15,8 @@ using System.Windows.Forms;
 //c Use Task class. This class allows you to invoke a method on a secondary thread. And this class also can be used as an alternative to working with asynchronous delegate. By this code, ProcessFiles() method is invoke on the secondary thread.
 //c Add cancel token field which will be used for suspending parallel task in the middle of executing.
 //c Add btnCancel_Click() event handler method. When I click the cancel button, it leads to invoking Cancel() method on cancelToken object and this tells all the worker threads to stop.
+//c Update ProcessFiles() method to implement the functionality of suspending to task executing. For this, I use ParallelOptions object to store cancelToken I created via field. And I pass this configured ParallelOptions object to ForEach() method. And inside of this method, I implement ThrowIfCancellationRequested() to be invoked if I click cancel button on the UI.
+
 
 namespace DataParallelismWithForEach
 {
@@ -40,38 +42,54 @@ namespace DataParallelismWithForEach
 
         private void ProcessFiles()
         {
+            // Use ParallelOptions instance to store the CancellationToken.
+            ParallelOptions parOpts = new ParallelOptions();
+            parOpts.CancellationToken = cancelToken.Token;
+            parOpts.MaxDegreeOfParallelism = System.Environment.ProcessorCount;
+
             // Load up all *.jpg files, and make a new folder for the modified data.
             string[] files = Directory.GetFiles
               (@"C:\TestPictures", "*.jpg", SearchOption.AllDirectories);
             string newDir = @"C:\ModifiedPictures";
             Directory.CreateDirectory(newDir);
 
-            // Process the image data in a parallel manner!
-            Parallel.ForEach(files, currentFile =>
+
+            try
             {
-                string filename = Path.GetFileName(currentFile);
-
-                using (Bitmap bitmap = new Bitmap(currentFile))
+                // Process the image data in a parallel manner!
+                Parallel.ForEach(files, parOpts, currentFile =>
                 {
-                    bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
-                    bitmap.Save(Path.Combine(newDir, filename));
+                    parOpts.CancellationToken.ThrowIfCancellationRequested();
 
-                    // Eek! This will not work anymore!
-                    //this.Text = string.Format("Processing {0} on thread {1}", filename,
-                    //  Thread.CurrentThread.ManagedThreadId);
-
-                    // Invoke on the Form object, to allow secondary threads to access controls
-                    // in a thread-safe manner.
-                    this.Invoke((Action)delegate
+                    string filename = Path.GetFileName(currentFile);
+                    using (Bitmap bitmap = new Bitmap(currentFile))
                     {
-                        this.Text = string.Format("Processing {0} on thread {1}", filename,
-                                    Thread.CurrentThread.ManagedThreadId);
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(Path.Combine(newDir, filename));
+                        this.Invoke((Action)delegate
+                        {
+                            this.Text = string.Format("Processing {0} on thread {1}", filename,
+                              Thread.CurrentThread.ManagedThreadId);
+                        }
+                        );
                     }
-                    );
                 }
+                );
+                this.Invoke((Action)delegate
+                {
+                    this.Text = "Done!";
+                });
             }
-            );
+            catch (OperationCanceledException ex)
+            {
+                this.Invoke((Action)delegate
+                {
+                    this.Text = ex.Message;
+                });
+            }
         }
+
+
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
