5fe1c9b3546d4c0e03848fbffb3cb28863c999ce
youngmtool <youngmtool@gmail.com>
2017-11-27 07:37:08 +0900

    At this mement, I made a textual data in memory, and I copy that textual data in value way and print it on the console window. And I use GetStringBuilder object by invokng StringWriter.GetStringBuilder() method. By using this StringBuilder object, I can insert and remove characters located in the memory by specifying the index numbers flexibly.

--- CSharp/AnPhCs6Net46/20/StringReaderWriterApp/Program.cs
+++ CSharp/AnPhCs6Net46/20/StringReaderWriterApp/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a project StringReaderWriterApp. StringWriter and StringReader classes is treating a textual information as a stream of in-memory character. I use StringWriter object to append character-based information to an underlying in-memory buffer by writing a block of string data to a StringWriter object rather than a physical file on the local hard drive.
+//c At this mement, I made a textual data in memory, and I copy that textual data in value way and print it on the console window. And I use GetStringBuilder object by invokng StringWriter.GetStringBuilder() method. By using this StringBuilder object, I can insert and remove characters located in the memory by specifying the index numbers flexibly.
 
 namespace StringReaderWriterApp
 {
@@ -22,6 +23,13 @@ namespace StringReaderWriterApp
                 // Get a copy of the contents (stored in a string) and dump
                 // to console.
                 Console.WriteLine("Contents of StringWriter:\n{0}", strWriter);
+
+                // Get the internal StringBuilder.
+                StringBuilder sb = strWriter.GetStringBuilder();
+                sb.Insert(0, "Hey!! ");
+                Console.WriteLine("-> {0}", sb.ToString());
+                sb.Remove(0, "Hey!! ".Length);
+                Console.WriteLine("-> {0}", sb.ToString());
             }
             Console.ReadLine();
         }
