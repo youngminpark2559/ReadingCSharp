f8b58156dbf16a93f369d0803d6574be6c2731cf
youngmtool <youngmtool@gmail.com>
2017-11-26 16:10:02 +0900

    Use CreateSubdirectory() method of DirectoryInfo class, to create the folder(s).

--- CSharp/AnPhCs6Net46/20/DirectoryApp/Program.cs
+++ CSharp/AnPhCs6Net46/20/DirectoryApp/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Add a project DirectoryApp to examine DirectoryInfo class and its members.
+//c Use CreateSubdirectory() method of DirectoryInfo class, to create the folder(s).
 
 namespace DirectoryApp
 {
@@ -14,7 +15,9 @@ namespace DirectoryApp
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Directory(Info) *****\n");
-            ShowWindowsDirectoryInfo();
+            //ShowWindowsDirectoryInfo();
+            //DisplayImageFiles();
+            ModifyAppDirectory();
             Console.ReadLine();
         }
 
@@ -30,6 +33,39 @@ namespace DirectoryApp
             Console.WriteLine("Attributes: {0}", dir.Attributes);
             Console.WriteLine("Root: {0}", dir.Root);
             Console.WriteLine("**************************\n");
+        }
+
+        static void DisplayImageFiles()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Windows\Web\Wallpaper");
+            // Get all files with a *.jpg extension.
+            FileInfo[] imageFiles = dir.GetFiles("*.jpg", SearchOption.AllDirectories);
+
+            // How many were found?
+            Console.WriteLine("Found {0} *.jpg files\n", imageFiles.Length);
+
+            // Now print out info for each file.
+            foreach (FileInfo f in imageFiles)
+            {
+                Console.WriteLine("***************************");
+                Console.WriteLine("File name: {0}", f.Name);
+                Console.WriteLine("File size: {0}", f.Length);
+                Console.WriteLine("Creation: {0}", f.CreationTime);
+                Console.WriteLine("Attributes: {0}", f.Attributes);
+                Console.WriteLine("***************************\n");
+            }
+        }
+
+        
+        static void ModifyAppDirectory()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\");
+
+            // Create \MyFolder off application directory.
+            dir.CreateSubdirectory("MyFolder");
+
+            // Create \MyFolder2\Data off application directory.
+            dir.CreateSubdirectory(@"MyFolder2\Data");
         }
     }
 }
