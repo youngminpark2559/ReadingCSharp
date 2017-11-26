921c7315c00584144fc5a24c9e2bf601da4ec7f2
youngmtool <youngmtool@gmail.com>
2017-11-26 10:16:07 +0900

    Use async method with multiple await.

--- CSharp/AnPhCs6Net46/19/FunWithCSharpAsync/MainForm.Designer.cs
+++ CSharp/AnPhCs6Net46/19/FunWithCSharpAsync/MainForm.Designer.cs
@@ -31,11 +31,12 @@
             this.btnCallMethod = new System.Windows.Forms.Button();
             this.txtInput = new System.Windows.Forms.TextBox();
             this.btnVoidMethodCall = new System.Windows.Forms.Button();
+            this.btnMutliAwaits = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCallMethod
             // 
-            this.btnCallMethod.Location = new System.Drawing.Point(12, 142);
+            this.btnCallMethod.Location = new System.Drawing.Point(12, 103);
             this.btnCallMethod.Name = "btnCallMethod";
             this.btnCallMethod.Size = new System.Drawing.Size(165, 23);
             this.btnCallMethod.TabIndex = 0;
@@ -45,14 +46,14 @@
             // 
             // txtInput
             // 
-            this.txtInput.Location = new System.Drawing.Point(77, 45);
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
             this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(100, 21);
+            this.txtInput.Size = new System.Drawing.Size(260, 21);
             this.txtInput.TabIndex = 1;
             // 
             // btnVoidMethodCall
             // 
-            this.btnVoidMethodCall.Location = new System.Drawing.Point(12, 202);
+            this.btnVoidMethodCall.Location = new System.Drawing.Point(12, 145);
             this.btnVoidMethodCall.Name = "btnVoidMethodCall";
             this.btnVoidMethodCall.Size = new System.Drawing.Size(165, 23);
             this.btnVoidMethodCall.TabIndex = 2;
@@ -60,11 +61,22 @@
             this.btnVoidMethodCall.UseVisualStyleBackColor = true;
             this.btnVoidMethodCall.Click += new System.EventHandler(this.btnVoidMethodCall_Click);
             // 
+            // btnMutliAwaits
+            // 
+            this.btnMutliAwaits.Location = new System.Drawing.Point(12, 192);
+            this.btnMutliAwaits.Name = "btnMutliAwaits";
+            this.btnMutliAwaits.Size = new System.Drawing.Size(165, 23);
+            this.btnMutliAwaits.TabIndex = 3;
+            this.btnMutliAwaits.Text = "btnMutliAwaits";
+            this.btnMutliAwaits.UseVisualStyleBackColor = true;
+            this.btnMutliAwaits.Click += new System.EventHandler(this.btnMutliAwaits_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnMutliAwaits);
             this.Controls.Add(this.btnVoidMethodCall);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.btnCallMethod);
@@ -80,6 +92,7 @@
         private System.Windows.Forms.Button btnCallMethod;
         private System.Windows.Forms.TextBox txtInput;
         private System.Windows.Forms.Button btnVoidMethodCall;
+        private System.Windows.Forms.Button btnMutliAwaits;
     }
 }
 
--- CSharp/AnPhCs6Net46/19/FunWithCSharpAsync/MainForm.cs
+++ CSharp/AnPhCs6Net46/19/FunWithCSharpAsync/MainForm.cs
@@ -12,6 +12,8 @@ using System.Windows.Forms;
 //c Add a project FunWithCSharpAsync to examine "async and await" feature to implement multi-threading and asynchronous programming in a simple way. This commit is before applying async and await feature to the project.
 //c Invoke btnCallMethod_Click() event handler method asychronously and inside of that, invoke DoWorkAsync() method on the secondary thread asychronously. This DoWorkAsync() method makes the calling thread(the thread which called DoWorkAsync() method) sleep 4 seconds and return Task<string> to the place which called the DoWorkAsync() method. And on there, by await keyword, it extracts string which is the form to be assigned to this.Text from Task<string>.
 //c Invoke method returning void by async and await feature.
+//c Use async method with multiple await.
+
 namespace FunWithCSharpAsync
 {
     public partial class MainForm : Form
@@ -49,6 +51,18 @@ namespace FunWithCSharpAsync
             { /* Do some work here... */
                 Thread.Sleep(4000);
             });
+        }
+
+        private async void btnMutliAwaits_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => { Thread.Sleep(2000); });
+            MessageBox.Show("Done with first task!");
+
+            await Task.Run(() => { Thread.Sleep(2000); });
+            MessageBox.Show("Done with second task!");
+
+            await Task.Run(() => { Thread.Sleep(2000); });
+            MessageBox.Show("Done with third task!");
         }
     }
 }
