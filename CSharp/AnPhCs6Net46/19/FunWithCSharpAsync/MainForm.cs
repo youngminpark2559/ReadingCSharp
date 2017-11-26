e438851ee598180515c89a985cdbe124a49bcf54
youngmtool <youngmtool@gmail.com>
2017-11-26 09:31:38 +0900

    Invoke btnCallMethod_Click() event handler method asychronously and inside of that, invoke DoWorkAsync() method on the secondary thread asychronously. This DoWorkAsync() method makes the calling thread(the thread which called DoWorkAsync() method) sleep 4 seconds and return Task<string> to the place which called the DoWorkAsync() method. And on there, by await keyword, it extracts string which is the form to be assigned to this.Text from Task<string>.

--- CSharp/AnPhCs6Net46/19/FunWithCSharpAsync/MainForm.cs
+++ CSharp/AnPhCs6Net46/19/FunWithCSharpAsync/MainForm.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 //c Add a project FunWithCSharpAsync to examine "async and await" feature to implement multi-threading and asynchronous programming in a simple way. This commit is before applying async and await feature to the project.
+//c Invoke btnCallMethod_Click() event handler method asychronously and inside of that, invoke DoWorkAsync() method on the secondary thread asychronously. This DoWorkAsync() method makes the calling thread(the thread which called DoWorkAsync() method) sleep 4 seconds and return Task<string> to the place which called the DoWorkAsync() method. And on there, by await keyword, it extracts string which is the form to be assigned to this.Text from Task<string>.
 
 namespace FunWithCSharpAsync
 {
@@ -20,18 +21,19 @@ namespace FunWithCSharpAsync
             InitializeComponent();
         }
 
-        private void btnCallMethod_Click(object sender, EventArgs e)
+        private async void btnCallMethod_Click(object sender, EventArgs e)
         {
-            this.Text = DoWork();
+            this.Text = await DoWorkAsync();
         }
 
-
-        private string DoWork()
+        // See below for code walkthrough...
+        private async Task<string> DoWorkAsync()
         {
-            Thread.Sleep(4000);
-            return "Done with work!";
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(4000);
+                return "Done with work!";
+            });
         }
-
-
     }
 }
