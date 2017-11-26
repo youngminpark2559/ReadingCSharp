e5f0d92713af359d746a425369c78e6b73b2d814
youngmtool <youngmtool@gmail.com>
2017-11-26 09:07:05 +0900

    Add a project FunWithCSharpAsync to examine "async and await" feature to implement multi-threading and asynchronous programming in a simple way. This commit is before applying async and await feature to the project.

---
+++ CSharp/AnPhCs6Net46/19/FunWithCSharpAsync/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace FunWithCSharpAsync
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCallMethod = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCallMethod
+            // 
+            this.btnCallMethod.Location = new System.Drawing.Point(42, 152);
+            this.btnCallMethod.Name = "btnCallMethod";
+            this.btnCallMethod.Size = new System.Drawing.Size(75, 23);
+            this.btnCallMethod.TabIndex = 0;
+            this.btnCallMethod.Text = "button1";
+            this.btnCallMethod.UseVisualStyleBackColor = true;
+            this.btnCallMethod.Click += new System.EventHandler(this.btnCallMethod_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(77, 45);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 21);
+            this.txtInput.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnCallMethod);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCallMethod;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ CSharp/AnPhCs6Net46/19/FunWithCSharpAsync/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//c Add a project FunWithCSharpAsync to examine "async and await" feature to implement multi-threading and asynchronous programming in a simple way. This commit is before applying async and await feature to the project.
+
+namespace FunWithCSharpAsync
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCallMethod_Click(object sender, EventArgs e)
+        {
+            this.Text = DoWork();
+        }
+
+
+        private string DoWork()
+        {
+            Thread.Sleep(4000);
+            return "Done with work!";
+        }
+
+
+    }
+}
