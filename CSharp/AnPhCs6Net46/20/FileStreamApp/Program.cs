7cd91ba580f200e206dc168707016f173d4f3201
youngmtool <youngmtool@gmail.com>
2017-11-26 20:11:50 +0900

    Create a project FileStreamApp to examine the process of encoding a string to byte array represented in int and write this byte array to a file. And then read the file and decode byte array to string and print that on the console.

---
+++ CSharp/AnPhCs6Net46/20/FileStreamApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a project FileStreamApp to examine the process of encoding a string to byte array represented in int and write this byte array to a file. And then read the file and decode byte array to string and print that on the console.
+
+namespace FileStreamApp
+{
+    class Program
+    {
+        // Don't forget to import the System.Text and System.IO namespaces.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with FileStreams *****\n");
+
+            // Obtain a FileStream object.
+            using (FileStream fStream = File.Open(@"C:\myMessage.dat",
+              FileMode.Create))
+            {
+                // Encode a string as an array of bytes.
+                string msg = "Hello!";
+                byte[] msgAsByteArray = Encoding.Default.GetBytes(msg);
+
+                foreach (var a in msgAsByteArray)
+                {
+                    Console.WriteLine($"a: {a}");
+                }
+
+
+
+                // Write byte[] to file.
+                fStream.Write(msgAsByteArray, 0, msgAsByteArray.Length);
+
+                // Reset internal position of stream.
+                fStream.Position = 0;
+
+                // Read the types from file and display to console.
+                Console.Write("Your message as an array of bytes: ");
+                byte[] bytesFromFile = new byte[msgAsByteArray.Length];
+                for (int i = 0; i < msgAsByteArray.Length; i++)
+                {
+                    bytesFromFile[i] = (byte)fStream.ReadByte();
+                    Console.Write(bytesFromFile[i]);
+                }
+
+
+
+                // Display decoded messages.
+                Console.Write("\nDecoded Message: ");
+                Console.WriteLine(Encoding.Default.GetString(bytesFromFile));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/AnPhCs6Net46/20/SimpleFileIO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple I/O with the File Type *****\n");
+            string[] myTasks = {
+      "Fix bathroom sink", "Call Dave",
+      "Call Mom and Dad", "Play Xbox One"};
+
+            // Write out all data to file on C drive.
+            File.WriteAllLines(@"C:\tasks.txt", myTasks);
+
+            // Read it all back and print out.
+            foreach (string task in File.ReadAllLines(@"C:\tasks.txt"))
+            {
+                Console.WriteLine("TODO: {0}", task);
+            }
+            Console.ReadLine();
+        }
+    }
+}
