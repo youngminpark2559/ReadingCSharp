16dcc63c9e73a290af719aea3279058e732b0e1f
youngmtool <youngmtool@gmail.com>
2017-11-27 11:06:19 +0900

    Add a Person class to examine the exception for XML format serialization. When I serialize object graph, this ignores members which have a private access. When I need to include private member to serialization, I should use the public property which sets private fields.

--- CSharp/AnPhCs6Net46/20/SimpleSerialize/Program.cs
+++ CSharp/AnPhCs6Net46/20/SimpleSerialize/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 //c Create a project SimpleSerialize. And add a Radio class which is marked with [Serializable] and one field, radioID, in the Radio class is marked with [NonSerialized].
 //c Add a Car class which is decorated by [Serializable].
 //c Add a JamesBondCar class which is decorated by [Serializable] and derived from Car base class.
+//c Add a Person class to examine the exception for XML format serialization. When I serialize object graph, this ignores members which have a private access. When I need to include private member to serialization, I should use the public property which sets private fields.
 
 namespace SimpleSerialize
 {
@@ -36,6 +37,25 @@ namespace SimpleSerialize
     {
         public bool canFly;
         public bool canSubmerge;
+    }
+
+
+    [Serializable]
+    public class Person
+    {
+        // A public field.
+        public bool isAlive = true;
+
+        // A private field.
+        private int personAge = 21;
+
+        // Public property/private data.
+        private string fName = string.Empty;
+        public string FirstName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
     }
 
     class Program
