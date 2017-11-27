599821d365644c4019571ded1dc6c21127f249ff
youngmtool <youngmtool@gmail.com>
2017-11-27 09:40:34 +0900

    Create a project MyDirectoryWatcher. I use FileSystemWatcher class and its members. And I also use custom event handler methods which are pointed by several event handler delegates. And the custom event handler methods are fired when FileSystemWatcher object is occured by events such as Changed, Created, Deleted, and Renamed.

---
+++ CSharp/AnPhCs6Net46/20/MyDirectoryWatcher/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a project MyDirectoryWatcher. I use FileSystemWatcher class and its members. And I also use custom event handler methods which are pointed by several event handler delegates. And the custom event handler methods are fired when FileSystemWatcher object is occured by events such as Changed, Created, Deleted, and Renamed.
+
+namespace MyDirectoryWatcher
+{
+    class Program
+    {
+        static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: {0} {1}!", e.FullPath, e.ChangeType);
+        }
+
+        static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** The Amazing File Watcher App *****\n");
+            // Establish the path to the directory to watch.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            try
+            {
+                watcher.Path = @"C:\MyFolder";
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            // Set up the things to be on the lookout for.
+            watcher.NotifyFilter = NotifyFilters.LastAccess
+              | NotifyFilters.LastWrite
+              | NotifyFilters.FileName
+              | NotifyFilters.DirectoryName;
+
+            // Only watch text files.
+            watcher.Filter = "*.txt";
+
+            // Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // Begin watching the directory.
+            watcher.EnableRaisingEvents = true;
+
+            // Wait for the user to quit the program.
+            Console.WriteLine(@"Press 'q' to quit app.");
+            while (Console.Read() != 'q')
+                ;
+        }
+    }
+}
