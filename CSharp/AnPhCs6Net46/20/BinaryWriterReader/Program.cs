8d58232dd6ba9f0126c731e378faeefc878847bd
youngmtool <youngmtool@gmail.com>
2017-11-27 09:25:16 +0900

    Read a BinFile.dat file by using BinaryReader object instantiated by being passed into FileInfo object retrieved from FileInfo.OpenRead() method. And with this BinaryReader object, I call each Read() method such as for double type, int type, or string type.

--- CSharp/AnPhCs6Net46/20/BinaryWriterReader/Program.cs
+++ CSharp/AnPhCs6Net46/20/BinaryWriterReader/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a project BinaryWriterReader. Set a file named BinFile.dat in the destination place. I instantiate BinaryWriter object with passing FileStream object retrieved by invoking FileInfo.OpenWrite() method. And I write various types of data into the stream by using BinaryWriter.Write() method.
+//c Read a BinFile.dat file by using BinaryReader object instantiated by being passed into FileInfo object retrieved from FileInfo.OpenRead() method. And with this BinaryReader object, I call each Read() method such as for double type, int type, or string type.
 
 namespace BinaryWriterReader
 {
@@ -34,6 +35,16 @@ namespace BinaryWriterReader
                 bw.Write(aString);
             }
             Console.WriteLine("Done!");
+
+
+
+            // Read the binary data from the stream.
+            using (BinaryReader br = new BinaryReader(f.OpenRead()))
+            {
+                Console.WriteLine(br.ReadDouble());
+                Console.WriteLine(br.ReadInt32());
+                Console.WriteLine(br.ReadString());
+            }
             Console.ReadLine();
         }
     }
