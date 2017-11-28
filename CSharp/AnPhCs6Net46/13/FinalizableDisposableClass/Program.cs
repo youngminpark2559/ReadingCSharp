3cad8ea7e4e7d5b5d444d0231e069940d40b22ff
youngmtool <youngmtool@gmail.com>
2017-11-28 23:39:32 +0900

    Use IDisposable and finalizer by helper method.

--- CSharp/AnPhCs6Net46/13/FinalizableDisposableClass/Program.cs
+++ CSharp/AnPhCs6Net46/13/FinalizableDisposableClass/Program.cs
@@ -5,28 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a project FinalizableDisposableClass. A MyResourceWrapper class will use both of ways to clean object from the unmanaged heap. IDisposable and finalizer.
+//c Use IDisposable and finalizer by helper method.
 
 namespace FinalizableDisposableClass
 {
-    // A sophisticated resource wrapper.
-    public class MyResourceWrapper : IDisposable
+    class MyResourceWrapper : IDisposable
     {
-        // The garbage collector will call this method if the
-        // object user forgets to call Dispose().
-        ~MyResourceWrapper()
-        {
-            // Clean up any internal unmanaged resources.
-            // Do **not** call Dispose() on any managed objects.
-        }
-        // The object user will call this method to clean up
-        // resources ASAP.
+        // Used to determine if Dispose()
+        // has already been called.
+        private bool disposed = false;
+
         public void Dispose()
         {
-            // Clean up unmanaged resources here.
-            // Call Dispose() on other contained disposable objects.
-            // No need to finalize if user called Dispose(),
-            // so suppress finalization.
+            // Call our helper method.
+            // Specifying "true" signifies that
+            // the object user triggered the cleanup.
+            CleanUp(true);
+
+            // Now suppress finalization.
             GC.SuppressFinalize(this);
+        }
+
+        private void CleanUp(bool disposing)
+        {
+            // Be sure we have not already been disposed!
+            if (!this.disposed)
+            {
+                // If disposing equals true, dispose all
+                // managed resources.
+                if (disposing)
+                {
+                    // Dispose managed resources.
+                }
+                // Clean up unmanaged resources here.
+            }
+            disposed = true;
+        }
+        ~MyResourceWrapper()
+        {
+            // Call our helper method.
+            // Specifying "false" signifies that
+            // the GC triggered the cleanup.
+            CleanUp(false);
         }
     }
 
