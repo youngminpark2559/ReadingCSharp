5100057ad3b475e0b5b79edef31923cac378536b
youngmtool <youngmtool@gmail.com>
2017-11-28 19:27:31 +0900

    Create a project SimpleDispose.

---
+++ CSharp/AnPhCs6Net46/13/SimpleDispose/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a project SimpleDispose.
+
+namespace SimpleDispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
