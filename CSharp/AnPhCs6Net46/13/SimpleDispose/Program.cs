f4d587eded67ddcc7de839584424bac097287a86
youngmtool <youngmtool@gmail.com>
2017-11-28 21:57:52 +0900

    I can use same functionality with try catch finally and Dispose() by using using syntax. using syntax makes the use of try catch finally and Dispose() very simple and convinient. And this using syntax is converted to try catch finally and Dispose() syntax in CIL code after being compiled.

--- CSharp/AnPhCs6Net46/13/SimpleDispose/Program.cs
+++ CSharp/AnPhCs6Net46/13/SimpleDispose/Program.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 //c Add a MyResourceWrapper class and it implements IDisposable. Someone will use this class by instantiating this object and after using done he should invoke Dispose() to immediately clean up unmanaged resources and other contained disposable objects from the managed heap. With this Dispose() of IDisposable, I can clean without using Finalize() consuming time and resource of the system.
 //c Some base class libraries which implement IDisposable have alias name for Dispose(). It's Close(). It's identical in their functionality.
 //c Dispose() is often used with try catch clauses and Dispose() is located in finally block to ensure object must be cleaned no matter what in try block there is some exception or not.
+//c I can use same functionality with try catch finally and Dispose() by using using syntax. using syntax makes the use of try catch finally and Dispose() very simple and convinient. And this using syntax is converted to try catch finally and Dispose() syntax in CIL code after being compiled.
 
 namespace SimpleDispose
 {
@@ -37,20 +38,26 @@ namespace SimpleDispose
             //    rw.Dispose();
 
 
+            //Console.WriteLine("***** Fun with Dispose *****\n");
+            //MyResourceWrapper rw = new MyResourceWrapper();
+            //try
+            //{
+            //    // Use the members of rw.
+            //}
+
+            //finally
+            //{
+            //    // Always call Dispose(), error or not.
+            //    rw.Dispose();
+            //}
+
             Console.WriteLine("***** Fun with Dispose *****\n");
-            MyResourceWrapper rw = new MyResourceWrapper();
-            try
+            // Dispose() is called automatically when the
+            // using scope exits.
+            using (MyResourceWrapper rw = new MyResourceWrapper())
             {
-                // Use the members of rw.
+                // Use rw object.
             }
-
-            finally
-            {
-                // Always call Dispose(), error or not.
-                rw.Dispose();
-            }
-
-
 
 
             Console.ReadLine();
