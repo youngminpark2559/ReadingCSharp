b2911f9198042ac3dfd29453bc9f727379adc220
youngmtool <youngmtool@gmail.com>
2017-11-28 17:06:01 +0900

    Add a test functionality to instantiate many objects and execute the garbage collection and see what happened.

--- CSharp/AnPhCs6Net46/13/SimpleGC/Program.cs
+++ CSharp/AnPhCs6Net46/13/SimpleGC/Program.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 //c Create a project SimpleGC and add a Car class.
 //c Instantiate Car object with passing 2 arguments and use that instantiated instance by using dot operator.
 //c Add methods to show the bytes on the heap and object generations and so on.
-//c Use GC.Collect() and GC.WaitForPendingFinalizers() to execute garbage collectioin immediately and suspend the calling thread for object which I want to delete. 
+//c Use GC.Collect() and GC.WaitForPendingFinalizers() to execute garbage collection immediately and suspend the calling thread for object which I want to delete. 
+//c Add a test functionality to instantiate many objects and execute the garbage collection and see what happened.
 
 namespace SimpleGC
 {
@@ -49,29 +50,77 @@ namespace SimpleGC
             //Console.ReadLine();
 
 
+
+
+            //Console.WriteLine("***** Fun with System.GC *****");
+
+            //// Print out estimated number of bytes on heap.
+            //Console.WriteLine("Estimated bytes on heap: {0}", GC.GetTotalMemory(false));
+
+            //// MaxGeneration is zero based, so add 1 for display purposes.
+            //Console.WriteLine("This OS has {0} object generations.\n", (GC.MaxGeneration + 1));
+
+            //Car refToMyCar = new Car("Zippy", 100);
+            //Console.WriteLine(refToMyCar.ToString());
+
+            //// Print out generation of refToMyCar object.
+            //Console.WriteLine("Generation of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
+
+            //// Force a garbage collection immediately.
+            //GC.Collect();
+            //// And wait for each object to be finalized.
+            //// Using GC.WaitForPendingFinalizers() is mandatory when you use GC.Collect()
+            //GC.WaitForPendingFinalizers();
+
+            //// Only investigate generation 0 objects.
+            //GC.Collect(0);
+            //GC.WaitForPendingFinalizers();
+
+
+
+
             Console.WriteLine("***** Fun with System.GC *****");
 
             // Print out estimated number of bytes on heap.
             Console.WriteLine("Estimated bytes on heap: {0}", GC.GetTotalMemory(false));
 
-            // MaxGeneration is zero based, so add 1 for display purposes.
+            // MaxGeneration is zero based.
             Console.WriteLine("This OS has {0} object generations.\n", (GC.MaxGeneration + 1));
 
             Car refToMyCar = new Car("Zippy", 100);
             Console.WriteLine(refToMyCar.ToString());
+            // Print out generation of refToMyCar.
+            Console.WriteLine("\nGeneration of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
 
-            // Print out generation of refToMyCar object.
+            // Make a ton of objects for testing purposes.
+            object[] tonsOfObjects = new object[50000];
+            for (int i = 0; i < 50000; i++)
+                tonsOfObjects[i] = new object();
+
+            // Collect only gen 0 objects.
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+
+            // Print out generation of refToMyCar.
             Console.WriteLine("Generation of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
 
-            // Force a garbage collection immediately.
-            GC.Collect();
-            // And wait for each object to be finalized.
-            // Using GC.WaitForPendingFinalizers() is mandatory when you use GC.Collect()
-            GC.WaitForPendingFinalizers();
+            // See if tonsOfObjects[9000] is still alive.
+            if (tonsOfObjects[9000] != null)
+            {
+                Console.WriteLine("Generation of tonsOfObjects[9000] is: {0}",
+                  GC.GetGeneration(tonsOfObjects[9000]));
+            }
+            else
+                Console.WriteLine("tonsOfObjects[9000] is no longer alive.");
 
-            // Only investigate generation 0 objects.
-            GC.Collect(0);
-            GC.WaitForPendingFinalizers();
+            // Print out how many times a generation has been swept.
+            Console.WriteLine("\nGen 0 has been swept {0} times",
+              GC.CollectionCount(0));
+            Console.WriteLine("Gen 1 has been swept {0} times",
+              GC.CollectionCount(1));
+            Console.WriteLine("Gen 2 has been swept {0} times",
+              GC.CollectionCount(2));
+
 
             Console.ReadLine();
         }
