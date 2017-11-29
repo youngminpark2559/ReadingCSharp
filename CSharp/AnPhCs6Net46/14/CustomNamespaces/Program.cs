3c9861cd6e4adbcaa5a787539ddf27e2a7df6fa2
youngmtool <youngmtool@gmail.com>
2017-11-29 14:08:57 +0900

    I can use 3 class types declared in the same namespace MyShapes but declared in 3 different cs files.

---
+++ CSharp/AnPhCs6Net46/14/CustomNamespaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyShapes;
+
+//c I can use 3 class types declared in the same namespace MyShapes but declared in 3 different cs files.
+
+namespace CustomNamespaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Hexagon h = new Hexagon();
+            Circle c = new Circle();
+            Square s = new Square();
+        }
+    }
+}
