48bc00bc3f72613e04d8721263f84357528f5502
youngmtool <youngmtool@gmail.com>
2017-11-29 17:39:20 +0900

    Create a project CSharpCarClien to examine the clien which uses the custom class library.

---
+++ CSharp/AnPhCs6Net46/14/CSharpCarClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a project CSharpCarClien to examine the clien which uses the custom class library.
+
+namespace CSharpCarClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
