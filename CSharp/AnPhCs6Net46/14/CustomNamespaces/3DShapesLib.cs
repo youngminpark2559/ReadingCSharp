23c318b426ecedbc46a643c2ae325fa7da123ee4
youngmtool <youngmtool@gmail.com>
2017-11-29 14:24:13 +0900

    I put My3DShapes namespace into Chapter14 namespace. So My3DShapes namespace is nested namespace.

--- CSharp/AnPhCs6Net46/14/CustomNamespaces/3DShapesLib.cs
+++ CSharp/AnPhCs6Net46/14/CustomNamespaces/3DShapesLib.cs
@@ -2,15 +2,20 @@
 using System;
 
 //c Add 3DShapresLib.cs file and in this file, I create a unique namespace named My3DShapes and in this namespace I create 3 classes whose class name are same with the ones in the MyShapes namespace. So, ambiguity issue is occured.
+//c I put My3DShapes namespace into Chapter14 namespace. So My3DShapes namespace is nested namespace.
 
-namespace My3DShapes
+// Nesting a namespace.
+namespace Chapter14
 {
-    // 3D Circle class.
-    public class Circle { }
+    namespace My3DShapes
+    {
+        // 3D Circle class.
+        public class Circle { }
 
-    // 3D Hexagon class.
-    public class Hexagon { }
+        // 3D Hexagon class.
+        public class Hexagon { }
 
-    // 3D Square class.
-    public class Square { }
+        // 3D Square class.
+        public class Square { }
+    }
 }
