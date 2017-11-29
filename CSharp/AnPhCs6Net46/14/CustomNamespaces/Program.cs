28c8fec1ea9af34b74b18c5fed1c3948caa0286d
youngmtool <youngmtool@gmail.com>
2017-11-29 14:12:10 +0900

    I can use 3 class types without using using keyword to reference the namespace. Instead, to use class types, I can use fully qualified name of the class like <namespace name><class name>.

--- CSharp/AnPhCs6Net46/14/CustomNamespaces/Program.cs
+++ CSharp/AnPhCs6Net46/14/CustomNamespaces/Program.cs
@@ -1,6 +1,7 @@
-﻿using MyShapes;
+﻿using System;
 
 //c I can use 3 class types declared in the same namespace MyShapes but declared in 3 different cs files.
+//c I can use 3 class types without using using keyword to reference the namespace. Instead, to use class types, I can use fully qualified name of the class like <namespace name><class name>.
 
 namespace CustomNamespaces
 {
@@ -8,9 +9,9 @@ namespace CustomNamespaces
     {
         static void Main(string[] args)
         {
-            Hexagon h = new Hexagon();
-            Circle c = new Circle();
-            Square s = new Square();
+            MyShapes.Hexagon h = new MyShapes.Hexagon();
+            MyShapes.Circle c = new MyShapes.Circle();
+            MyShapes.Square s = new MyShapes.Square();
         }
     }
 }
