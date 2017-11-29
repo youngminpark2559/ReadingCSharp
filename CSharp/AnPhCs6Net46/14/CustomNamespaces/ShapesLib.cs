11853941d054c49b962f7c92fe00d6a32fc522dd
youngmtool <youngmtool@gmail.com>
2017-11-29 13:22:22 +0900

    One cs file ShapeLib.cs and one namespace MyShapes, and 3 class types. This is more of less bad practice.

---
+++ CSharp/AnPhCs6Net46/14/CustomNamespaces/ShapesLib.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//c Create a project CustomNamespaces to examine creating a custom namespace.
+//c One cs file ShapeLib.cs and one namespace MyShapes, and 3 class types. This is more of less bad practice.
+
+namespace MyShapes
+{
+    // Circle class
+    public class Circle { /* Interesting members... */ }
+    // Hexagon class
+    public class Hexagon { /* More interesting members... */ }
+
+    // Square class
+    public class Square { /* Even more interesting members... */ }
+}
