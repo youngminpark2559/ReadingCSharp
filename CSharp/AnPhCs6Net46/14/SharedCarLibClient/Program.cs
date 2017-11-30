05905af66f40ba19e38d66f730156bd45e892681
youngmtool <youngmtool@gmail.com>
2017-11-30 13:12:26 +0900

    Create a SharedCarLibClient to use a shared assembly, CarLibrary.dll.

---
+++ CSharp/AnPhCs6Net46/14/SharedCarLibClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CarLibrary;
+
+//c Create a SharedCarLibClient to use a shared assembly, CarLibrary.dll.
+
+namespace SharedCarLibClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Shared Assembly Client *****");
+            SportsCar c = new SportsCar();
+            c.TurboBoost();
+            Console.ReadLine();
+        }
+    }
+}
