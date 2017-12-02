4bde36c5f5b01bbdef34a4ea6faaeaaef452bcd3
youngmtool <youngmtool@gmail.com>
2017-12-02 18:48:04 +0900

    Create a class library AttributedCarLibrary to examine how to create and use a custom attribute.

---
+++ CSharp/AnPhCs6Net46/15/AttributedCarLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a class library AttributedCarLibrary to examine how to create and use a custom attribute.
+
+namespace AttributedCarLibrary
+{
+    // A custom attribute.
+    public sealed class VehicleDescriptionAttribute : System.Attribute
+    {
+        public string Description { get; set; }
+        public VehicleDescriptionAttribute(string vehicalDescription)
+        {
+            Description = vehicalDescription;
+        }
+        public VehicleDescriptionAttribute() { }
+    }
+
+    public class Class1
+    {
+    }
+}
