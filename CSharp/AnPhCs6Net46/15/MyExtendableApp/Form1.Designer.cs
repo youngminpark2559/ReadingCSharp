17b5a5e6689df27ca27491d388b5b723a0bb9793
youngmtool <youngmtool@gmail.com>
2017-12-02 22:29:02 +0900

    Create a winforms application in which I will install 2 plug-ins.

---
+++ CSharp/AnPhCs6Net46/15/MyExtendableApp/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MyExtendableApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lstLoadedSnapIns = new System.Windows.Forms.ListBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.snapInModuleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(759, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lstLoadedSnapIns
+            // 
+            this.lstLoadedSnapIns.FormattingEnabled = true;
+            this.lstLoadedSnapIns.ItemHeight = 12;
+            this.lstLoadedSnapIns.Location = new System.Drawing.Point(12, 356);
+            this.lstLoadedSnapIns.Name = "lstLoadedSnapIns";
+            this.lstLoadedSnapIns.Size = new System.Drawing.Size(735, 196);
+            this.lstLoadedSnapIns.TabIndex = 1;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.snapInModuleToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // snapInModuleToolStripMenuItem
+            // 
+            this.snapInModuleToolStripMenuItem.Name = "snapInModuleToolStripMenuItem";
+            this.snapInModuleToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.snapInModuleToolStripMenuItem.Text = "Snap In Module";
+            this.snapInModuleToolStripMenuItem.Click += new System.EventHandler(this.snapInModuleToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 630);
+            this.Controls.Add(this.lstLoadedSnapIns);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem snapInModuleToolStripMenuItem;
+        private System.Windows.Forms.ListBox lstLoadedSnapIns;
+    }
+}
+
---
+++ CSharp/AnPhCs6Net46/15/MyExtendableApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//c Create a winforms application in which I will install 2 plug-ins.
+
+namespace MyExtendableApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void snapInModuleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Allow user to select an assembly to load.
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                if (dlg.FileName.Contains("CommonSnappableTypes"))
+                    MessageBox.Show("CommonSnappableTypes has no snap-ins!");
+                else if (!LoadExternalModule(dlg.FileName))
+                    MessageBox.Show("Nothing implements IAppFunctionality!");
+            }
+        }
+    }
+}
