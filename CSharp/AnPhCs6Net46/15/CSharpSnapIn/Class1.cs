d8ca2d7ecdce740dc16b59ef18447d567a52d2e2
youngmtool <youngmtool@gmail.com>
2017-12-02 22:19:20 +0900

    Create a class library CSharpSnapIn. This dll file will be used in executable application as a plug in along with CommonSnappableTypes.dll.

---
+++ CSharp/AnPhCs6Net46/15/CSharpSnapIn/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonSnappableTypes;
+using System.Windows.Forms;
+
+//c Create a class library CSharpSnapIn. This dll file will be used in executable application as a plug in along with CommonSnappableTypes.dll.
+
+namespace CSharpSnapIn
+{
+    [CompanyInfo(CompanyName = "FooBar", CompanyUrl = "www.FooBar.com")]
+    public class CSharpModule : IAppFunctionality
+    {
+        void IAppFunctionality.DoIt()
+        {
+            MessageBox.Show("You have just used the C# snap-in!");
+        }
+    }
+}
