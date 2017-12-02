127de1c8d633c96846f36b0e98f5e6b14dc54472
youngmtool <youngmtool@gmail.com>
2017-12-02 15:51:37 +0900

    Create a project SharedAsmReflector to examine how to retrieve informations from the shared assembly by using reflection.

---
+++ CSharp/AnPhCs6Net46/15/ExternalAssemblyReflector/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Reflection;
+using System.IO; // For FileNotFoundException definition.
+
+namespace ExternalAssemblyReflector
+{
+    class Program
+    {
+        static void DisplayTypesInAsm(Assembly asm)
+        {
+            Console.WriteLine("\n***** Types in Assembly *****");
+            Console.WriteLine("->{0}", asm.FullName);
+            Type[] types = asm.GetTypes();
+            foreach (Type t in types)
+                Console.WriteLine("Type: {0}", t);
+            Console.WriteLine("");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** External Assembly Viewer *****");
+
+            string asmName = "";
+            Assembly asm = null;
+
+            do
+            {
+                Console.WriteLine("\nEnter an assembly to evaluate");
+                Console.Write("or enter Q to quit: ");
+
+                // Get name of assembly.
+                asmName = Console.ReadLine();
+
+                // Does user want to quit?
+                if (asmName.ToUpper() == "Q")
+                {
+                    break;
+                }
+                // Try to load assembly.
+                try
+                {
+                    asm = Assembly.Load(asmName);
+                    DisplayTypesInAsm(asm);
+                }
+                catch
+                {
+                    Console.WriteLine("Sorry, can't find assembly.");
+                }
+            } while (true);
+        }
+    }
+}
