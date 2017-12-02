78a2ee97c236b2807fbe1fe904ca62ae04a8892a
youngmtool <youngmtool@gmail.com>
2017-12-02 18:34:03 +0900

    Add a class Motorcycle which is decorated by [Serializable] and some of its members are decorated by [NonSerialized].

--- CSharp/AnPhCs6Net46/15/ApplyingAttributes/Program.cs
+++ CSharp/AnPhCs6Net46/15/ApplyingAttributes/Program.cs
@@ -5,9 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a project ApplyingAttributes to examine how to use attribute feature.
+//c Add a class Motorcycle which is decorated by [Serializable] and some of its members are decorated by [NonSerialized].
 
 namespace ApplyingAttributes
 {
+    // This class can be saved to disk.
+    [Serializable]
+    public class Motorcycle
+    {
+        // However, this field will not be persisted.
+        [NonSerialized]
+        float weightOfCurrentPassengers;
+
+        // These fields are still serializable.
+        bool hasRadioSystem;
+        bool hasHeadSet;
+        bool hasSissyBar;
+    }
+
+
     class Program
     {
         static void Main(string[] args)
