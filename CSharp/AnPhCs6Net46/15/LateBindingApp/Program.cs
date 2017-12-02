ab79c86a4a9aacc0a63d825df221af03370a7c5a
youngmtool <youngmtool@gmail.com>
2017-12-02 16:23:42 +0900

    Create a project LateBindingApp to examie how to load assembly without a reference, how to retrieve types from that assembly, all of them by using late binding technique.

---
+++ CSharp/AnPhCs6Net46/15/LateBindingApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a project LateBindingApp to examie how to load assembly without a reference, how to retrieve types from that assembly, all of them by using late binding technique.
+
+namespace LateBindingApp
+{
+    // This program will load an external library,
+    // and create an object using late binding.
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Late Binding *****");
+            // Try to load a local copy of CarLibrary.
+            Assembly a = null;
+            try
+            {
+                a = Assembly.Load("CarLibrary");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            if (a != null)
+                CreateUsingLateBinding(a);
+
+            Console.ReadLine();
+        }
+
+        static void CreateUsingLateBinding(Assembly asm)
+        {
+            try
+            {
+                // Get metadata for the Minivan type.
+                Type miniVan = asm.GetType("CarLibrary.MiniVan");
+
+                // Create a Minivan instance on the fly.
+                object obj = Activator.CreateInstance(miniVan);
+                Console.WriteLine("Created a {0} using late binding!", obj);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
