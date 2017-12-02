3a6e8bfc48e0b01367522a03bb7786c410e1a575
youngmtool <youngmtool@gmail.com>
2017-12-02 22:10:55 +0900

    Create a class library CommonSnappableTypes which will be used in an executable application as a plug-in along with other plug-ins.

---
+++ CSharp/AnPhCs6Net46/15/CommonSnappableTypes/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a class library CommonSnappableTypes which will be used in an executable application as a plug-in along with other plug-ins.
+
+namespace CommonSnappableTypes
+{
+    public interface IAppFunctionality
+    {
+        void DoIt();
+    }
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class CompanyInfoAttribute : System.Attribute
+    {
+        public string CompanyName { get; set; }
+        public string CompanyUrl { get; set; }
+    }
+}
