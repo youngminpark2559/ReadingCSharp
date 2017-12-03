4f4545bf56e7483242322082f1565f65968092c5
youngmtool <youngmtool@gmail.com>
2017-12-03 21:21:56 +0900

    Add ArrayListOfRandomObjects() to show that I can add any types into ArrayList because ArrayList members are prototyped to object type.

--- CSharp/AnPhCs6Net46/09/IssuesWithNonGenericCollections/Program.cs
+++ CSharp/AnPhCs6Net46/09/IssuesWithNonGenericCollections/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+//c Add ArrayListOfRandomObjects() to show that I can add any types into ArrayList because ArrayList members are prototyped to object type.
 
 namespace IssuesWithNonGenericCollections
 {
@@ -30,6 +33,25 @@ namespace IssuesWithNonGenericCollections
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+        static void WorkWithArrayList()
+        {
+            // Value types are automatically boxed when
+            // passed to a method requesting an object.
+            ArrayList myInts = new ArrayList();
+            myInts.Add(10);
+            myInts.Add(20);
+            myInts.Add(35);
+        }
+
+        static void ArrayListOfRandomObjects()
+        {
+            // The ArrayList can hold anything at all.
+            ArrayList allMyObjects = new ArrayList();
+            allMyObjects.Add(true);
+            allMyObjects.Add(new OperatingSystem(PlatformID.MacOSX, new Version(10, 0)));
+            allMyObjects.Add(66);
+            allMyObjects.Add(3.14);
         }
     }
 }
