b0c38ae1684fcdd678b9ae8127c9463e42062b6d
youngmtool <youngmtool@gmail.com>
2017-12-04 13:36:07 +0900

    Add Swap().

--- CSharp/AnPhCs6Net46/09/CustomGenericMethods/Program.cs
+++ CSharp/AnPhCs6Net46/09/CustomGenericMethods/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console applicatioin CustomGenericMethods.
+//c Add Swap().
 
 namespace CustomGenericMethods
 {
@@ -13,5 +14,15 @@ namespace CustomGenericMethods
         static void Main(string[] args)
         {
         }
+
+        // Swap two integers.
+        static void Swap(ref int a, ref int b)
+        {
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
     }
 }
