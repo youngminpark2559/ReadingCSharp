46428d3de9355ff23ea778dfd930a9c5d13ff286
youngmtool <youngmtool@gmail.com>
2017-12-04 13:07:08 +0900

    Create a console application FunWithObservableCollection.

--- CSharp/AnPhCs6Net46/09/FunWithGenericCollections/Program.cs
+++ CSharp/AnPhCs6Net46/09/FunWithGenericCollections/Program.cs
@@ -145,5 +145,30 @@ namespace FunWithGenericCollections
                 Console.WriteLine(p);
             }
         }
+
+        private static void UseDictionary()
+        {
+            // Populate using Add() method
+            Dictionary<string, Person> peopleA = new Dictionary<string, Person>();
+            peopleA.Add("Homer", new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            peopleA.Add("Marge", new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+            peopleA.Add("Lisa", new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+
+            // Get Homer.
+            Person homer = peopleA["Homer"];
+            Console.WriteLine(homer);
+
+            // Populate with initialization syntax.
+            Dictionary<string, Person> peopleB = new Dictionary<string, Person>()
+            {
+                { "Homer", new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 } },
+                { "Marge", new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 } },
+                { "Lisa",  new Person { FirstName = "Lisa",  LastName = "Simpson", Age = 9 } }
+            };
+
+            // Get Lisa.
+            Person lisa = peopleB["Lisa"];
+            Console.WriteLine(lisa);
+        }
     }
 }
