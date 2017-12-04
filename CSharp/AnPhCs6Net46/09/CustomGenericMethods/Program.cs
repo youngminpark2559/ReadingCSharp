61f40f1ab2b22221fa74c2ccc3995630283f12cd
youngmtool <youngmtool@gmail.com>
2017-12-04 13:38:15 +0900

    Add Swap() which is defferent Swap(ref int a, ref int b). This method swaps Person objects incoming to this method.

--- CSharp/AnPhCs6Net46/09/CustomGenericMethods/Program.cs
+++ CSharp/AnPhCs6Net46/09/CustomGenericMethods/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 //c Create a console applicatioin CustomGenericMethods.
 //c Add Swap().
+//c Add Swap() which is defferent Swap(ref int a, ref int b). This method swaps Person objects incoming to this method.
 
 namespace CustomGenericMethods
 {
@@ -19,6 +20,15 @@ namespace CustomGenericMethods
         static void Swap(ref int a, ref int b)
         {
             int temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // Swap two Person objects.
+        static void Swap(ref Person a, ref Person b)
+        {
+            Person temp;
             temp = a;
             a = b;
             b = temp;
