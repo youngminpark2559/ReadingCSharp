a3eaf51adba8ef059120357a25eeea1a7361fd51
youngmtool <youngmtool@gmail.com>
2017-12-04 14:05:40 +0900

    Add DisplayBaseClass() and update Main() to invoke DisplayBaseClass().

--- CSharp/AnPhCs6Net46/09/CustomGenericMethods/Program.cs
+++ CSharp/AnPhCs6Net46/09/CustomGenericMethods/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 //c Add Swap().
 //c Add Swap() which is defferent Swap(ref int a, ref int b). This method swaps Person objects incoming to this method.
 //c Add a class Person.
+//c Add DisplayBaseClass() and update Main() to invoke DisplayBaseClass().
 
 namespace CustomGenericMethods
 {
@@ -23,6 +24,36 @@ namespace CustomGenericMethods
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Custom Generic Methods *****\n");
+
+            // Swap 2 ints.
+            int a = 10, b = 90;
+            Console.WriteLine("Before swap: {0}, {1}", a, b);
+            Swap<int>(ref a, ref b);
+            Console.WriteLine("After swap: {0}, {1}", a, b);
+            Console.WriteLine();
+
+            // Swap 2 strings.
+            string s1 = "Hello", s2 = "There";
+            Console.WriteLine("Before swap: {0} {1}!", s1, s2);
+            Swap<string>(ref s1, ref s2);
+            Console.WriteLine("After swap: {0} {1}!", s1, s2);
+
+            // Compiler will infer System.Boolean.
+            bool b1 = true, b2 = false;
+            Console.WriteLine("Before swap: {0}, {1}", b1, b2);
+            Swap(ref b1, ref b2);
+            Console.WriteLine("After swap: {0}, {1}", b1, b2);
+
+            // Must supply type parameter if
+            // the method does not take params.
+            DisplayBaseClass<int>();
+            DisplayBaseClass<string>();
+
+            // Compiler error! No params? Must supply placeholder!
+            // DisplayBaseClass();
+
+            Console.ReadLine();
         }
 
         // Swap two integers.
@@ -43,5 +74,24 @@ namespace CustomGenericMethods
             b = temp;
         }
 
+        // This method will swap any two items.
+        // as specified by the type parameter <T>.
+        static void Swap<T>(ref T a, ref T b)
+        {
+            Console.WriteLine("You sent the Swap() method a {0}",
+              typeof(T));
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void DisplayBaseClass<T>()
+        {
+            // BaseType is a method used in reflection,
+            // which will be examined in Chapter 15
+            Console.WriteLine("Base class of {0} is: {1}.",
+              typeof(T), typeof(T).BaseType);
+        }
     }
 }
