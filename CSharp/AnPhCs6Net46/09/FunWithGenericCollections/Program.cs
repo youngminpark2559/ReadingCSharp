db491500af2ae2a8e0c40caf64a9ea4397028cc4
youngmtool <youngmtool@gmail.com>
2017-12-04 12:06:10 +0900

    Add a UseGenericQueue() which uses its members(methods).

--- CSharp/AnPhCs6Net46/09/FunWithGenericCollections/Program.cs
+++ CSharp/AnPhCs6Net46/09/FunWithGenericCollections/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 //c Create a console application FunWithGenericCollections to examine List<T>.
 //c Add GetCoffee() which gets Person object and within that Person object, retrieve FirstName value.
+//c Add a UseGenericQueue() which uses its members(methods).
 
 namespace FunWithGenericCollections
 {
@@ -57,6 +58,48 @@ namespace FunWithGenericCollections
         static void GetCoffee(Person p)
         {
             Console.WriteLine("{0} got coffee!", p.FirstName);
+        }
+
+
+        static void UseGenericQueue()
+        {
+            // Make a Q with three people.
+            Queue<Person> peopleQ = new Queue<Person>();
+            peopleQ.Enqueue(new Person
+            {
+                FirstName = "Homer",
+                LastName = "Simpson",
+                Age = 47
+            });
+            peopleQ.Enqueue(new Person
+            {
+                FirstName = "Marge",
+                LastName = "Simpson",
+                Age = 45
+            });
+            peopleQ.Enqueue(new Person
+            {
+                FirstName = "Lisa",
+                LastName = "Simpson",
+                Age = 9
+            });
+
+            // Peek at first person in Q.
+            Console.WriteLine("{0} is first in line!", peopleQ.Peek().FirstName);
+
+            // Remove each person from Q.
+            GetCoffee(peopleQ.Dequeue());
+            GetCoffee(peopleQ.Dequeue());
+            GetCoffee(peopleQ.Dequeue());
+            // Try to de-Q again?
+            try
+            {
+                GetCoffee(peopleQ.Dequeue());
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Error! {0}", e.Message);
+            }
         }
     }
 }
