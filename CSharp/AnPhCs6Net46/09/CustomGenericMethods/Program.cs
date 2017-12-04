3c67791aa40d4b7c0e330597a8ab19d31cc321fa
youngmtool <youngmtool@gmail.com>
2017-12-04 13:39:23 +0900

    Add a class Person.

--- CSharp/AnPhCs6Net46/09/CustomGenericMethods/Program.cs
+++ CSharp/AnPhCs6Net46/09/CustomGenericMethods/Program.cs
@@ -7,9 +7,18 @@ using System.Threading.Tasks;
 //c Create a console applicatioin CustomGenericMethods.
 //c Add Swap().
 //c Add Swap() which is defferent Swap(ref int a, ref int b). This method swaps Person objects incoming to this method.
+//c Add a class Person.
 
 namespace CustomGenericMethods
 {
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
