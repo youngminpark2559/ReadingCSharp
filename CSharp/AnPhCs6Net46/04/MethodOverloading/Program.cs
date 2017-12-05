b6d35b706d4ef460c5ba4a066d2c0b041761d29d
youngmtool <youngmtool@gmail.com>
2017-12-05 22:51:45 +0900

    Add 3 overloaded Add().

--- CSharp/AnPhCs6Net46/04/MethodOverloading/Program.cs
+++ CSharp/AnPhCs6Net46/04/MethodOverloading/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application MethodOverloading.
+//c Add 3 overloaded Add().
 
 namespace MethodOverloading
 {
@@ -13,5 +14,15 @@ namespace MethodOverloading
         static void Main(string[] args)
         {
         }
+
+        // Overloaded Add() method.
+        static int Add(int x, int y)
+        { return x + y; }
+
+        static double Add(double x, double y)
+        { return x + y; }
+
+        static long Add(long x, long y)
+        { return x + y; }
     }
 }
