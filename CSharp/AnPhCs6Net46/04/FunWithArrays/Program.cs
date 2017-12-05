2b249f8c77a99f116c4a5233210919f63a967502
youngmtool <youngmtool@gmail.com>
2017-12-06 06:39:30 +0900

    Add DeclareImplicitArrays() which uses implicitly typed local variable type by using var keyword for declaration of array.

--- CSharp/AnPhCs6Net46/04/FunWithArrays/Program.cs
+++ CSharp/AnPhCs6Net46/04/FunWithArrays/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 //c Create a console application FunWithArrays.
 //c Add SimpleArrays(). In this method, I create 3 length int array and 100 length string array.
 //c Update SimpleArrays(). I create 3 length int array and put the values into the array. And I print all value from myInts array source by using foreach looping.
+//c Add DeclareImplicitArrays() which uses implicitly typed local variable type by using var keyword for declaration of array.
 
 namespace FunWithArrays
 {
@@ -32,6 +33,24 @@ namespace FunWithArrays
             // Now print each value.
             foreach (int i in myInts)
                 Console.WriteLine(i);
+            Console.WriteLine();
+        }
+
+        static void DeclareImplicitArrays()
+        {
+            Console.WriteLine("=> Implicit Array Initialization.");
+
+            // a is really int[].
+            var a = new[] { 1, 10, 100, 1000 };
+            Console.WriteLine("a is a: {0}", a.ToString());
+
+            // b is really double[].
+            var b = new[] { 1, 1.5, 2, 2.5 };
+            Console.WriteLine("b is a: {0}", b.ToString());
+
+            // c is really string[].
+            var c = new[] { "hello", null, "world" };
+            Console.WriteLine("c is a: {0}", c.ToString());
             Console.WriteLine();
         }
     }
