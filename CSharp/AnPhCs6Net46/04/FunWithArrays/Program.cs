3bdc941ed37958e1ce204d61ed22f8ad53f30d14
youngmtool <youngmtool@gmail.com>
2017-12-05 23:08:21 +0900

    Update SimpleArrays(). I create 3 length int array and put the values into the array. And I print all value from myInts array source by using foreach looping.

--- CSharp/AnPhCs6Net46/04/FunWithArrays/Program.cs
+++ CSharp/AnPhCs6Net46/04/FunWithArrays/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 //c Create a console application FunWithArrays.
 //c Add SimpleArrays(). In this method, I create 3 length int array and 100 length string array.
+//c Update SimpleArrays(). I create 3 length int array and put the values into the array. And I print all value from myInts array source by using foreach looping.
 
 namespace FunWithArrays
 {
@@ -22,11 +23,15 @@ namespace FunWithArrays
         static void SimpleArrays()
         {
             Console.WriteLine("=> Simple Array Creation.");
-            // Create an array of ints containing 3 elements indexed 0, 1, 2
+            // Create and fill an array of 3 Integers
             int[] myInts = new int[3];
+            myInts[0] = 100;
+            myInts[1] = 200;
+            myInts[2] = 300;
 
-            // Create a 100 item string array, indexed 0 - 99
-            string[] booksOnDotNet = new string[100];
+            // Now print each value.
+            foreach (int i in myInts)
+                Console.WriteLine(i);
             Console.WriteLine();
         }
     }
