9d965e7d462f41c1bd4e4e0ceefa2320a2d9d239
youngmtool <youngmtool@gmail.com>
2017-12-05 16:43:10 +0900

    Add a Add() and this method's parameters have no parameter modifiers. So when you invoke this method with passing arguments, the value of arguments is copied and goes into this method.

--- CSharp/AnPhCs6Net46/04/FunWithMethods/Program.cs
+++ CSharp/AnPhCs6Net46/04/FunWithMethods/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application FunWithMethods to examine parameter modifiers such none, out, ref, and param.
+//c Add a Add() and this method's parameters have no parameter modifiers. So when you invoke this method with passing arguments, the value of arguments is copied and goes into this method.
 
 namespace FunWithMethods
 {
@@ -12,6 +13,18 @@ namespace FunWithMethods
     {
         static void Main(string[] args)
         {
+        }
+
+        // Arguments are passed by value by default.
+        static int Add(int x, int y)
+        {
+            int ans = x + y;
+            // Caller will not see these changes
+            // as you are modifying a copy of the
+            // original data.
+            x = 10000;
+            y = 88888;
+            return ans;
         }
     }
 }
