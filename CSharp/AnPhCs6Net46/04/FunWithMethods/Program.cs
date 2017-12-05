1fc4388c7aa6b9c8492c7837715eeacfee31a04c
youngmtool <youngmtool@gmail.com>
2017-12-05 16:28:15 +0900

    Create a console application FunWithMethods to examine parameter modifiers such none, out, ref, and param.

---
+++ CSharp/AnPhCs6Net46/04/FunWithMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a console application FunWithMethods to examine parameter modifiers such none, out, ref, and param.
+
+namespace FunWithMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
