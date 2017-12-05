6cf583ec2968e57c7d458eefaf48b6d4e3712d2c
youngmtool <youngmtool@gmail.com>
2017-12-05 11:59:46 +0900

    Add StringEquality() to compare string value to other string value by using ==, !=, String.Equals().

--- CSharp/AnPhCs6Net46/03/FunWithStrings/Program.cs
+++ CSharp/AnPhCs6Net46/03/FunWithStrings/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 //c Add StringConcatenation() to examine string concatenation.
 //c Update StringConcatenation() to use directly String.Concat() to concatenate 2 string type values instead of using + operator which is finally converted to String.Concat() by C# compiler.
 //c Add EscapeChars() to examine how to use escape characters.
+//c Add StringEquality() to compare string value to other string value by using ==, !=, String.Equals().
 
 namespace FunWithStrings
 {
@@ -54,6 +55,25 @@ namespace FunWithStrings
 
             // Adds a total of 4 blank lines (then beep again!).
             Console.WriteLine("All finished.\n\n\n\a ");
+            Console.WriteLine();
+        }
+
+        static void StringEquality()
+        {
+            Console.WriteLine("=> String equality:");
+            string s1 = "Hello!";
+            string s2 = "Yo!";
+            Console.WriteLine("s1 = {0}", s1);
+            Console.WriteLine("s2 = {0}", s2);
+            Console.WriteLine();
+
+            // Test these strings for equality.
+            Console.WriteLine("s1 == s2: {0}", s1 == s2);
+            Console.WriteLine("s1 == Hello!: {0}", s1 == "Hello!");
+            Console.WriteLine("s1 == HELLO!: {0}", s1 == "HELLO!");
+            Console.WriteLine("s1 == hello!: {0}", s1 == "hello!");
+            Console.WriteLine("s1.Equals(s2): {0}", s1.Equals(s2));
+            Console.WriteLine("Yo.Equals(s2): {0}", "Yo!".Equals(s2));
             Console.WriteLine();
         }
     }
