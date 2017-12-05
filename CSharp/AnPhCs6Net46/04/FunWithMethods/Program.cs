4b610c1039077e42bd582118efa99f284cf0687d
youngmtool <youngmtool@gmail.com>
2017-12-05 17:04:08 +0900

    Add FillTheseValues(). By using out parameter modifier, I can get the similar effect that I can get multiple return values from the single method.

--- CSharp/AnPhCs6Net46/04/FunWithMethods/Program.cs
+++ CSharp/AnPhCs6Net46/04/FunWithMethods/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 //c Create a console application FunWithMethods to examine parameter modifiers such none, out, ref, and param.
 //c Add a Add() and this method's parameters have no parameter modifiers. So when you invoke this method with passing arguments, the value of arguments is copied and goes into this method.
 //c Add an overriden Add() whose parameter modifier is out. With this feature I can change the variable which is located in the place where this method is invoked. Note that this method's return type is void.
+//c Add FillTheseValues(). By using out parameter modifier, I can get the similar effect that I can get multiple return values from the single method.
 
 namespace FunWithMethods
 {
@@ -21,6 +22,21 @@ namespace FunWithMethods
             Console.WriteLine("Before call: X: {0}, Y: {1}", x, y);
             Console.WriteLine("Answer is: {0}", Add(x, y));
             Console.WriteLine("After call: X: {0}, Y: {1}", x, y);
+
+            // No need to assign initial value to local variables
+            // used as output parameters, provided the first time
+            // you use them is as output arguments.
+            int ans;
+            Add(90, 90, out ans);
+            Console.WriteLine("90 + 90 = {0}", ans);
+
+            int i; string str; bool b;
+            FillTheseValues(out i, out str, out b);
+
+            Console.WriteLine("Int is: {0}", i);
+            Console.WriteLine("String is: {0}", str);
+            Console.WriteLine("Boolean is: {0}", b);
+
             Console.ReadLine();
         }
 
@@ -41,6 +57,14 @@ namespace FunWithMethods
         static void Add(int x, int y, out int ans)
         {
             ans = x + y;
+        }
+
+        // Returning multiple output parameters.
+        static void FillTheseValues(out int a, out string b, out bool c)
+        {
+            a = 9;
+            b = "Enjoy your string.";
+            c = true;
         }
     }
 }
