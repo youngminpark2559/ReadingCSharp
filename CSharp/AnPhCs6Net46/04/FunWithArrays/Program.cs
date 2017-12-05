d46d12a458ff553175b26d558f41cfe4baedcba3
youngmtool <youngmtool@gmail.com>
2017-12-05 23:04:46 +0900

    Add SimpleArrays(). In this method, I create 3 length int array and 100 length string array.

--- CSharp/AnPhCs6Net46/04/FunWithArrays/Program.cs
+++ CSharp/AnPhCs6Net46/04/FunWithArrays/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application FunWithArrays.
+//c Add SimpleArrays(). In this method, I create 3 length int array and 100 length string array.
 
 namespace FunWithArrays
 {
@@ -12,6 +13,21 @@ namespace FunWithArrays
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Arrays *****");
+            SimpleArrays();
+            Console.ReadLine();
+        }
+
+
+        static void SimpleArrays()
+        {
+            Console.WriteLine("=> Simple Array Creation.");
+            // Create an array of ints containing 3 elements indexed 0, 1, 2
+            int[] myInts = new int[3];
+
+            // Create a 100 item string array, indexed 0 - 99
+            string[] booksOnDotNet = new string[100];
+            Console.WriteLine();
         }
     }
 }
