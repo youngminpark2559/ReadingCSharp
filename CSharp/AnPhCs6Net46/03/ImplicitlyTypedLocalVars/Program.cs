aead70fae12e37714ae33e10b9527b054687186a
youngmtool <youngmtool@gmail.com>
2017-12-05 15:15:03 +0900

    Add a DeclareImplicitVars() to use var keyword to declare local variable value type.

--- CSharp/AnPhCs6Net46/03/ImplicitlyTypedLocalVars/Program.cs
+++ CSharp/AnPhCs6Net46/03/ImplicitlyTypedLocalVars/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application ImplicitlyTypedLocalVars to exmaine implicitly typed local variable by using var keyword.
+//c Add a DeclareImplicitVars() to use var keyword to declare local variable value type.
 
 namespace ImplicitlyTypedLocalVars
 {
@@ -12,6 +13,16 @@ namespace ImplicitlyTypedLocalVars
     {
         static void Main(string[] args)
         {
+        }
+
+        static void DeclareImplicitVars()
+        {
+            // Implicitly typed local variables
+            // are declared as follows:
+            // var variableName = initialValue;
+            var myInt = 0;
+            var myBool = true;
+            var myString = "Time, marches on...";
         }
     }
 }
