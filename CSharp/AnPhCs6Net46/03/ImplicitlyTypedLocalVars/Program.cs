67b32b0c5791513b40da2b0e0af641cff87e9429
youngmtool <youngmtool@gmail.com>
2017-12-05 15:13:56 +0900

    Create a console application ImplicitlyTypedLocalVars to exmaine implicitly typed local variable by using var keyword.

---
+++ CSharp/AnPhCs6Net46/03/ImplicitlyTypedLocalVars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a console application ImplicitlyTypedLocalVars to exmaine implicitly typed local variable by using var keyword.
+
+namespace ImplicitlyTypedLocalVars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- CSharp/AnPhCs6Net46/03/TypeConversions/Program.cs
+++ CSharp/AnPhCs6Net46/03/TypeConversions/Program.cs
@@ -16,8 +16,7 @@ namespace TypeConversions
 
             // Add two shorts and print the result.
             short numb1 = 9, numb2 = 10;
-            Console.WriteLine("{0} + {1} = {2}",
-              numb1, numb2, Add(numb1, numb2));
+            Console.WriteLine("{0} + {1} = {2}", numb1, numb2, Add(numb1, numb2));
             Console.ReadLine();
         }
         static int Add(int x, int y)
