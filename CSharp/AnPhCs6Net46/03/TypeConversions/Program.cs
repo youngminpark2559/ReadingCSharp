7d9523bc0bfcafcd1c6654d9ff86440ffbca5212
youngmtool <youngmtool@gmail.com>
2017-12-05 13:13:17 +0900

    Create a console application TypeConversions to examine narrowing and widening data type conversions.

---
+++ CSharp/AnPhCs6Net46/03/TypeConversions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a console application TypeConversions to examine narrowing and widening data type conversions.
+
+namespace TypeConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with type conversions *****");
+
+            // Add two shorts and print the result.
+            short numb1 = 9, numb2 = 10;
+            Console.WriteLine("{0} + {1} = {2}",
+              numb1, numb2, Add(numb1, numb2));
+            Console.ReadLine();
+        }
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
