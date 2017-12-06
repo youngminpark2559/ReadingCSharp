01f27885015910e66f62c960d8346bf0de32b442
youngmtool <youngmtool@gmail.com>
2017-12-06 11:47:03 +0900

    Create a console application FunWithEnums to examine the enumeration type.

--- CSharp/AnPhCs6Net46/04/FunWithArrays/Program.cs
+++ CSharp/AnPhCs6Net46/04/FunWithArrays/Program.cs
@@ -24,7 +24,8 @@ namespace FunWithArrays
         {
             Console.WriteLine("***** Fun with Arrays *****");
             //SimpleArrays();
-            ArrayOfObjects();
+            //ArrayOfObjects();
+            SystemArrayFunctionality();
 
             Console.ReadLine();
         }
@@ -153,6 +154,44 @@ namespace FunWithArrays
             foreach (string s in strs)
                 Console.WriteLine(s);
 
+            Console.WriteLine();
+        }
+
+        static void SystemArrayFunctionality()
+        {
+            Console.WriteLine("=> Working with System.Array.");
+            // Initialize items at startup.
+            string[] gothicBands = { "Tones on Tail", "Bauhaus", "Sisters of Mercy" };
+
+            // Print out names in declared order.
+            Console.WriteLine("-> Here is the array:");
+            for (int i = 0; i < gothicBands.Length; i++)
+            {
+                // Print a name.
+                Console.Write(gothicBands[i] + ", ");
+            }
+            Console.WriteLine("\n");
+
+            // Reverse them...
+            Array.Reverse(gothicBands);
+            Console.WriteLine("-> The reversed array");
+            // ... and print them.
+            for (int i = 0; i < gothicBands.Length; i++)
+            {
+                // Print a name.
+                Console.Write(gothicBands[i] + ", ");
+            }
+            Console.WriteLine("\n");
+
+            // Clear out all but the first member.
+            Console.WriteLine("-> Cleared out all but one...");
+            Array.Clear(gothicBands, 1, 2);
+
+            for (int i = 0; i < gothicBands.Length; i++)
+            {
+                // Print a name.
+                Console.Write(gothicBands[i] + ", ");
+            }
             Console.WriteLine();
         }
     }
