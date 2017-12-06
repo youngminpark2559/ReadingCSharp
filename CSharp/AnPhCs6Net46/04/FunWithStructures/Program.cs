9947e9dbc3c288f3a060eca704a481e03bdb2445
youngmtool <youngmtool@gmail.com>
2017-12-06 15:30:48 +0900

    Add a structure Point. This struct contains fields, methods.

---
+++ CSharp/AnPhCs6Net46/04/FunWithStructures/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a structure Point. This struct contains fields, methods.
+
+namespace FunWithStructures
+{
+    struct Point
+    {
+        // Fields of the structure.
+        public int X;
+        public int Y;
+        // Add 1 to the (X, Y) position.
+        public void Increment()
+        {
+            X++; Y++;
+        }
+
+        // Subtract 1 from the (X, Y) position.
+        public void Decrement()
+        {
+            X--; Y--;
+        }
+
+        // Display the current position.
+        public void Display()
+        {
+            Console.WriteLine("X = {0}, Y = {1}", X, Y);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
