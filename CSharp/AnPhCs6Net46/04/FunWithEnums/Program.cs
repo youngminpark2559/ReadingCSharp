57da34487566f4fc7b0bc9d0ab13f6ca93ab9fe3
youngmtool <youngmtool@gmail.com>
2017-12-06 11:49:53 +0900

    Add enum EmpType holding 4 items.

--- CSharp/AnPhCs6Net46/04/FunWithEnums/Program.cs
+++ CSharp/AnPhCs6Net46/04/FunWithEnums/Program.cs
@@ -5,9 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application FunWithEnums to examine the enumeration type.
+//c Add enum EmpType holding 4 items.
 
 namespace FunWithEnums
 {
+
+    // A custom enumeration.
+    enum EmpType
+    {
+        Manager,      // = 0
+        Grunt,        // = 1
+        Contractor,   // = 2
+        VicePresident // = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
