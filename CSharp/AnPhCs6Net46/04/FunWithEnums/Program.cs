7ee3a775f09f300e80267b2906a655a0c1d903ae
youngmtool <youngmtool@gmail.com>
2017-12-06 12:07:14 +0900

    Update EmpType. I can change the underlying data type (byte, short, int, or long) for numerical value mapped to the corresponding named constant.

--- CSharp/AnPhCs6Net46/04/FunWithEnums/Program.cs
+++ CSharp/AnPhCs6Net46/04/FunWithEnums/Program.cs
@@ -8,12 +8,13 @@ using System.Threading.Tasks;
 //c Add enum EmpType holding 4 items.
 //c Update EmpType. I set 102 numerical value for Manager named constant. 
 //c Update EmpType. In enum, named constant should be unique. But numerical value for the named constant doesn't need to be unique. And also numerical value for the named constant doesn't need to follow sequential ordering.
+//c Update EmpType. I can change the underlying data type (byte, short, int, or long) for numerical value mapped to the corresponding named constant.
 
 namespace FunWithEnums
 {
 
-    // Elements of an enumeration need not be sequential!
-    enum EmpType
+    // This time, EmpType maps to an underlying byte.
+    enum EmpType : byte
     {
         Manager = 10,
         Grunt = 1,
