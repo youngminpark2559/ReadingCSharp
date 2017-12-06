36cb17dde5b71885e97b13b713350bcbaeeb3fe9
youngmtool <youngmtool@gmail.com>
2017-12-06 11:56:48 +0900

    Update EmpType. In enum, named constant should be unique. But numerical value for the named constant doesn't need to be unique. And also numerical value for the named constant doesn't need to follow sequential ordering.

--- CSharp/AnPhCs6Net46/04/FunWithEnums/Program.cs
+++ CSharp/AnPhCs6Net46/04/FunWithEnums/Program.cs
@@ -7,17 +7,18 @@ using System.Threading.Tasks;
 //c Create a console application FunWithEnums to examine the enumeration type.
 //c Add enum EmpType holding 4 items.
 //c Update EmpType. I set 102 numerical value for Manager named constant. 
+//c Update EmpType. In enum, named constant should be unique. But numerical value for the named constant doesn't need to be unique. And also numerical value for the named constant doesn't need to follow sequential ordering.
 
 namespace FunWithEnums
 {
 
-    // Begin with 102.
+    // Elements of an enumeration need not be sequential!
     enum EmpType
     {
-        Manager = 102,
-        Grunt,        // = 103
-        Contractor,   // = 104
-        VicePresident // = 105
+        Manager = 10,
+        Grunt = 1,
+        Contractor = 100,
+        VicePresident = 9
     }
 
     class Program
