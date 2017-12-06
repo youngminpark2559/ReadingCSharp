080d12279e1c604427ea66cc97f5eaf25f9ddbf3
youngmtool <youngmtool@gmail.com>
2017-12-06 12:10:31 +0900

    When I set underlying data type for numerical value in the way I like, I must follow the range of each data type. For example, When I use byte data type, I can't store 999 as a numerical value for the named constant VicePresident because 999 is out of range for the byte data type.

--- CSharp/AnPhCs6Net46/04/FunWithEnums/Program.cs
+++ CSharp/AnPhCs6Net46/04/FunWithEnums/Program.cs
@@ -9,17 +9,19 @@ using System.Threading.Tasks;
 //c Update EmpType. I set 102 numerical value for Manager named constant. 
 //c Update EmpType. In enum, named constant should be unique. But numerical value for the named constant doesn't need to be unique. And also numerical value for the named constant doesn't need to follow sequential ordering.
 //c Update EmpType. I can change the underlying data type (byte, short, int, or long) for numerical value mapped to the corresponding named constant.
+//c When I set underlying data type for numerical value in the way I like, I must follow the range of each data type. For example, When I use byte data type, I can't store 999 as a numerical value for the named constant VicePresident because 999 is out of range for the byte data type.
 
 namespace FunWithEnums
 {
 
-    // This time, EmpType maps to an underlying byte.
+    // Compile-time error! 999 is too big for a byte!
     enum EmpType : byte
     {
         Manager = 10,
         Grunt = 1,
         Contractor = 100,
         VicePresident = 9
+        //VicePresident = 999
     }
 
     class Program
