57d962fb9c243d4b5fcc5af208da0970e540f145
youngmtool <youngmtool@gmail.com>
2017-12-07 07:56:57 +0900

    Create a console application RefTypeValTypeParams and add a class Person.

---
+++ CSharp/AnPhCs6Net46/04/RefTypeValTypeParams/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a console application RefTypeValTypeParams and add a class Person.
+
+namespace RefTypeValTypeParams
+{
+    class Person
+    {
+        public string personName;
+        public int personAge;
+
+        // Constructors.
+        public Person(string name, int age)
+        {
+            personName = name;
+            personAge = age;
+        }
+        public Person() { }
+
+        public void Display()
+        {
+            Console.WriteLine("Name: {0}, Age: {1}", personName, personAge);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
