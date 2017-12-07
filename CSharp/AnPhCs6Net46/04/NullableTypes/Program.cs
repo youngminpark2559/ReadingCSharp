134dd814f0425a85d64aabe14fe287b0916a31ed
youngmtool <youngmtool@gmail.com>
2017-12-07 10:03:23 +0900

    Updata Main() by declaring value data type local variables and assigning null to them. It's going to cause compile-time error. But when I declare string(class) data type local variables and assign null to it, that brings no issue.

--- CSharp/AnPhCs6Net46/04/NullableTypes/Program.cs
+++ CSharp/AnPhCs6Net46/04/NullableTypes/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application NullableTypes.
+//c Updata Main() by declaring value data type local variables and assigning null to them. It's going to cause compile-time error. But when I declare string(class) data type local variables and assign null to it, that brings no issue.
 
 namespace NullableTypes
 {
@@ -12,6 +13,13 @@ namespace NullableTypes
     {
         static void Main(string[] args)
         {
+            // Compiler errors!
+            // Value types cannot be set to null!
+            //bool myBool = null;
+            //int myInt = null;
+
+            // OK! Strings are reference types.
+            string myString = null;
         }
     }
 }
