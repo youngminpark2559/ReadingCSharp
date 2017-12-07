220dd76b57d6fe9f08e3adb7b2d568f3f80c873d
youngmtool <youngmtool@gmail.com>
2017-12-07 23:45:12 +0900

    Add a staic class TimeUtilClass which contains 2 static methods.

--- CSharp/AnPhCs6Net46/05/SimpleUtilityClass/Program.cs
+++ CSharp/AnPhCs6Net46/05/SimpleUtilityClass/Program.cs
@@ -5,9 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application SimpleUtilityClass.
+//c Add a staic class TimeUtilClass which contains 2 static methods.
 
 namespace SimpleUtilityClass
 {
+    // Static classes can only
+    // contain static members!
+    static class TimeUtilClass
+    {
+        public static void PrintTime()
+        { Console.WriteLine(DateTime.Now.ToShortTimeString()); }
+
+        public static void PrintDate()
+        { Console.WriteLine(DateTime.Today.ToShortDateString()); }
+    }
+
     class Program
     {
         static void Main(string[] args)
