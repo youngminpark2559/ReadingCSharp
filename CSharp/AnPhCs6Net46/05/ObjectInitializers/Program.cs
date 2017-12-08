abb7c046aa9e1a7a7bc4d1f4ed3b20027da79d14
youngmtool <youngmtool@gmail.com>
2017-12-08 20:28:29 +0900

    Update a method Main(). I instantiate Point(class) type 3 objects and initialize them all in 3 different ways. 1st: default constructor + set object state data via property by property. 2nd: use a custom constructor with passing values which I want to assign to the object state data(private field.) then inside of this custom constructor, it will assign them via property. 3rd: use object initialize syntax.

--- CSharp/AnPhCs6Net46/05/ObjectInitializers/Program.cs
+++ CSharp/AnPhCs6Net46/05/ObjectInitializers/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 //c Create a console application ObjectInitializers.
 //c Add a class Point.
+//c Update a method Main(). I instantiate Point(class) type 3 objects and initialize them all in 3 different ways. 1st: default constructor + set object state data via property by property. 2nd: use a custom constructor with passing values which I want to assign to the object state data(private field.) then inside of this custom constructor, it will assign them via property. 3rd: use object initialize syntax.
 
 namespace ObjectInitializers
 {
@@ -31,6 +32,22 @@ namespace ObjectInitializers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Object Init Syntax *****\n");
+
+            // Make a Point by setting each property manually.
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            // Or make a Point via a custom constructor.
+            Point anotherPoint = new Point(20, 20);
+            anotherPoint.DisplayStats();
+
+            // Or make a Point using object init syntax.
+            Point finalPoint = new Point { X = 30, Y = 30 };
+            finalPoint.DisplayStats();
+            Console.ReadLine();
         }
     }
 }
