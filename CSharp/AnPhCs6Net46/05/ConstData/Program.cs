4180f2e1777ee644e96cdb986bcb65488dfdf772
youngmtool <youngmtool@gmail.com>
2017-12-08 23:38:50 +0900

    Add a class MyMathClass which contains const double data type PI field which is assigned by 3.14 by initialization field on declaration syntaxt. In Main(), I try to manipulate PI but it causes compile time error because const is never changed after it is assigned.

--- CSharp/AnPhCs6Net46/05/ConstData/Program.cs
+++ CSharp/AnPhCs6Net46/05/ConstData/Program.cs
@@ -5,13 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application ConstData.
+//c Add a class MyMathClass which contains const double data type PI field which is assigned by 3.14 by initialization field on declaration syntaxt. In Main(), I try to manipulate PI but it causes compile time error because const is never changed after it is assigned.
 
 namespace ConstData
 {
+    class MyMathClass
+    {
+        public const double PI = 3.14;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Const *****\n");
+            Console.WriteLine("The value of PI is: {0}", MyMathClass.PI);
+            // Error! Can't change a constant!
+            // MyMathClass.PI = 3.1444;
+
+            Console.ReadLine();
         }
     }
 }
