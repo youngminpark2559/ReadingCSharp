605245ff7a9d087a190355cad9d02169b443200f
youngmtool <youngmtool@gmail.com>
2017-12-08 11:06:50 +0900

    Create a new cs file Employee.cs and add a class Employee. I add a private fields and a default constructor and a custom constructor and some methods manipulating data of fields.

---
+++ CSharp/AnPhCs6Net46/05/EmployeeApp/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a new cs file Employee.cs and add a class Employee. I add a private fields and a default constructor and a custom constructor and some methods manipulating data of fields.
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        // Field data.
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        // Constructors.
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            currPay = pay;
+        }
+
+        // Methods.
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+    }
+}
---
+++ CSharp/AnPhCs6Net46/05/EmployeeApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
