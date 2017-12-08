d20171c578b8eeb7d09b583d8d3e4b45d710909e
youngmtool <youngmtool@gmail.com>
2017-12-08 18:50:38 +0900

    Interact with properties by assign data into backing fields of properties by using get/set property.

---
+++ CSharp/AnPhCs6Net46/05/AutoProps/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Interact with properties by assign data into backing fields of properties by using get/set property.
+
+namespace AutoProps
+{
+    class Car
+    {
+        // Automatic properties.
+        // No need to define backing fields.
+        public string PetName { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+
+        // Read-only property? This is OK!
+        public int MyReadOnlyProp { get; }
+        // Write only property? Error!
+        //public int MyWriteOnlyProp { set; }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Car Name: {0}", PetName);
+            Console.WriteLine("Speed: {0}", Speed);
+            Console.WriteLine("Color: {0}", Color);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Automatic Properties *****\n");
+
+            Car c = new Car();
+            c.PetName = "Frank";
+            c.Speed = 55;
+            c.Color = "Red";
+
+            Console.WriteLine("Your car is named {0}? That's odd...",
+              c.PetName);
+            c.DisplayStats();
+
+            Console.ReadLine();
+        }
+    }
+}
