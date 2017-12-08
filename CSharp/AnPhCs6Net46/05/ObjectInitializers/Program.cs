1cbf6db43268deda7aeb0375299a3077c362850e
youngmtool <youngmtool@gmail.com>
2017-12-08 20:12:40 +0900

    Create a console application ObjectInitializers.

---
+++ CSharp/AnPhCs6Net46/05/ObjectInitializers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a console application ObjectInitializers.
+
+namespace ObjectInitializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
