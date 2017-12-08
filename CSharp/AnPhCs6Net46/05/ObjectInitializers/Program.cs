f2f9468b1196102b280439c2e01d0aeba19a4cf0
youngmtool <youngmtool@gmail.com>
2017-12-08 22:50:04 +0900

    Use object initialization syntax for Rectangle and inside of it use object initialization syntax for properties setting the private fields. In other words, I do initialize data by object initialization syntax.

--- CSharp/AnPhCs6Net46/05/ObjectInitializers/Program.cs
+++ CSharp/AnPhCs6Net46/05/ObjectInitializers/Program.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 //c Add a class Point.
 //c Update a method Main(). I instantiate Point(class) type 3 objects and initialize them all in 3 different ways. 1st: default constructor + set object state data via property by property. 2nd: use a custom constructor with passing values which I want to assign to the object state data(private field.) then inside of this custom constructor, it will assign them via property. 3rd: use object initialize syntax.
 //c Add a class Rectangle.
+//c Use object initialization syntax for Rectangle and inside of it use object initialization syntax for properties setting the private fields. In other words, I do initialize data by object initialization syntax.
+
 namespace ObjectInitializers
 {
     enum PointColor
@@ -77,20 +79,27 @@ namespace ObjectInitializers
             firstPoint.X = 10;
             firstPoint.Y = 10;
             firstPoint.DisplayStats();
-            
+
             // Or make a Point via a custom constructor.
             Point anotherPoint = new Point(20, 20);
             anotherPoint.DisplayStats();
-            
+
 
             // Or make a Point using object init syntax.
             Point finalPoint = new Point { X = 30, Y = 30 };
             finalPoint.DisplayStats();
-            
+
 
             // Calling a more interesting custom constructor with init syntax.
             Point goldPoint = new Point(PointColor.Gold) { X = 90, Y = 20 };
             goldPoint.DisplayStats();
+
+            // Create and initialize a Rectangle.
+            Rectangle myRect = new Rectangle
+            {
+                TopLeft = new Point { X = 10, Y = 10 },
+                BottomRight = new Point { X = 200, Y = 200 }
+            };
 
             Console.ReadLine();
         }
