ea3e7a09a09a1634aea33fbaa291a1f6cebabc20
youngmtool <youngmtool@gmail.com>
2017-12-08 22:51:35 +0900

    Create a console application ConstData.

---
+++ CSharp/AnPhCs6Net46/05/ConstData/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a console application ConstData.
+
+namespace ConstData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
