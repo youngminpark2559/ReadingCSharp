24e6f0a9905b7a07cec9ae8eca8ba7d070275934
youngmtool <youngmtool@gmail.com>
2017-12-08 20:23:56 +0900

    Add a class Point.

--- CSharp/AnPhCs6Net46/05/ObjectInitializers/Program.cs
+++ CSharp/AnPhCs6Net46/05/ObjectInitializers/Program.cs
@@ -5,9 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application ObjectInitializers.
+//c Add a class Point.
 
 namespace ObjectInitializers
 {
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xVal, int yVal)
+        {
+            X = xVal;
+            Y = yVal;
+        }
+        public Point() { }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("[{0}, {1}]", X, Y);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
