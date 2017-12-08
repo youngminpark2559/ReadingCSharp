9d8a2221c532809470271dbf96830f1ec166c894
youngmtool <youngmtool@gmail.com>
2017-12-08 11:15:23 +0900

    Update a method Main() by using SetName() but this time I'm trying to manipulate the data of private field with the data which is not allowed to be stored into the private field by the logic acting like a filter inside of SetName().

--- CSharp/AnPhCs6Net46/05/EmployeeApp/Program.cs
+++ CSharp/AnPhCs6Net46/05/EmployeeApp/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 //c Updata a method Main() by trying to manipulate the data of private field empName. But I get the compile-time error because of its access modifier.
 //c Updata a method Main() by using SetName().
+//c Update a method Main() by using SetName() but this time I'm trying to manipulate the data of private field with the data which is not allowed to be stored into the private field by the logic acting like a filter inside of SetName().
 
 namespace EmployeeApp
 {
@@ -24,6 +25,11 @@ namespace EmployeeApp
             // Use the get/set methods to interact with the object's name.
             emp.SetName("Marv");
             Console.WriteLine("Employee is named: {0}", emp.GetName());
+
+            // Longer than 15 characters! Error will print to console.
+            Employee emp2 = new Employee();
+            emp2.SetName("Xena the warrior princess");
+
             Console.ReadLine();
         }
     }
