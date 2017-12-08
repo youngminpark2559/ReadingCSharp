5d525eb01fbbbf5e54b860047938dc8fd57de340
youngmtool <youngmtool@gmail.com>
2017-12-08 11:13:04 +0900

    Updata a method Main() by using SetName().

--- CSharp/AnPhCs6Net46/05/EmployeeApp/Program.cs
+++ CSharp/AnPhCs6Net46/05/EmployeeApp/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Updata a method Main() by trying to manipulate the data of private field empName. But I get the compile-time error because of its access modifier.
+//c Updata a method Main() by using SetName().
 
 namespace EmployeeApp
 {
@@ -12,11 +13,18 @@ namespace EmployeeApp
     {
         static void Main(string[] args)
         {
-            Employee emp = new Employee();
-
             // Error! Cannot directly access private members
             // from an object!
             //emp.empName = "Marv";
+
+            Console.WriteLine("***** Fun with Encapsulation *****\n");
+            Employee emp = new Employee("Marvin", 456, 30000);
+            emp.GiveBonus(1000);
+            emp.DisplayStats();
+            // Use the get/set methods to interact with the object's name.
+            emp.SetName("Marv");
+            Console.WriteLine("Employee is named: {0}", emp.GetName());
+            Console.ReadLine();
         }
     }
 }
