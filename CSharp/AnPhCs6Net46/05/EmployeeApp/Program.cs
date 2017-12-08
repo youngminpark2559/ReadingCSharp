d0ee00aad12199d01833ed52024f411fc4a5063e
youngmtool <youngmtool@gmail.com>
2017-12-08 12:13:41 +0900

    Manipulate the data of private field empName by using set property and retrieve that data by using get property.

--- CSharp/AnPhCs6Net46/05/EmployeeApp/Program.cs
+++ CSharp/AnPhCs6Net46/05/EmployeeApp/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 //c Updata a method Main() by trying to manipulate the data of private field empName. But I get the compile-time error because of its access modifier.
 //c Updata a method Main() by using SetName().
 //c Update a method Main() by using SetName() but this time I'm trying to manipulate the data of private field with the data which is not allowed to be stored into the private field by the logic acting like a filter inside of SetName().
+//c Manipulate the data of private field empName by using set property and retrieve that data by using get property.
 
 namespace EmployeeApp
 {
@@ -29,6 +30,10 @@ namespace EmployeeApp
             // Longer than 15 characters! Error will print to console.
             Employee emp2 = new Employee();
             emp2.SetName("Xena the warrior princess");
+
+            // Reset and then get the Name property.
+            emp.Name = "Marv";
+            Console.WriteLine("Employee is named: {0}", emp.Name);
 
             Console.ReadLine();
         }
