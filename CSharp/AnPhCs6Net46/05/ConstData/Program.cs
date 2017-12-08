9c1f7e6d1849833229732263d8e4f1338b01b359
youngmtool <youngmtool@gmail.com>
2017-12-08 23:41:59 +0900

    Add a static method LocalConstStringVariable() in which it declares const string data type local variable fixedStr and assign initial value to it.

--- CSharp/AnPhCs6Net46/05/ConstData/Program.cs
+++ CSharp/AnPhCs6Net46/05/ConstData/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 //c Create a console application ConstData.
 //c Add a class MyMathClass which contains const double data type PI field which is assigned by 3.14 by initialization field on declaration syntaxt. In Main(), I try to manipulate PI but it causes compile time error because const is never changed after it is assigned.
+//c Add a static method LocalConstStringVariable() in which it declares const string data type local variable fixedStr and assign initial value to it.
 
 namespace ConstData
 {
@@ -23,7 +24,19 @@ namespace ConstData
             // Error! Can't change a constant!
             // MyMathClass.PI = 3.1444;
 
+            LocalConstStringVariable();
+
             Console.ReadLine();
+        }
+
+        static void LocalConstStringVariable()
+        {
+            // A local constant data point can be directly accessed.
+            const string fixedStr = "Fixed string Data";
+            Console.WriteLine(fixedStr);
+
+            // Error!
+            // fixedStr = "This will not work!";
         }
     }
 }
