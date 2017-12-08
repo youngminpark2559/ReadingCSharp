597904c29f6c4ad1d5225acb0713ea0eb0bd2380
youngmtool <youngmtool@gmail.com>
2017-12-08 22:19:37 +0900

    Update

--- CSharp/AnPhCs6Net46/05/ObjectInitializers/Program.cs
+++ CSharp/AnPhCs6Net46/05/ObjectInitializers/Program.cs
@@ -10,21 +10,35 @@ using System.Threading.Tasks;
 
 namespace ObjectInitializers
 {
+    enum PointColor
+    { LightBlue, BloodRed, Gold }
+
     class Point
     {
         public int X { get; set; }
         public int Y { get; set; }
+        public PointColor Color { get; set; }
 
         public Point(int xVal, int yVal)
         {
             X = xVal;
             Y = yVal;
+            Color = PointColor.Gold;
         }
-        public Point() { }
+
+        public Point(PointColor ptColor)
+        {
+            Color = ptColor;
+        }
+
+        public Point()
+          : this(PointColor.BloodRed)
+        { }
 
         public void DisplayStats()
         {
             Console.WriteLine("[{0}, {1}]", X, Y);
+            Console.WriteLine("Point is {0}", Color);
         }
     }
 
@@ -39,14 +53,21 @@ namespace ObjectInitializers
             firstPoint.X = 10;
             firstPoint.Y = 10;
             firstPoint.DisplayStats();
-
+            
             // Or make a Point via a custom constructor.
             Point anotherPoint = new Point(20, 20);
             anotherPoint.DisplayStats();
+            
 
             // Or make a Point using object init syntax.
             Point finalPoint = new Point { X = 30, Y = 30 };
             finalPoint.DisplayStats();
+            
+
+            // Calling a more interesting custom constructor with init syntax.
+            Point goldPoint = new Point(PointColor.Gold) { X = 90, Y = 20 };
+            goldPoint.DisplayStats();
+
             Console.ReadLine();
         }
     }
