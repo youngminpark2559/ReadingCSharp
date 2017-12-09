9a376a270862e6534c1be0d3e4739e1ca0f147c4
youngmtool <youngmtool@gmail.com>
2017-12-09 23:26:12 +0900

    Add a new keyword to string data type field PetName which hides PetName in base class.

--- CSharp/AnPhCs6Net46/06/Shapes/ThreeDCircle.cs
+++ CSharp/AnPhCs6Net46/06/Shapes/ThreeDCircle.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 //c Add a class ThreeDCircle with pretending I get this class from third-party.
 //c Inherit Circle class from ThreeDCircle.
 //c Add a new keyword to explicitly state Draw() in ThreeDCircle hides Draw() in base class Circle, Shape.
-//c Add a new keyword to string data type field PetName which hides PetName in base class.
+//c Add a new keyword to string data type field PetName which hides PetName in base class..
 
 namespace Shapes
 {
