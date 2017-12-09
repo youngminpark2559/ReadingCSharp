2cea3aafc95c83e82780d9c005b4f4a0dd82b18f
youngmtool <youngmtool@gmail.com>
2017-12-09 16:43:43 +0900

    Update a class SalesPerson. I add a overriden method GiveBonus() from the base class.

--- CSharp/AnPhCs6Net46/06/Employees/SalesPerson.cs
+++ CSharp/AnPhCs6Net46/06/Employees/SalesPerson.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Add a sealed class PTSalesPerson derived from SalesPerson class.
+//c Update a class SalesPerson. I add a overriden method GiveBonus() from the base class.
 
 namespace Employees
 {
@@ -34,6 +35,22 @@ namespace Employees
         {
             // This belongs with us!
             SalesNumber = numbOfSales;
+        }
+
+        // A salesperson's bonus is influenced by the number of sales.
+        public override void GiveBonus(float amount)
+        {
+            int salesBonus = 0;
+            if (SalesNumber >= 0 && SalesNumber <= 100)
+                salesBonus = 10;
+            else
+            {
+                if (SalesNumber >= 101 && SalesNumber <= 200)
+                    salesBonus = 15;
+                else
+                    salesBonus = 20;
+            }
+            base.GiveBonus(amount * salesBonus);
         }
     }
 
