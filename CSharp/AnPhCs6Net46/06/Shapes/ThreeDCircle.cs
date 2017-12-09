a1f7c51b3559cede6f981c1e9653f04011674d4c
youngmtool <youngmtool@gmail.com>
2017-12-09 23:24:38 +0900

    Add a new keyword to explicitly state Draw() in ThreeDCircle hides Draw() in base class Circle, Shape.

--- CSharp/AnPhCs6Net46/06/Shapes/ThreeDCircle.cs
+++ CSharp/AnPhCs6Net46/06/Shapes/ThreeDCircle.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 //c Add a class ThreeDCircle with pretending I get this class from third-party.
 //c Inherit Circle class from ThreeDCircle.
+//c Add a new keyword to explicitly state Draw() in ThreeDCircle hides Draw() in base class Circle, Shape.
 
 namespace Shapes
 {
     class ThreeDCircle : Circle
     {
-        public void Draw()
+        public new void Draw()
         {
             Console.WriteLine("Drawing a 3D Circle");
         }
