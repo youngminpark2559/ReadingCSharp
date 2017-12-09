22a7278426d9b7f82673ee3c7657abd71979b5dc
youngmtool <youngmtool@gmail.com>
2017-12-09 22:21:53 +0900

    Add 2 classes Circle and Hexagon which are derived from Shape base class. But Circle class is not overriding Draw() and Hexagon is overriding Draw() by its own logic.

--- CSharp/AnPhCs6Net46/06/Shapes/Program.cs
+++ CSharp/AnPhCs6Net46/06/Shapes/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Add an abstract class Shapes which contains one virtual method.
+//c Add 2 classes Circle and Hexagon which are derived from Shape base class. But Circle class is not overriding Draw() and Hexagon is overriding Draw() by its own logic.
 
 namespace Shapes
 {
@@ -20,6 +21,24 @@ namespace Shapes
         public virtual void Draw()
         {
             Console.WriteLine("Inside Shape.Draw()");
+        }
+    }
+
+    // Circle DOES NOT override Draw().
+    class Circle : Shape
+    {
+        public Circle() { }
+        public Circle(string name) : base(name) { }
+    }
+
+    // Hexagon DOES override Draw().
+    class Hexagon : Shape
+    {
+        public Hexagon() { }
+        public Hexagon(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Hexagon", PetName);
         }
     }
 
