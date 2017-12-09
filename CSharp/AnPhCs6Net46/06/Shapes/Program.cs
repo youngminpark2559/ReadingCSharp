fe87cf6d54801858fb893bb6d82658390f92a7d0
youngmtool <youngmtool@gmail.com>
2017-12-09 23:21:46 +0900

    Add a class ThreeDCircle with pretending I get this class from third-party.

--- CSharp/AnPhCs6Net46/06/Shapes/Program.cs
+++ CSharp/AnPhCs6Net46/06/Shapes/Program.cs
@@ -58,11 +58,15 @@ namespace Shapes
         {
             Console.WriteLine("***** Fun with Polymorphism *****\n");
 
-            Hexagon hex = new Hexagon("Beth");
-            hex.Draw();
-            Circle cir = new Circle("Cindy");
-            // Calls base class implementation!
-            cir.Draw();
+            // Make an array of Shape-compatible objects.
+            Shape[] myShapes = {new Hexagon(), new Circle(), new Hexagon("Mick"), new Circle("Beth"), new Hexagon("Linda")};
+
+            // Loop over each item and interact with the
+            // polymorphic interface.
+            foreach (Shape s in myShapes)
+            {
+                s.Draw();
+            }
             Console.ReadLine();
         }
     }
---
+++ CSharp/AnPhCs6Net46/06/Shapes/ThreeDCircle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a class ThreeDCircle with pretending I get this class from third-party.
+
+namespace Shapes
+{
+    class ThreeDCircle
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a 3D Circle");
+        }
+    }
+}
