db6dceac89fbef44830063e36932e131cacc6390
youngmtool <youngmtool@gmail.com>
2017-12-10 07:35:35 +0900

    Add a method CastingExamples() which stores Derived(class) type object reference into Base(class) type variable by "implicit type cast".

--- CSharp/AnPhCs6Net46/06/Employees/Program.cs
+++ CSharp/AnPhCs6Net46/06/Employees/Program.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 //c Update a method Main(). Via Manager(class) type object derived from Employee, I invoke GetBenefitCost() inherited from its base class Employee with using functionality of BenefitPackage class GetBenefitCost().
 //c Update a method Main() to use a nested BenefitPackageLevel(enum) type inside of a nested BenefitPackage(class) type of Employee(class) type.
 //c Update a method Main(). Via Manager(class) type object, I invoke a derived method GiveBonus() of Employee(class) type. I do same thing via SalesPerson(class) type object. Now the problem is I'm using same bonus logic for all employees such as Manager, SalesPerson, Part-time SalesPerson.
+//c Add a method CastingExamples() which stores Derived(class) type object reference into Base(class) type variable by "implicit type cast".
 
 namespace Employees
 {
@@ -50,6 +51,19 @@ namespace Employees
             fran.DisplayStats();
 
             Console.ReadLine();
+        }
+
+        static void CastingExamples()
+        {
+            // A Manager "is-a" System.Object, so we can
+            // store a Manager reference in an object variable just fine.
+            object frank = new Manager("Frank Zappa", 9, 3000, 40000, "111-11-1111", 5);
+
+            // A Manager "is-an" Employee too.
+            Employee moonUnit = new Manager("MoonUnit Zappa", 2, 3001, 20000, "101-11-1321", 1);
+
+            // A PTSalesPerson "is-a" SalesPerson.
+            SalesPerson jill = new PTSalesPerson("Jill", 834, 3002, 100000, "111-12-1119", 90);
         }
     }
 }
