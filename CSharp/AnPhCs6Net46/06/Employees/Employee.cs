65586099be356ca7e103efd8d68710b49b0123c5
youngmtool <youngmtool@gmail.com>
2017-12-09 14:43:08 +0900

    Add a nested class BenefitPackage inside of Employee class.

--- CSharp/AnPhCs6Net46/06/Employees/Employee.cs
+++ CSharp/AnPhCs6Net46/06/Employees/Employee.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//c Add a nested class BenefitPackage inside of Employee class.
+
 namespace Employees
 {
     abstract partial class Employee
     {
-        
+
         public class BenefitPackage
         {
             public enum BenefitPackageLevel
@@ -23,9 +25,9 @@ namespace Employees
                 return 125.0;
             }
         }
-        
 
-        
+
+
         public virtual void GiveBonus(float amount)
         { Pay += amount; }
 
@@ -37,9 +39,9 @@ namespace Employees
             Console.WriteLine("Pay: {0}", Pay);
             Console.WriteLine("SSN: {0}", SocialSecurityNumber);
         }
-        
 
-        
+
+
         // Accessor (get method)
         public string GetName()
         {
@@ -56,6 +58,6 @@ namespace Employees
             else
                 empName = name;
         }
-        
+
     }
 }
