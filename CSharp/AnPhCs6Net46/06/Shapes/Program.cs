37aca152e2b28f21bed904ad3fd408fbe44a0fe1
youngmtool <youngmtool@gmail.com>
2017-12-09 22:18:44 +0900

    Add an abstract class Shapes which contains one virtual method.

---
+++ CSharp/AnPhCs6Net46/06/Shapes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add an abstract class Shapes which contains one virtual method.
+
+namespace Shapes
+{
+    // The abstract base class of the hierarchy.
+    abstract class Shape
+    {
+        public Shape(string name = "NoName")
+        { PetName = name; }
+
+        public string PetName { get; set; }
+
+        // A single virtual method.
+        public virtual void Draw()
+        {
+            Console.WriteLine("Inside Shape.Draw()");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
