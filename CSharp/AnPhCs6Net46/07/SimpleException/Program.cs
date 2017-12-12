e51fa250b8802e1bcec08a7df773d9625969fd1b
youngmtool <youngmtool@gmail.com>
2017-12-12 10:51:12 +0900

    Add a class Radio. It has a method TurnOn() functioning depeding on a boolean data type argument.

---
+++ CSharp/AnPhCs6Net46/07/SimpleException/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a class Radio. It has a method TurnOn() functioning depeding on a boolean data type argument.
+
+namespace SimpleException
+{
+    class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            if (on)
+                Console.WriteLine("Jamming...");
+            else
+                Console.WriteLine("Quiet time...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
