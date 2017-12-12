c47f951c09fe9a86c600f3e416c00ae0b8bf4ec5
youngmtool <youngmtool@gmail.com>
2017-12-12 19:15:31 +0900

    Update a method Main(). I instantiate Car instance and I invoke Accelerate() and if exception is occurred I use a custom exception CarIsDeadException exception I defined and put the invoking code in Accelerate().

--- CSharp/AnPhCs6Net46/07/CustomException/Program.cs
+++ CSharp/AnPhCs6Net46/07/CustomException/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 //c Add a class CarIsDeadException which will play a role of a custom exception. I can derive this class from ApplicationException class or simply Exception class to make this a custom exception. ApplicationException class contains simple constructor and by this class I can specify this exception is application exception not system exception thrown by CLR runtime.
 //c Update a method Accelerate(). I use a custom exception by CarIsDeadException rather than a built-in excetion by Exception class. But note that I also can use Exception class in catch clause.
+//c Update a method Main(). I instantiate Car instance and I invoke Accelerate() and if exception is occurred I use a custom exception CarIsDeadException exception I defined and put the invoking code in Accelerate().
 
 namespace CustomException
 {
@@ -104,6 +105,21 @@ namespace CustomException
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Custom Exceptions *****\n");
+            Car myCar = new Car("Rusty", 90);
+
+            try
+            {
+                // Trip exception.
+                myCar.Accelerate(50);
+            }
+            catch (CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ErrorTimeStamp);
+                Console.WriteLine(e.CauseOfError);
+            }
+            Console.ReadLine();
         }
     }
 }
