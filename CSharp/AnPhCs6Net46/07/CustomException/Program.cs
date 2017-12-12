7afabaf3c5c4901a82dc350c7a8df44e41a120c7
youngmtool <youngmtool@gmail.com>
2017-12-12 19:32:01 +0900

    Update a class CarIsDeadException. I use a custom constructor to use detailed message for this exception by passing this string data type value message to the base class (ApplicationException(class) type)'s constructor. In this way, I don't need to have a property for the message(detailedMessage).

--- CSharp/AnPhCs6Net46/07/CustomException/Program.cs
+++ CSharp/AnPhCs6Net46/07/CustomException/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 //c Add a class CarIsDeadException which will play a role of a custom exception. I can derive this class from ApplicationException class or simply Exception class to make this a custom exception. ApplicationException class contains simple constructor and by this class I can specify this exception is application exception not system exception thrown by CLR runtime.
 //c Update a method Accelerate(). I use a custom exception by CarIsDeadException rather than a built-in excetion by Exception class. But note that I also can use Exception class in catch clause.
 //c Update a method Main(). I instantiate Car instance and I invoke Accelerate() and if exception is occurred I use a custom exception CarIsDeadException exception I defined and put the invoking code in Accelerate().
+//c Update a class CarIsDeadException. I use a custom constructor to use detailed message for this exception by passing this string data type value message to the base class (ApplicationException(class) type)'s constructor. In this way, I don't need to have a property for the message(detailedMessage).
 
 namespace CustomException
 {
@@ -74,30 +75,19 @@ namespace CustomException
         }
     }
 
-    // This custom exception describes the details of the car-is-dead condition.
-    // (Remember, you can also simply extend Exception.)
     public class CarIsDeadException : ApplicationException
     {
-        private string messageDetails = String.Empty;
         public DateTime ErrorTimeStamp { get; set; }
         public string CauseOfError { get; set; }
 
         public CarIsDeadException() { }
-        public CarIsDeadException(string message,
-          string cause, DateTime time)
+
+        // Feed message to parent constructor.
+        public CarIsDeadException(string message, string cause, DateTime time)
+          : base(message)
         {
-            messageDetails = message;
             CauseOfError = cause;
             ErrorTimeStamp = time;
-        }
-
-        // Override the Exception.Message property.
-        public override string Message
-        {
-            get
-            {
-                return string.Format("Car Error Message: {0}", messageDetails);
-            }
         }
     }
 
