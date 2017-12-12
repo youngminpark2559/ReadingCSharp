770a5291599410bbdd70e591a69308c892d9fcfb
youngmtool <youngmtool@gmail.com>
2017-12-12 19:09:40 +0900

    Add a class CarIsDeadException which will play a role of a custom exception. I can derive this class from ApplicationException class or simply Exception class to make this a custom exception. ApplicationException class contains simple constructor and by this class I can specify this exception is application exception not system exception thrown by CLR runtime.

---
+++ CSharp/AnPhCs6Net46/07/CustomException/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a class CarIsDeadException which will play a role of a custom exception. I can derive this class from ApplicationException class or simply Exception class to make this a custom exception. ApplicationException class contains simple constructor and by this class I can specify this exception is application exception not system exception thrown by CLR runtime.
+
+namespace CustomException
+{
+    class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            if (on)
+                Console.WriteLine("Jamming...");
+            else
+                Console.WriteLine("Quiet time...");
+        }
+    }
+
+    class Car
+    {
+        // Constant for maximum speed.
+        public const int MaxSpeed = 100;
+
+        // Car properties.
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        // Is the car still operational?
+        private bool carIsDead;
+
+        // A car has-a radio.
+        private Radio theMusicBox = new Radio();
+
+        // Constructors.
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public void CrankTunes(bool state)
+        {
+            // Delegate request to inner object.
+            theMusicBox.TurnOn(state);
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                    CurrentSpeed = 0;
+                    // We need to call the HelpLink property, thus we need to
+                    // create a local variable before throwing the Exception object.
+                    Exception ex =
+                      new Exception(string.Format("{0} has overheated!", PetName));
+                    ex.HelpLink = "http://www.CarsRUs.com";
+                    // Stuff in custom data regarding the error.
+                    ex.Data.Add("TimeStamp", string.Format("The car exploded at {0}", DateTime.Now));
+                    ex.Data.Add("Cause", "You have a lead foot.");
+                    throw ex;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+
+    // This custom exception describes the details of the car-is-dead condition.
+    // (Remember, you can also simply extend Exception.)
+    public class CarIsDeadException : ApplicationException
+    {
+        private string messageDetails = String.Empty;
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+
+        public CarIsDeadException() { }
+        public CarIsDeadException(string message,
+          string cause, DateTime time)
+        {
+            messageDetails = message;
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+
+        // Override the Exception.Message property.
+        public override string Message
+        {
+            get
+            {
+                return string.Format("Car Error Message: {0}", messageDetails);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
