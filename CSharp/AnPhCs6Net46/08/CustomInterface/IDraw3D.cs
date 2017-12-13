d949c7b3a8d5e42f17bd0e64a88b5e5a12026665
youngmtool <youngmtool@gmail.com>
2017-12-13 16:15:32 +0900

    Add an interface IDraw3D which defines Draw3D().

---
+++ CSharp/AnPhCs6Net46/08/CustomInterface/IDraw3D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add an interface IDraw3D which defines Draw3D().
+
+namespace CustomInterface
+{
+    // Models the ability to render a type in stunning 3D.
+    public interface IDraw3D
+    {
+        void Draw3D();
+    }
+}
