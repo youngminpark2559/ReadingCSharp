818cbf5d673624f78136c68bdcdf66b3920ae32e
youngmtool <youngmtool@gmail.com>
2017-12-13 11:46:44 +0900

    Update a interface IPointy to test what the interface type can't have. Interface type can't have field, can't have a constructor, can't have an implementation of interface's members.

--- CSharp/AnPhCs6Net46/08/CustomInterface/IPointy.cs
+++ CSharp/AnPhCs6Net46/08/CustomInterface/IPointy.cs
@@ -5,13 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Add a interface IPointy which defines one unimplemented method GetNumberOfPoints().
+//c Update a interface IPointy to test what the interface type can't have. Interface type can't have field, can't have a constructor, can't have an implementation of interface's members.
 
 namespace CustomInterface
 {
     // This interface defines the behavior of "having points."
+    // Ack! Errors abound!
     public interface IPointy
     {
-        // Implicitly public and abstract.
-        byte GetNumberOfPoints();
+        // Error! Interfaces cannot have data fields!
+        public int numbOfPoints;
+
+        // Error! Interfaces do not have constructors!
+        public IPointy() { numbOfPoints = 0; }
+
+        // Error! Interfaces don't provide an implementation of members!
+        byte GetNumberOfPoints() { return numbOfPoints; }
     }
 }
