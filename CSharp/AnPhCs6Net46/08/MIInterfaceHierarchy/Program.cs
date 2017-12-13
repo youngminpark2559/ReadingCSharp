2a2adc673ce7ee24b6cd16106b812f8c6403e1e8
youngmtool <youngmtool@gmail.com>
2017-12-13 21:25:54 +0900

    Create a console application MIInterfaceHierarchy to examine multiple interface type inheritance.

---
+++ CSharp/AnPhCs6Net46/08/MIInterfaceHierarchy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a console application MIInterfaceHierarchy to examine multiple interface type inheritance.
+
+namespace MIInterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
