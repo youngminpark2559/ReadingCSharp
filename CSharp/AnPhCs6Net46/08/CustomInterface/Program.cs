8cd85ba07ef6992baad71801aa176cfc77b7ab5a
youngmtool <youngmtool@gmail.com>
2017-12-13 13:48:34 +0900

    Update a method Main(). After printing hex.Points, I instantiate Circle instance c and I try to explicit type cast from Circle(class) type to IPointy(interface) type. But This try is not possible because IPointy and Circle have no relationship between them. In other words, they're not type compatable to each other. So this try will trigger the exception with InvalidCastException(class) type object and this instance is caught by catch clause and print the exception object's data as backing field of Message property.

--- CSharp/AnPhCs6Net46/08/CustomInterface/Program.cs
+++ CSharp/AnPhCs6Net46/08/CustomInterface/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//c Update a method Main(). I instantiate Hexagon instance and access to Points property.
+//c Update a method Main(). I instantiate Hexagon instance hex and access to Points property.
+//c Update a method Main(). After printing hex.Points, I instantiate Circle instance c and I try to explicit type cast from Circle(class) type to IPointy(interface) type. But This try is not possible because IPointy and Circle have no relationship between them. In other words, they're not type compatable to each other. So this try will trigger the exception with InvalidCastException(class) type object and this instance is caught by catch clause and print the exception object's data as backing field of Message property.
 
 namespace CustomInterface
 {
@@ -16,6 +17,20 @@ namespace CustomInterface
             // Call Points property defined by IPointy.
             Hexagon hex = new Hexagon();
             Console.WriteLine("Points: {0}", hex.Points);
+
+            // Catch a possible InvalidCastException.
+            Circle c = new Circle("Lisa");
+            IPointy itfPt = null;
+            try
+            {
+                itfPt = (IPointy)c;
+                Console.WriteLine(itfPt.Points);
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
             Console.ReadLine();
         }
     }
