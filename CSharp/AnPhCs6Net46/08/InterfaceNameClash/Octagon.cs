da37bf5947b13c93aef360cac7df69d8e11d7320
youngmtool <youngmtool@gmail.com>
2017-12-13 19:06:59 +0900

    Add a class Octagon which implements 3 interfaces such as IDrawToForm, IDrawToMemory, IDrawToPrinter. And Octagon implements Draw().

---
+++ CSharp/AnPhCs6Net46/08/InterfaceNameClash/Octagon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a class Octagon which implements 3 interfaces such as IDrawToForm, IDrawToMemory, IDrawToPrinter. And Octagon implements Draw().
+
+namespace InterfaceNameClash
+{
+    class Octagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        public void Draw()
+        {
+            // Shared drawing logic.
+            Console.WriteLine("Drawing the Octagon...");
+        }
+    }
+}
