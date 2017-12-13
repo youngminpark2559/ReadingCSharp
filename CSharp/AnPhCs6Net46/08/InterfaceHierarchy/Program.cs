52103824fbf730c84646a65212d7420588aa7891
youngmtool <youngmtool@gmail.com>
2017-12-13 20:26:48 +0900

    Update a method Main(). I use 3 methods implemented from 2 base interfaces(one explicitly, one implicitly).

--- CSharp/AnPhCs6Net46/08/InterfaceHierarchy/Program.cs
+++ CSharp/AnPhCs6Net46/08/InterfaceHierarchy/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//c Update a method Main(). I use 3 methods implemented from 2 base interfaces(one explicitly, one implicitly).
+
 namespace InterfaceHierarchy
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Simple Interface Hierarchy *****");
+
+            // Call from object level.
+            BitmapImage myBitmap = new BitmapImage();
+            myBitmap.Draw();
+            myBitmap.DrawInBoundingBox(10, 10, 100, 150);
+            myBitmap.DrawUpsideDown();
+
+            // Get IAdvancedDraw explicitly.
+            IAdvancedDraw iAdvDraw = myBitmap as IAdvancedDraw;
+            if (iAdvDraw != null)
+                iAdvDraw.DrawUpsideDown();
+            Console.ReadLine();
         }
     }
 }
