c5e6a5466f47bc57858bf9eac8da2f6e891de798
youngmtool <youngmtool@gmail.com>
2017-12-13 19:41:00 +0900

    Test code before using "explicit interface member implementation syntax".

--- CSharp/AnPhCs6Net46/08/InterfaceNameClash/Program.cs
+++ CSharp/AnPhCs6Net46/08/InterfaceNameClash/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application InterfaceNameClash and add 3 interfaces.
+//c Test code before using "explicit interface member implementation syntax".
 
 namespace InterfaceNameClash
 {
@@ -12,6 +13,20 @@ namespace InterfaceNameClash
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Interface Name Clashes *****\n");
+            // All of these invocations call the
+            // same Draw() method!
+            Octagon oct = new Octagon();
+            IDrawToForm itfForm = (IDrawToForm)oct;
+            itfForm.Draw();
+
+            IDrawToPrinter itfPriner = (IDrawToPrinter)oct;
+            itfPriner.Draw();
+
+            IDrawToMemory itfMemory = (IDrawToMemory)oct;
+            itfMemory.Draw();
+
+            Console.ReadLine();
         }
     }
 }
