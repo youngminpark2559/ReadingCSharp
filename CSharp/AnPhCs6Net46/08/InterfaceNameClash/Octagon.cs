f31c922fa2588fe291edd3d06a4ea125cfb02c0c
youngmtool <youngmtool@gmail.com>
2017-12-13 19:46:19 +0900

    Update a class Octagon. I implement Draw() from each interface by using "explicit interface member implementation syntax".

--- CSharp/AnPhCs6Net46/08/InterfaceNameClash/Octagon.cs
+++ CSharp/AnPhCs6Net46/08/InterfaceNameClash/Octagon.cs
@@ -5,15 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Add a class Octagon which implements 3 interfaces such as IDrawToForm, IDrawToMemory, IDrawToPrinter. And Octagon implements Draw().
+//c Update a class Octagon. I implement Draw() from each interface by using "explicit interface member implementation syntax".
 
 namespace InterfaceNameClash
 {
     class Octagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
     {
-        public void Draw()
+        // Explicitly bind Draw() implementations
+        // to a given interface.
+        void IDrawToForm.Draw()
         {
-            // Shared drawing logic.
-            Console.WriteLine("Drawing the Octagon...");
+            Console.WriteLine("Drawing to form...");
+        }
+        void IDrawToMemory.Draw()
+        {
+            Console.WriteLine("Drawing to memory...");
+        }
+        void IDrawToPrinter.Draw()
+        {
+            Console.WriteLine("Drawing to a printer...");
         }
     }
 }
