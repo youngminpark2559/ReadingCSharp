bbef42e485bb25f6ea2ac151eba139e6f4042d63
youngmtool <youngmtool@gmail.com>
2017-12-13 20:21:45 +0900

    Add a interface IDrawable which defines Draw().

---
+++ CSharp/AnPhCs6Net46/08/InterfaceHierarchy/IDrawable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a interface IDrawable which defines Draw().
+
+namespace InterfaceHierarchy
+{
+    public interface IDrawable
+    {
+        void Draw();
+    }
+}
