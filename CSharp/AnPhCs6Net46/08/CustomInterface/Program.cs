f55fa1a8a155e7a42feafa000dc17951576f4319
youngmtool <youngmtool@gmail.com>
2017-12-13 13:43:21 +0900

    Update a method Main(). I instantiate Hexagon instance and access to Points property.

--- CSharp/AnPhCs6Net46/08/CustomInterface/Program.cs
+++ CSharp/AnPhCs6Net46/08/CustomInterface/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//c Update a method Main(). I instantiate Hexagon instance and access to Points property.
+
 namespace CustomInterface
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Interfaces *****\n");
+            // Call Points property defined by IPointy.
+            Hexagon hex = new Hexagon();
+            Console.WriteLine("Points: {0}", hex.Points);
+            Console.ReadLine();
         }
     }
 }
