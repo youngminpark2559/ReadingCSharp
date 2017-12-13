9a28bf4223a76fc7506e9d4157399fc8e3e4d149
youngmtool <youngmtool@gmail.com>
2017-12-13 16:19:37 +0900

    Update a class Hexagon by implementing an IDraw3D(interface) type and also implementing Draw3D() from IDraw3D.

--- CSharp/AnPhCs6Net46/08/CustomInterface/Hexagon.cs
+++ CSharp/AnPhCs6Net46/08/CustomInterface/Hexagon.cs
@@ -8,11 +8,12 @@ using System.Threading.Tasks;
 //c Inherit Circle class from ThreeDCircle.
 //c Add a new keyword to explicitly state Draw() in ThreeDCircle hides Draw() in base class Circle, Shape.
 //c Add a new keyword to string data type field PetName which hides PetName in base class..
+//c Update a class Hexagon by implementing an IDraw3D(interface) type and also implementing Draw3D() from IDraw3D.
 
 namespace CustomInterface
 {
     // Hexagon now implements IPointy.
-    class Hexagon : Shape, IPointy
+    class Hexagon : Shape, IPointy, IDraw3D
     {
         public Hexagon() { }
         public Hexagon(string name) : base(name) { }
@@ -23,6 +24,11 @@ namespace CustomInterface
         public byte Points
         {
             get { return 6; }
+        }
+
+        public void Draw3D()
+        {
+            Console.WriteLine("Drawing Hexagon in 3D!");
         }
     }
 }
