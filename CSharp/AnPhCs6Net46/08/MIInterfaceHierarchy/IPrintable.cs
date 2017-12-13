97a2218da356473d3b740439f585e191804f8e99
youngmtool <youngmtool@gmail.com>
2017-12-13 21:43:06 +0900

    Add 3 interfaces. One inherits 2 base interfaces. Rectangle class implments one interface then it means that this class implements 3 interfaces by inheritance of interface.

---
+++ CSharp/AnPhCs6Net46/08/MIInterfaceHierarchy/IPrintable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIInterfaceHierarchy
+{
+    interface IPrintable
+    {
+        void Print();
+        // Note possible name clash here!
+        void Draw(); 
+    }
+}
--- CSharp/AnPhCs6Net46/08/MIInterfaceHierarchy/Program.cs
+++ CSharp/AnPhCs6Net46/08/MIInterfaceHierarchy/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application MIInterfaceHierarchy to examine multiple interface type inheritance.
+//c Add 3 interfaces. One inherits 2 base interfaces. Rectangle class implments one interface then it means that this class implements 3 interfaces by inheritance of interface.
 
 namespace MIInterfaceHierarchy
 {
