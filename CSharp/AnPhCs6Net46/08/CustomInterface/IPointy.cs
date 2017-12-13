b63b5d14c61c86ef8dca163b0215ce64724065dc
youngmtool <youngmtool@gmail.com>
2017-12-13 11:44:51 +0900

    Add a interface IPointy which defines one unimplemented method GetNumberOfPoints().

---
+++ CSharp/AnPhCs6Net46/08/CustomInterface/IPointy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a interface IPointy which defines one unimplemented method GetNumberOfPoints().
+
+namespace CustomInterface
+{
+    // This interface defines the behavior of "having points."
+    public interface IPointy
+    {
+        // Implicitly public and abstract.
+        byte GetNumberOfPoints();
+    }
+}
---
+++ CSharp/AnPhCs6Net46/08/CustomInterface/Shapes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface
+{
+    // The abstract base class of the hierarchy.
+    abstract class Shape
+    {
+        public Shape(string name = "NoName")
+        { PetName = name; }
+
+        public string PetName { get; set; }
+
+        //// A single virtual method.
+        //public virtual void Draw()
+        //{
+        //    Console.WriteLine("Inside Shape.Draw()");
+        //}
+
+        // Force all child classes to define how to be rendered.
+        public abstract void Draw();
+    }
+}
