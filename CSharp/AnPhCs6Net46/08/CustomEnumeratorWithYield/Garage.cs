cb845c79242b64a4d395207b0def1539863baacc
youngmtool <youngmtool@gmail.com>
2017-12-14 08:58:34 +0900

    Create a console application CustomEnumeratorWithYield to examine the "yield keyword".

---
+++ CSharp/AnPhCs6Net46/08/CustomEnumeratorWithYield/Garage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumeratorWithYield
+{
+    public class Garage : IEnumerable
+    {
+        // System.Array already implements IEnumerator!
+        private Car[] carArray = new Car[4];
+
+        public Garage()
+        {
+            carArray[0] = new Car("FeeFee", 200);
+            carArray[1] = new Car("Clunker", 90);
+            carArray[2] = new Car("Zippy", 30);
+            carArray[3] = new Car("Fred", 30);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            // Return the array object's IEnumerator.
+            return carArray.GetEnumerator();
+            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            // Return the array object's IEnumerator.
+            return carArray.GetEnumerator();
+        }
+    }
+}
---
+++ CSharp/AnPhCs6Net46/08/CustomEnumeratorWithYield/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a console application CustomEnumeratorWithYield to examine the "yield keyword".
+
+namespace CustomEnumeratorWithYield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
