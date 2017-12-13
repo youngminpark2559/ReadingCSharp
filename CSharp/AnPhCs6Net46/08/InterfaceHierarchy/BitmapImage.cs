aa8d98ecdcdcc9a8587feb22ae9c365918ffca71
youngmtool <youngmtool@gmail.com>
2017-12-13 20:25:09 +0900

    Add a class BitmapImage which implements IAdvancedDraw and implements Draw() from IDrawable and DrawInBoundingBox(), DrawUpsideDown() from IAdvancedDraw.

---
+++ CSharp/AnPhCs6Net46/08/InterfaceHierarchy/BitmapImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a class BitmapImage which implements IAdvancedDraw and implements Draw() from IDrawable and DrawInBoundingBox(), DrawUpsideDown() from IAdvancedDraw.
+
+namespace InterfaceHierarchy
+{
+    public class BitmapImage : IAdvancedDraw
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing...");
+        }
+
+        public void DrawInBoundingBox(int top, int left, int bottom, int right)
+        {
+            Console.WriteLine("Drawing in a box...");
+        }
+
+        public void DrawUpsideDown()
+        {
+            Console.WriteLine("Drawing upside down!");
+        }
+    }
+}
