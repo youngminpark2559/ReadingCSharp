b11253a1ea6f8206e49b1140f792f40b1a22ab4e
youngmtool <youngmtool@gmail.com>
2017-12-13 17:51:49 +0900

    Add a class PointyTestClass which implements IPointy and implements members from IPointy by using VS tool.

---
+++ CSharp/AnPhCs6Net46/08/CustomInterface/PointyTestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a class PointyTestClass which implements IPointy and implements members from IPointy by using VS tool.
+
+namespace CustomInterface
+{
+    class PointyTestClass : IPointy
+    {
+        public byte Points
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+    }
+}
