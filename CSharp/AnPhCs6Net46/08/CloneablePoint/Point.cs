0c02ed37242e1aeaa38ed7e4cb616fe24830a1ff
youngmtool <youngmtool@gmail.com>
2017-12-14 10:40:10 +0900

    Add a class Point.

---
+++ CSharp/AnPhCs6Net46/08/CloneablePoint/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a class Point.
+
+namespace CloneablePoint
+{
+    // A class named Point.
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xPos, int yPos) { X = xPos; Y = yPos; }
+        public Point() { }
+
+        // Override Object.ToString().
+        public override string ToString()
+        { return string.Format("X = {0}; Y = {1}", X, Y); }
+    }
+}
