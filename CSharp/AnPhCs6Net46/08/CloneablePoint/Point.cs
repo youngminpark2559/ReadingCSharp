92ffef1ffaac8e09284b0b13825d60b8430860b4
youngmtool <youngmtool@gmail.com>
2017-12-14 10:49:30 +0900

    Refactor a method Clone(). I'm copying only value type variable data(int x = 100) I can simplify this method by using MemberwiseClone(). But If there's a reference type variable data(Person x = person1) this approach results in just a shallow copy so you should be careful.

--- CSharp/AnPhCs6Net46/08/CloneablePoint/Point.cs
+++ CSharp/AnPhCs6Net46/08/CloneablePoint/Point.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 //c Add a class Point.
 //c Update a class Point to derive from ICloneable. And I implement Clone() from ICloneable to internally instantiate new Point type object and copy the values of p1's fields to p2's fields and return this instance's reference to the caller.
+//c Refactor a method Clone(). I'm copying only value type variable data(int x = 100) I can simplify this method by using MemberwiseClone(). But If there's a reference type variable data(Person x = person1) this approach results in just a shallow copy so you should be careful.
 
 namespace CloneablePoint
 {
@@ -22,8 +23,14 @@ namespace CloneablePoint
         public override string ToString()
         { return string.Format("X = {0}; Y = {1}", X, Y); }
 
-        // Return a copy of the current object.
+        //// Return a copy of the current object.
+        //public object Clone()
+        //{ return new Point(this.X, this.Y); }
+
         public object Clone()
-        { return new Point(this.X, this.Y); }
+        {
+            // Copy each field of the Point member by member.
+            return this.MemberwiseClone();
+        }
     }
 }
