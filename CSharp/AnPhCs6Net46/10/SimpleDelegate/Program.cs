90ae9e9fdc5135f8261bb542ff52bdd02b8120c2
youngmtool <youngmtool@gmail.com>
2017-12-14 21:52:05 +0900

    Add a method SquareNumber(). I make BinaryOp(delegate) type instance point to SquareNumber(). But it fails with showing compile time error because the method signature BinaryOp(delegate) type object pointing to is not compatable to the method signature of SquareNumber().

--- CSharp/AnPhCs6Net46/10/SimpleDelegate/Program.cs
+++ CSharp/AnPhCs6Net46/10/SimpleDelegate/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application SimpleDelegate. It contains BinaryOp(delegate) type being able to point to any method taking 2 parameters in Int32(struct) data type, returning Int32 data type value. It also contains SimpleMath(class) type containing two method matched to what BinaryOp(delegate) type points to.
+//c Add a method SquareNumber(). I make BinaryOp(delegate) type instance point to SquareNumber(). But it fails with showing compile time error because the method signature BinaryOp(delegate) type object pointing to is not compatable to the method signature of SquareNumber().
 
 namespace SimpleDelegate
 {
@@ -20,6 +21,8 @@ namespace SimpleDelegate
         { return x + y; }
         public static int Subtract(int x, int y)
         { return x - y; }
+        public static int SquareNumber(int a)
+        { return a * a; }
     }
 
     class Program
@@ -33,6 +36,10 @@ namespace SimpleDelegate
             BinaryOp b = new BinaryOp(SimpleMath.Add);
             // Invoke Add() method indirectly using delegate object.
             Console.WriteLine("10 + 10 is {0}", b(10, 10));
+
+            // Compiler error! Method does not match delegate pattern!
+            //BinaryOp b2 = new BinaryOp(SimpleMath.SquareNumber);
+
             Console.ReadLine();
         }
     }
