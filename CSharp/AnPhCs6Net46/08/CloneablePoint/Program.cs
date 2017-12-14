dca9e4acb582d76ae1433ca26f481d7b492bedd5
youngmtool <youngmtool@gmail.com>
2017-12-14 10:46:17 +0900

    Update a method Main() by deep copying by using Clone() which I implemented in Point(class) type.

--- CSharp/AnPhCs6Net46/08/CloneablePoint/Program.cs
+++ CSharp/AnPhCs6Net46/08/CloneablePoint/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 //c Create a console application CloneablePoint to examine ICloneable.
 //c Update a method Main() by shallow copying instance p1 to p2 and I try to change the value of p2.X to 0 and it will also change the value of p1.X to 0 because p2 is shallow copied.
+//c Update a method Main() by deep copying by using Clone() which I implemented in Point(class) type.
 
 namespace CloneablePoint
 {
@@ -14,12 +15,17 @@ namespace CloneablePoint
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Object Cloning *****\n");
-            // Two references to same object!
-            Point p1 = new Point(50, 50);
-            Point p2 = p1;
-            p2.X = 0;
-            Console.WriteLine(p1);
-            Console.WriteLine(p2);
+            // Notice Clone() returns a plain object type.
+            // You must perform an explicit cast to obtain the derived type.
+            Point p3 = new Point(100, 100);
+            Point p4 = (Point)p3.Clone();
+
+            // Change p4.X (which will not change p3.X).
+            p4.X = 0;
+
+            // Print each object.
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
             Console.ReadLine();
         }
     }
