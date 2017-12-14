506c18026fee2267649c3f39bb06934609ba582c
youngmtool <youngmtool@gmail.com>
2017-12-14 21:48:25 +0900

    Create a console application SimpleDelegate. It contains BinaryOp(delegate) type being able to point to any method taking 2 parameters in Int32(struct) data type, returning Int32 data type value. It also contains SimpleMath(class) type containing two method matched to what BinaryOp(delegate) type points to.

---
+++ CSharp/AnPhCs6Net46/10/SimpleDelegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a console application SimpleDelegate. It contains BinaryOp(delegate) type being able to point to any method taking 2 parameters in Int32(struct) data type, returning Int32 data type value. It also contains SimpleMath(class) type containing two method matched to what BinaryOp(delegate) type points to.
+
+namespace SimpleDelegate
+{
+    // This delegate can point to any method,
+    // taking two integers and returning an integer.
+    public delegate int BinaryOp(int x, int y);
+
+    // This class contains methods BinaryOp will
+    // point to.
+    public class SimpleMath
+    {
+        public static int Add(int x, int y)
+        { return x + y; }
+        public static int Subtract(int x, int y)
+        { return x - y; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple Delegate Example *****\n");
+
+            // Create a BinaryOp delegate object that
+            // "points to" SimpleMath.Add().
+            BinaryOp b = new BinaryOp(SimpleMath.Add);
+            // Invoke Add() method indirectly using delegate object.
+            Console.WriteLine("10 + 10 is {0}", b(10, 10));
+            Console.ReadLine();
+        }
+    }
+}
