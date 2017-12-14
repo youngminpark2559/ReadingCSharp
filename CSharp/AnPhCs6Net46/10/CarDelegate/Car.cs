5ef7f6b6df23be06d3c24aa70f0830b1e9262756
youngmtool <youngmtool@gmail.com>
2017-12-15 08:33:28 +0900

    Add a Car(class) type.

---
+++ CSharp/AnPhCs6Net46/10/CarDelegate/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a Car(class) type.
+
+namespace CarDelegate
+{
+    public class Car
+    {
+        // Internal state data.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+
+        // Is the car alive or dead?
+        private bool carIsDead;
+
+        // Class constructors.
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+    }
+}
