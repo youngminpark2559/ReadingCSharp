16d9c744cf3764983748b7928224704b9b5186c4
youngmtool <youngmtool@gmail.com>
2017-12-14 09:04:15 +0900

    Update a class Garage. I update a method GetEnumerator() using multiple "yield return syntax" without using foreach iteration loop.

--- CSharp/AnPhCs6Net46/08/CustomEnumeratorWithYield/Garage.cs
+++ CSharp/AnPhCs6Net46/08/CustomEnumeratorWithYield/Garage.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Update a class Garage. I add a method GetEnumerator() using "yield return syntax" inside of foreach iteration loop.
+//c Update a class Garage. I update a method GetEnumerator() using multiple "yield return syntax" without using foreach iteration loop.
 
 namespace CustomEnumeratorWithYield
 {
@@ -35,13 +36,21 @@ namespace CustomEnumeratorWithYield
             return carArray.GetEnumerator();
         }
 
-        // Iterator method.
+        //// Iterator method.
+        //public IEnumerator GetEnumerator()
+        //{
+        //    foreach (Car c in carArray)
+        //    {
+        //        yield return c;
+        //    }
+        //}
+
         public IEnumerator GetEnumerator()
         {
-            foreach (Car c in carArray)
-            {
-                yield return c;
-            }
+            yield return carArray[0];
+            yield return carArray[1];
+            yield return carArray[2];
+            yield return carArray[3];
         }
     }
 }
