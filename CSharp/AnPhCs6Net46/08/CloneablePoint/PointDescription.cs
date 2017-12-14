700735868edd3236dfd3524d4579fd35d22c99b1
youngmtool <youngmtool@gmail.com>
2017-12-14 11:46:21 +0900

    Add a class PointDescription which will be used as a data stored in a reference type field in the Point type.

---
+++ CSharp/AnPhCs6Net46/08/CloneablePoint/PointDescription.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a class PointDescription which will be used as a data stored in a reference type field in the Point type.
+
+namespace CloneablePoint
+{
+    // This class describes a point.
+    public class PointDescription
+    {
+        public string PetName { get; set; }
+        public Guid PointID { get; set; }
+
+        public PointDescription()
+        {
+            PetName = "No-name";
+            PointID = Guid.NewGuid();
+        }
+    }
+}
