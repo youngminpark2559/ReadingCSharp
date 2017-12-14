758aba538e9f01706da0d4765f4cf52fa82af552
youngmtool <youngmtool@gmail.com>
2017-12-14 11:49:10 +0900

    Update a class Point. I add a PointDescription(class) type field desc. And I add a custom constructor and override ToString() to make the desc used.

--- CSharp/AnPhCs6Net46/08/CloneablePoint/Point.cs
+++ CSharp/AnPhCs6Net46/08/CloneablePoint/Point.cs
@@ -7,30 +7,36 @@ using System.Threading.Tasks;
 //c Add a class Point.
 //c Update a class Point to derive from ICloneable. And I implement Clone() from ICloneable to internally instantiate new Point type object and copy the values of p1's fields to p2's fields and return this instance's reference to the caller.
 //c Refactor a method Clone(). I'm copying only value type variable data(int x = 100) I can simplify this method by using MemberwiseClone(). But If there's a reference type variable data(Person x = person1) this approach results in just a shallow copy so you should be careful.
+//c Update a class Point. I add a PointDescription(class) type field desc. And I add a custom constructor and override ToString() to make the desc used.
 
 namespace CloneablePoint
 {
-    // The Point now supports "clone-ability."
     public class Point : ICloneable
     {
         public int X { get; set; }
         public int Y { get; set; }
+        public PointDescription desc = new PointDescription();
 
-        public Point(int xPos, int yPos) { X = xPos; Y = yPos; }
+        public Point(int xPos, int yPos, string petName)
+        {
+            X = xPos; Y = yPos;
+            desc.PetName = petName;
+        }
+        public Point(int xPos, int yPos)
+        {
+            X = xPos; Y = yPos;
+        }
         public Point() { }
 
         // Override Object.ToString().
         public override string ToString()
-        { return string.Format("X = {0}; Y = {1}", X, Y); }
-
-        //// Return a copy of the current object.
-        //public object Clone()
-        //{ return new Point(this.X, this.Y); }
-
-        public object Clone()
         {
-            // Copy each field of the Point member by member.
-            return this.MemberwiseClone();
+            return string.Format("X = {0}; Y = {1}; Name = {2};\nID = {3}\n",
+            X, Y, desc.PetName, desc.PointID);
         }
+
+        // Return a copy of the current object.
+        public object Clone()
+        { return this.MemberwiseClone(); }
     }
 }
