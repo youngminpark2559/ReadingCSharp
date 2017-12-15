03bd09fa81bbeb9c2d1fe1e388461810d711e5c6
youngmtool <youngmtool@gmail.com>
2017-12-15 12:42:57 +0900

    I use "method group conversion syntax" to register CarEngineHandler(delegate) type instance pointing to CallMeHere() to CarEngineHandler(delegate) type instance's invocation list (listOfHandlers) by passing method name I want to register into that invocation list rather than passing CarEngineHandler(delegate) type instance pointing to CallMeHere().

--- CSharp/AnPhCs6Net46/10/CarDelegateMethodGroupConversion/Program.cs
+++ CSharp/AnPhCs6Net46/10/CarDelegateMethodGroupConversion/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application CarDelegateMethodGroupConversion.
+//c I use "method group conversion syntax" to register CarEngineHandler(delegate) type instance pointing to CallMeHere() to CarEngineHandler(delegate) type instance's invocation list (listOfHandlers) by passing method name I want to register into that invocation list rather than passing CarEngineHandler(delegate) type instance pointing to CallMeHere().
 
 namespace CarDelegateMethodGroupConversion
 {
@@ -12,6 +13,29 @@ namespace CarDelegateMethodGroupConversion
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Method Group Conversion *****\n");
+            Car c1 = new Car();
+
+            // Register the simple method name.
+            c1.RegisterWithCarEngine(CallMeHere);
+
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            // Unregister the simple method name.
+            c1.UnRegisterWithCarEngine(CallMeHere);
+
+            // No more notifications!
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.ReadLine();
+        }
+
+        static void CallMeHere(string msg)
+        {
+            Console.WriteLine("=> Message from Car: {0}", msg);
         }
     }
 }
