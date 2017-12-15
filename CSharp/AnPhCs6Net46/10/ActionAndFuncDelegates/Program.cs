2fdd0f67142bf65f6bac0a13977a717024d34fb2
youngmtool <youngmtool@gmail.com>
2017-12-15 15:49:47 +0900

    Update a method Main(). I use Func<>(delegate) type.

--- CSharp/AnPhCs6Net46/10/ActionAndFuncDelegates/Program.cs
+++ CSharp/AnPhCs6Net46/10/ActionAndFuncDelegates/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 //c Update a method Main(). I create an Action<>(delegate) type instance pointing to DisplayMessage() and assgin the reference of that instance to actionTarget. And I invoke DisplayMessage() via actionTarget by making CLR invoke Invoke() in sealed Action<> class with passing 3 arguments.
 //c Add a method Add taking 2 Int32(struct) data type numerical values, returning Int32(string) data type value.
 //c Add a method SumToString().
+//c Update a method Main(). I use Func<>(delegate) type.
 
 namespace ActionAndFuncDelegates
 {
@@ -21,6 +22,15 @@ namespace ActionAndFuncDelegates
             Action<string, ConsoleColor, int> actionTarget =
               new Action<string, ConsoleColor, int>(DisplayMessage);
             actionTarget("Action Message!", ConsoleColor.Yellow, 5);
+
+
+            Func<int, int, int> funcTarget = new Func<int, int, int>(Add);
+            int result = funcTarget.Invoke(40, 40);
+            Console.WriteLine("40 + 40 = {0}", result);
+
+            Func<int, int, string> funcTarget2 = new Func<int, int, string>(SumToString);
+            string sum = funcTarget2(90, 300);
+            Console.WriteLine(sum);
 
             Console.ReadLine();
         }
