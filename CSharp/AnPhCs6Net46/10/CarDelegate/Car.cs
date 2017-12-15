e2a44b450de04859731b886fb06da7a46132d1b1
youngmtool <youngmtool@gmail.com>
2017-12-15 15:55:04 +0900

    I change listOfHandlers field as from private to public.

--- CSharp/AnPhCs6Net46/10/CarDelegate/Car.cs
+++ CSharp/AnPhCs6Net46/10/CarDelegate/Car.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 //c Update a method RegisterWithCarEngine() by making this method enable to register mulitple method to the CarEngineHandler(delegate) type instance's invocation list.
 //c Update a method RegisterWithCarEngine(). I can make delegate multicast methods functionality by using Delegate.Combine(). Overloaded += operator is shorthand for this method. Compiler converts += operator used to register mulitple method to the delegate type instance's invocation list to Combine() when it creates CIL instructions for assembly.
 //c Add a method UnRegisterWithCarEngine(). This method removes passed delegate instance pointing to a specific method from the delegate type instance's invocation list.
+//c I change listOfHandlers field as from private to public.
 
 namespace CarDelegate
 {
@@ -36,7 +37,9 @@ namespace CarDelegate
         public delegate void CarEngineHandler(string msgForCaller);
 
         // 2) Define a member variable of this delegate.
-        private CarEngineHandler listOfHandlers;
+        //private CarEngineHandler listOfHandlers;
+        // Now a public member!
+        public CarEngineHandler listOfHandlers;
 
         // 3) Add registration function for the caller.
         // Now with multicasting support!
