c8d58c396815a925b47339d48e512d9cd9be0f34
youngmtool <youngmtool@gmail.com>
2017-12-15 23:01:44 +0900

    I add a CarEventArgs(class) type which derives from EventArgs(class) type.

---
+++ CSharp/AnPhCs6Net46/10/CarEvents/CarEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c I add a CarEventArgs(class) type which derives from EventArgs(class) type.
+
+namespace CarEvents
+{
+    public class CarEventArgs : EventArgs
+    {
+        public readonly string msg;
+        public CarEventArgs(string message)
+        {
+            msg = message;
+        }
+    }
+}
