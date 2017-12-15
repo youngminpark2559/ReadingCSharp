7667a0eb39775c4a81b92721e29f77bf64820a9e
youngmtool <youngmtool@gmail.com>
2017-12-15 15:36:03 +0900

    Add a method DisplayMessage() taking 3 arguments, returning void.

---
+++ CSharp/AnPhCs6Net46/10/ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Add a method DisplayMessage() taking 3 arguments, returning void.
+
+namespace ActionAndFuncDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        // This is a target for the Action<> delegate.
+        static void DisplayMessage(string msg, ConsoleColor txtColor, int printCount)
+        {
+            // Set color of console text.
+            ConsoleColor previous = Console.ForegroundColor;
+            Console.ForegroundColor = txtColor;
+
+            for (int i = 0; i < printCount; i++)
+            {
+                Console.WriteLine(msg);
+            }
+
+            // Restore color.
+            Console.ForegroundColor = previous;
+        }
+    }
+}
