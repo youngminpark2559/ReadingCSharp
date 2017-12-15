687b4f3a48f58578585d68623810bfa2221da90e
youngmtool <youngmtool@gmail.com>
2017-12-15 11:05:30 +0900

    I register 2 methods into the invocation list of CarEngineHandler(delegate) type instance.

--- CSharp/AnPhCs6Net46/10/CarDelegate/Program.cs
+++ CSharp/AnPhCs6Net46/10/CarDelegate/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 //c Create a console application CarDelegate.
 //c Update a method Main(). I instantiate CarEngineHandler(delegate) type instance. At the same time of instantiating CarEngineHandler(delegate) type instance, I also register OnCarEngineEvent() in the CarEngineHandler(delegate) type instance's invocation list. And I assign this CarEngineHandler(delegate) type instance to the field listOfHandlers. When carIsDead is true, I pass string "Sorry, this car is dead..." to the method (void OnCarEngineEvent(string msg)) which CarEngineHandler(delegate) type is holding in his invocation list.
+//c I register 2 methods into the invocation list of CarEngineHandler(delegate) type instance.
 
 namespace CarDelegate
 {
@@ -17,9 +18,10 @@ namespace CarDelegate
 
             // First, make a Car object.
             Car c1 = new Car("SlugBug", 100, 10);
-            // Now, tell the car which method to call
-            // when it wants to send us messages.
+
+            // Register multiple targets for the notifications.
             c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent2));
 
             // Speed up (this will trigger the events).
             Console.WriteLine("***** Speeding up *****");
@@ -27,13 +29,18 @@ namespace CarDelegate
                 c1.Accelerate(20);
             Console.ReadLine();
         }
-
-        // This is the target for incoming events.
+        // We now have TWO methods that will be called by the Car
+        // when sending notifications.
         public static void OnCarEngineEvent(string msg)
         {
             Console.WriteLine("\n***** Message From Car Object *****");
             Console.WriteLine("=> {0}", msg);
             Console.WriteLine("***********************************\n");
+        }
+
+        public static void OnCarEngineEvent2(string msg)
+        {
+            Console.WriteLine("=> {0}", msg.ToUpper());
         }
     }
 }
