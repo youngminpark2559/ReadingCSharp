9153b3fd1874befbb0f821db458403a2b29f88e7
youngmtool <youngmtool@gmail.com>
2017-12-16 11:57:29 +0900

    I add Int32(struct) data type local variable aboutToBlowCounter in the scope of Main() defining many anonymous methods. And In the anonymous method, I increment aboutToBlowCounter local variable value by one. In the end, aboutToBlowCounter becomes 2.

--- CSharp/AnPhCs6Net46/10/CarEvents/Program.cs
+++ CSharp/AnPhCs6Net46/10/CarEvents/Program.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 //c I update a method CarAboutToBlow() which is the event handler method for Exploded event. I update this event handler method to take 2 arguments.
 //c I use EventHandler<CarEventArgs>(delegate) type in Main().
 //c I update a method Main() by refactoring codes by using anonymous methods. I create anonymous methods to be added into events, with not creating methods to be added to events in the scope out of Main().
+//c I add Int32(struct) data type local variable aboutToBlowCounter in the scope of Main() defining many anonymous methods. And In the anonymous method, I increment aboutToBlowCounter local variable value by one. In the end, aboutToBlowCounter becomes 2.
 
 namespace CarEvents
 {
@@ -39,26 +40,31 @@ namespace CarEvents
             //    c1.Accelerate(20);
 
             Console.WriteLine("***** Anonymous Methods *****\n");
+
+            int aboutToBlowCounter = 0;
+
+            // Make a car as usual.
             Car c1 = new Car("SlugBug", 100, 10);
 
             // Register event handlers as anonymous methods.
             c1.AboutToBlow += delegate
             {
+                aboutToBlowCounter++;
                 Console.WriteLine("Eek! Going too fast!");
             };
 
             c1.AboutToBlow += delegate (object sender, CarEventArgs e)
             {
-                Console.WriteLine("Message from Car: {0}", e.msg);
-            };
-            c1.Exploded += delegate (object sender, CarEventArgs e)
-            {
-                Console.WriteLine("Fatal Message from Car: {0}", e.msg);
+                aboutToBlowCounter++;
+                Console.WriteLine("Critical Message from Car: {0}", e.msg);
             };
 
             // This will eventually trigger the events.
             for (int i = 0; i < 6; i++)
                 c1.Accelerate(20);
+
+            Console.WriteLine("AboutToBlow event was fired {0} times.",
+              aboutToBlowCounter);
 
             Console.ReadLine();
         }
