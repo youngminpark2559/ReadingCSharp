6b9b2d6a5e03b08fac6f760752a3050cbab3b410
youngmtool <youngmtool@gmail.com>
2017-12-16 13:47:13 +0900

    Create a console application SimpleLambdaExpressions to examine lambda expression.

---
+++ CSharp/AnPhCs6Net46/10/SimpleLambdaExpressions/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//c Create a console application SimpleLambdaExpressions to examine lambda expression.
+
+namespace SimpleLambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Lambdas *****\n");
+            TraditionalDelegateSyntax();
+            Console.ReadLine();
+        }
+        static void TraditionalDelegateSyntax()
+        {
+            // Make a list of integers.
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+
+            // Call FindAll() using traditional delegate syntax.
+            Predicate<int> callback = IsEvenNumber;
+            List<int> evenNumbers = list.FindAll(callback);
+
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.Write("{0}\t", evenNumber);
+            }
+            Console.WriteLine();
+        }
+
+        static void AnonymousMethodSyntax()
+        {
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+
+            List<int> evenNumbers = list.FindAll(
+              // Now, you use an anonymous method.
+              delegate (int i)
+              {
+                  return (i % 2) == 0;
+              });
+
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.Write("{0}\t", evenNumber);
+            }
+            Console.WriteLine();
+        }
+
+        // Target for the Predicate<> delegate.
+        static bool IsEvenNumber(int i)
+        {
+            // Is it an even number?
+            return (i % 2) == 0;
+        }
+    }
+}
