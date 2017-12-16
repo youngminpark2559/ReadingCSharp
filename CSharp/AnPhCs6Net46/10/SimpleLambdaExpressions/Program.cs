a585871dcc4d796093d661ba2451b1002e0d8c94
youngmtool <youngmtool@gmail.com>
2017-12-16 14:01:28 +0900

    I use lambda expression instead of Predicate<T>(delegate) type instance pointing to IsEvenNumber() or anonymous method implementing same logic of IsEvenNumber().

--- CSharp/AnPhCs6Net46/10/SimpleLambdaExpressions/Program.cs
+++ CSharp/AnPhCs6Net46/10/SimpleLambdaExpressions/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //c Create a console application SimpleLambdaExpressions to examine lambda expression.
+//c I use lambda expression instead of Predicate<T>(delegate) type instance pointing to IsEvenNumber() or anonymous method implementing same logic of IsEvenNumber().
 
 namespace SimpleLambdaExpressions
 {
@@ -24,6 +25,8 @@ namespace SimpleLambdaExpressions
 
             // Call FindAll() using traditional delegate syntax.
             Predicate<int> callback = IsEvenNumber;
+
+            // FindAll() is defined in the System.Collections.Generic.List<T>(class) type.
             List<int> evenNumbers = list.FindAll(callback);
 
             Console.WriteLine("Here are your even numbers:");
@@ -45,6 +48,23 @@ namespace SimpleLambdaExpressions
               {
                   return (i % 2) == 0;
               });
+
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.Write("{0}\t", evenNumber);
+            }
+            Console.WriteLine();
+        }
+
+        static void LambdaExpressionSyntax()
+        {
+            // Make a list of integers.
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+
+            // Now, use a C# lambda expression.
+            List<int> evenNumbers = list.FindAll(i => (i % 2) == 0);
 
             Console.WriteLine("Here are your even numbers:");
             foreach (int evenNumber in evenNumbers)
